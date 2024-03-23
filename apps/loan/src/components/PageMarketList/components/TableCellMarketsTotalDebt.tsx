32de5e2e34c4775ca92f1f12eb5a29b92bfddc2b
Amy Tsang <amy0tsang@gmail.com>
2024-03-23 05:10:13 -0700

    fix: Change debt fraction to of total supply

--- apps/loan/src/components/PageMarketList/components/TableCellMarketsTotalDebt.tsx
+++ apps/loan/src/components/PageMarketList/components/TableCellMarketsTotalDebt.tsx
@@ -1,30 +1,36 @@
 import type { ChipProps } from '@/ui/Typography/types'
 
+import styled from 'styled-components'
+
 import { FORMAT_OPTIONS, formatNumber } from '@/ui/utils'
 import useStore from '@/store/useStore'
 
 import Chip from '@/ui/Typography/Chip'
 
-const TableCellMarketsTotalDebt = ({ type, ...props }: ChipProps & { type: 'debt-fraction' | 'peg-keeper-debt' }) => {
+const TableCellMarketsTotalDebt = (props: ChipProps) => {
   const totalSupplyResp = useStore((state) => state.crvusdTotalSupply)
 
   const { total, minted, pegKeepersDebt, error } = totalSupplyResp ?? {}
 
+  const formattedDebtFraction =
+    total === '' && minted === '' ? '-' : formatNumber(((+total - +minted) / +total) * 100, FORMAT_OPTIONS.PERCENT)
+
   return (
     <>
-      {error ? (
+      {typeof totalSupplyResp === 'undefined' ? null : error ? (
         '?'
-      ) : type === 'debt-fraction' ? (
-        +total > 0 && +minted > 0 ? (
-          <Chip {...props}>{formatNumber(((+total - +minted) / +total) * 100, FORMAT_OPTIONS.PERCENT)}</Chip>
-        ) : (
-          '-'
-        )
-      ) : type === 'peg-keeper-debt' ? (
-        <Chip {...props}>{formatNumber(pegKeepersDebt, { defaultValue: '-' })}</Chip>
-      ) : null}
+      ) : (
+        <Chip {...props}>
+          {formatNumber(pegKeepersDebt, { defaultValue: '-' })}{' '}
+          <DebtFraction>({formattedDebtFraction} of total supply)</DebtFraction>
+        </Chip>
+      )}
     </>
   )
 }
+
+const DebtFraction = styled.span`
+  font-weight: normal;
+`
 
 export default TableCellMarketsTotalDebt
