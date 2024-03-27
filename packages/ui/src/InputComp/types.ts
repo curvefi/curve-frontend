c1c8cd49d8e1638f5fa14dacfe307d93796073f6
JustJousting <justjousting2moon@gmail.com>
2024-03-27 10:15:41 +0200

    task: added small version of search input

--- packages/ui/src/InputComp/types.ts
+++ packages/ui/src/InputComp/types.ts
@@ -1,12 +1,13 @@
 import * as React from 'react'
 
 export type InputMinHeight = 'small' | 'medium' | 'large' | 'x-large'
-export type InputVariant = 'error' | 'warning'
+export type InputVariant = 'error' | 'warning' | 'small'
 
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   id: string
   globalDisabled?: boolean
   minHeight?: InputMinHeight
+  variant?: 'small'
   inputProps?: React.InputHTMLAttributes<HTMLInputElement>
 }
 
