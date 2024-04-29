7b87ea11edb36072671d69f4893129719415132c
JustJousting <justjousting2moon@gmail.com>
2024-04-29 15:54:49 +0300

    task: Init list of gauges

--- apps/dao/src/store/createGaugesSlice.ts
+++ apps/dao/src/store/createGaugesSlice.ts
@@ -5,13 +5,19 @@ import orderBy from 'lodash/orderBy'
 import produce from 'immer'
 
 import { shortenTokenAddress } from '@/ui/utils'
+import { platform } from 'os'
 
 type StateKey = keyof typeof DEFAULT_STATE
 
 type SliceState = {
   gaugesLoading: boolean
+  filteringGaugesLoading: boolean
+  activeSortBy: SortByFilterGauges
+  activeSortDirection: ActiveSortDirection
+  searchValue: string
   gaugeMapper: PricesGaugeOverviewData[]
-  pieData: PieData[]
+  gaugeFormattedData: GaugeFormattedData[]
+  filteredGauges: PricesGaugeOverviewData[]
 }
 
 const sliceKey = 'gauges'
@@ -19,7 +25,12 @@ const sliceKey = 'gauges'
 // prettier-ignore
 export type GaugesSlice = {
   [sliceKey]: SliceState & {
-    getGauges(curve: CurveApi): void
+    getGauges(curve: CurveApi): Promise<void>
+    setSearchValue(searchValue: string): void
+    setActiveSortBy(sortBy: SortByFilterGauges): void
+    setActiveSortDirection(direction: ActiveSortDirection): void
+    selectFilteredSortedGauges(): PricesGaugeOverviewData[]
+    setGauges(searchValue: string): void
     setStateByKey<T>(key: StateKey, value: T): void
     setStateByKeys(SliceState: Partial<SliceState>): void
     resetState(): void
@@ -28,8 +39,13 @@ export type GaugesSlice = {
 
 const DEFAULT_STATE: SliceState = {
   gaugesLoading: true,
+  filteringGaugesLoading: true,
+  activeSortBy: 'relativeWeight',
+  activeSortDirection: 'desc',
+  searchValue: '',
   gaugeMapper: [],
-  pieData: [],
+  gaugeFormattedData: [],
+  filteredGauges: [],
 }
 
 // units of gas used * (base fee + priority fee)
@@ -47,13 +63,18 @@ const createGaugesSlice = (set: SetState<State>, get: GetState<State>): GaugesSl
 
         console.log('formattedGauges', formattedGauges.gauges)
 
-        const pieData = formattedGauges.gauges
+        const gaugeFormattedData = formattedGauges.gauges
           .filter((gauge) => gauge.gauge_relative_weight > 0)
           .map((gauge) => ({
             name: gauge.name,
             address: gauge.address,
+            // make dynamic
+            network: 'ethereum',
+            platform: 'AMM',
+            emissions: gauge.emissions,
             title: gauge.pool?.name
-              ? gauge.pool.name.split(': ')[1] || gauge.pool.name
+              ? // remove extras like "Factory Pool" etc
+                (gauge.pool.name.split(': ')[1] || gauge.pool.name).replace(/curve\.fi/i, '').trim()
               : shortenTokenAddress(gauge.address),
             gauge_relative_weight: +(gauge.gauge_relative_weight * 100).toFixed(4),
             gauge_weight_7d_delta:
@@ -61,16 +82,64 @@ const createGaugesSlice = (set: SetState<State>, get: GetState<State>): GaugesSl
             gauge_weight_60d_delta:
               gauge.gauge_weight_60d_delta != null ? +(gauge.gauge_weight_60d_delta * 100).toFixed(4) : null,
           }))
-        // .sort((a, b) => b.gauge_relative_weight - a.gauge_relative_weight)
+          .sort((a, b) => b.gauge_relative_weight - a.gauge_relative_weight)
 
-        console.log('pieData', pieData)
+        console.log('GaugeFormattedData', gaugeFormattedData)
 
         get().setAppStateByKey(sliceKey, 'gaugeMapper', formattedGauges.gauges)
-        get().setAppStateByKey(sliceKey, 'pieData', pieData)
+        get().setAppStateByKey(sliceKey, 'gaugeFormattedData', gaugeFormattedData)
         get().setAppStateByKey(sliceKey, 'gaugesLoading', false)
       } catch (error) {
         console.log(error)
       }
+    },
+    selectFilteredSortedGauges: () => {
+      const { gaugeMapper, activeSortBy, activeSortDirection } = get()[sliceKey]
+
+      let gaugesCopy = [...gaugeMapper]
+
+      if (activeSortBy === 'relativeWeight') {
+        if (activeSortDirection === 'asc') {
+          gaugesCopy = gaugesCopy.sort((a, b) => a.gauge_relative_weight - b.gauge_relative_weight)
+        } else {
+          gaugesCopy = gaugesCopy.sort((a, b) => b.gauge_relative_weight - a.gauge_relative_weight)
+        }
+      } else {
+        gaugesCopy = orderBy(gaugesCopy, [activeSortBy], [activeSortDirection])
+      }
+
+      return gaugesCopy
+    },
+    setGauges: (searchValue: string) => {
+      const { selectFilteredSortedGauges } = get()[sliceKey]
+      get()[sliceKey].setStateByKey('filteringGaugesLoading', true)
+
+      setTimeout(() => {
+        const gauges = selectFilteredSortedGauges()
+
+        if (searchValue !== '') {
+          const searchFilteredProposals = searchFn(searchValue, gauges)
+          get()[sliceKey].setStateByKeys({
+            filteringGaugesLoading: false,
+            filteredGauges: searchFilteredProposals,
+          })
+          return searchFilteredProposals
+        }
+
+        get()[sliceKey].setStateByKeys({
+          filteringGaugesLoading: false,
+          filteredGauges: gauges,
+        })
+      }, 500)
+    },
+    setSearchValue: (filterValue) => {
+      get()[sliceKey].setStateByKey('searchValue', filterValue)
+    },
+    setActiveSortDirection: (direction: ActiveSortDirection) => {
+      get()[sliceKey].setStateByKey('activeSortDirection', direction)
+    },
+    setActiveSortBy: (sortBy: SortByFilterGauges) => {
+      get()[sliceKey].setStateByKey('activeSortBy', sortBy)
     },
     setStateByKey: (key, value) => {
       get().setAppStateByKey(sliceKey, key, value)
@@ -88,23 +157,23 @@ const convertNumber = (number: number) => {
   return number / 10 ** 18
 }
 
-const searchFn = (filterValue: string, proposals: ProposalData[]) => {
-  const fuse = new Fuse<ProposalData>(proposals, {
+const searchFn = (filterValue: string, gauges: PricesGaugeOverviewData[]) => {
+  const fuse = new Fuse<PricesGaugeOverviewData>(gauges, {
     ignoreLocation: true,
     threshold: 0.3,
     includeScore: true,
     keys: [
-      'voteId',
-      'creator',
-      'voteType',
-      {
-        name: 'metaData',
-        getFn: (proposal) => {
-          // Preprocess the metaData field
-          const metaData = proposal.metadata || ''
-          return metaData.toLowerCase()
-        },
-      },
+      'address',
+      'lp_token',
+      'name',
+      // {
+      //   name: 'metaData',
+      //   getFn: (proposal) => {
+      //     // Preprocess the metaData field
+      //     const metaData = proposal.metadata || ''
+      //     return metaData.toLowerCase()
+      //   },
+      // },
     ],
   })
 
