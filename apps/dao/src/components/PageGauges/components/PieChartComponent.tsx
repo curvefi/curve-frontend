dc53bbad015cb993aa9d33014881ca502073dc68
JustJousting <justjousting2moon@gmail.com>
2024-04-29 15:54:39 +0300

    task: Break out chart tooltip component

--- apps/dao/src/components/PageGauges/components/PieChartComponent.tsx
+++ apps/dao/src/components/PageGauges/components/PieChartComponent.tsx
@@ -1,14 +1,11 @@
-import type { NameType, ValueType } from 'recharts/types/component/DefaultTooltipContent'
-
 import styled from 'styled-components'
-import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer, TooltipProps } from 'recharts'
-import { t } from '@lingui/macro'
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from 'recharts'
 import { useState } from 'react'
 
-import Box from '@/ui/Box'
+import CustomTooltip from './CustomTooltip'
 
 type Props = {
-  data: PieData[]
+  data: GaugeFormattedData[]
 }
 
 type CustomLabelProps = {
@@ -19,7 +16,7 @@ type CustomLabelProps = {
   outerRadius: number
   percent: number
   index: number
-  payload: PieData
+  payload: GaugeFormattedData
 }
 
 const COLORS = [
@@ -78,7 +75,7 @@ const PieChartComponent = ({ data }: Props) => {
 
 // Custom label component
 const CustomLabel = ({ payload, cx, cy, midAngle, outerRadius, index }: CustomLabelProps) => {
-  if (payload.gauge_relative_weight > 2) {
+  if (payload.gauge_relative_weight > 1.7) {
     const RADIAN = Math.PI / 180
     // Calculate the starting point at the edge of the pie slice
     const startRadius = outerRadius * 0.99 // Slightly inside the outer edge to ensure visibility
@@ -116,45 +113,8 @@ const CustomLabel = ({ payload, cx, cy, midAngle, outerRadius, index }: CustomLa
   return null
 }
 
-const CustomTooltip = ({ active, payload }: TooltipProps<ValueType, NameType>) => {
-  if (active && payload && payload.length) {
-    const sevenDayDelta = payload[0].payload.gauge_weight_7d_delta
-    const sixtyDayDelta = payload[0].payload.gauge_weight_60d_delta
-
-    return (
-      <TooltipWrapper>
-        <TooltipTitle>{payload[0].name}</TooltipTitle>
-        <Box flex flexColumn flexGap={'var(--spacing-1)'}>
-          <TooltipColumn>
-            <TooltipDataTitle>{t`Relative Weight`}</TooltipDataTitle>
-            <TooltipData>{payload[0].payload.gauge_relative_weight}%</TooltipData>
-          </TooltipColumn>
-          <TooltipColumn>
-            <TooltipDataTitle>{t`Gauge weight 7d delta`}</TooltipDataTitle>
-            {sevenDayDelta ? (
-              <TooltipData className={sevenDayDelta > 0 ? 'positive' : 'negative'}>{sevenDayDelta}%</TooltipData>
-            ) : (
-              <TooltipDataNotAvailable>{t`Not available`}</TooltipDataNotAvailable>
-            )}
-          </TooltipColumn>
-          <TooltipColumn>
-            <TooltipDataTitle>{t`Gauge weight 60d delta`}</TooltipDataTitle>
-            {sixtyDayDelta ? (
-              <TooltipData className={sixtyDayDelta > 0 ? 'positive' : 'negative'}>{sixtyDayDelta}%</TooltipData>
-            ) : (
-              <TooltipDataNotAvailable>{t`Not available`}</TooltipDataNotAvailable>
-            )}
-          </TooltipColumn>
-        </Box>
-      </TooltipWrapper>
-    )
-  }
-
-  return null
-}
-
 const Wrapper = styled.div<{ chartHeight: number }>`
-  width: 550px;
+  width: 100%;
   height: ${({ chartHeight }) => `${chartHeight}px`};
   position: relative;
   margin: 0 auto;
@@ -172,50 +132,6 @@ const CellLabel = styled.text`
   font-size: var(--font-size-1);
   fill: var(--page--text-color);
   font-weight: var(--bold);
-`
-
-const TooltipWrapper = styled.div`
-  background-color: var(--page--background-color);
-  padding: var(--spacing-3);
-  border-radius: var(--border-radius-1);
-  display: flex;
-  flex-direction: column;
-  gap: var(--spacing-2);
-`
-
-const TooltipTitle = styled.p`
-  font-size: var(--font-size-3);
-  color: var(--page--text-color);
-  font-weight: var(--bold);
-`
-
-const TooltipColumn = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-
-const TooltipDataTitle = styled.p`
-  font-size: var(--font-size-1);
-  font-weight: var(--bold);
-  opacity: 0.7;
-  color: var(--page--text-color);
-`
-
-const TooltipData = styled.p`
-  font-size: var(--font-size-2);
-  color: var(--page--text-color);
-  font-weight: var(--bold);
-  &.positive {
-    color: var(--chart-green);
-  }
-  &.negative {
-    color: var(--chart-red);
-  }
-`
-const TooltipDataNotAvailable = styled.p`
-  font-size: var(--font-size-2);
-  color: var(--page--text-color);
-  font-style: italic;
 `
 
 export default PieChartComponent
