7fce6a4167f60014c1014215962df988d009236a
Amy Tsang <amy0tsang@gmail.com>
2024-05-01 08:02:41 -0700

    task: change cap base APY to 5000, add warning if over 5000

---
+++ apps/main/src/components/PagePoolList/components/ChipVolatileBaseApy.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { t } from '@lingui/macro'
+import styled from 'styled-components'
+
+import { formatNumber } from '@/ui/utils'
+
+import Chip from '@/ui/Typography/Chip'
+import Icon from '@/ui/Icon'
+
+const ChipVolatileBaseApy = ({ isBold, showIcon }: { isBold?: boolean; showIcon?: boolean }) => {
+  return (
+    <VolatileChip
+      size="md"
+      isBold={isBold}
+      tooltip={t`This is a volatile number that will very likely not persist.`}
+      tooltipProps={{ textAlign: 'left', minWidth: '250px' }}
+    >
+      {formatNumber(5000)}+% {showIcon && <Icon className="svg-tooltip" size={16} name="InformationSquare" />}
+    </VolatileChip>
+  )
+}
+
+const VolatileChip = styled(Chip)`
+  color: var(--danger-400);
+`
+
+export default ChipVolatileBaseApy
--- apps/main/src/components/PagePoolList/components/TableCellRewardsBase.tsx
+++ apps/main/src/components/PagePoolList/components/TableCellRewardsBase.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { t } from '@lingui/macro'
 
 import { LARGE_APY } from '@/constants'
 import { FORMAT_OPTIONS, formatNumber } from '@/ui/utils'
 
 import { Chip } from '@/ui/Typography'
+import ChipVolatileBaseApy from '@/components/PagePoolList/components/ChipVolatileBaseApy'
 import IconTooltip from '@/ui/Tooltip/TooltipIcon'
 import TooltipBaseApy from '@/components/PagePoolList/components/TooltipBaseApy'
 
@@ -14,35 +16,38 @@ type Props = {
 }
 
 const TableCellRewardsBase = ({ base, isHighlight, poolData }: Props) => {
-  let baseFormatted = ''
-  if (base?.day) {
-    if (+base.day > LARGE_APY) {
-      baseFormatted = `${formatNumber(LARGE_APY)}+%`
-    } else {
-      baseFormatted = `${formatNumber(base.day, FORMAT_OPTIONS.PERCENT)}`
-    }
-  }
-
   const failedFetching24hOldVprice =
     poolData && 'failedFetching24hOldVprice' in poolData && poolData.failedFetching24hOldVprice
 
-  return failedFetching24hOldVprice ? (
-    <span>
-      -<IconTooltip>Not available currently</IconTooltip>
-    </span>
-  ) : (
-    <Chip
-      isBold={isHighlight}
-      size="md"
-      tooltip={!!base ? <TooltipBaseApy poolData={poolData} baseApy={base} /> : null}
-      tooltipProps={{
-        noWrap: true,
-        placement: 'bottom end',
-        textAlign: 'left',
-      }}
-    >
-      {baseFormatted}
-    </Chip>
+  return (
+    <>
+      {failedFetching24hOldVprice ? (
+        <span>
+          -<IconTooltip>Not available currently</IconTooltip>
+        </span>
+      ) : (
+        typeof base !== 'undefined' && (
+          <>
+            {+base.day > LARGE_APY ? (
+              <ChipVolatileBaseApy isBold={isHighlight} />
+            ) : (
+              <Chip
+                isBold={isHighlight}
+                size="md"
+                tooltip={!!base ? <TooltipBaseApy poolData={poolData} baseApy={base} /> : null}
+                tooltipProps={{
+                  noWrap: true,
+                  placement: 'bottom end',
+                  textAlign: 'left',
+                }}
+              >
+                {formatNumber(base.day, FORMAT_OPTIONS.PERCENT)}
+              </Chip>
+            )}
+          </>
+        )
+      )}
+    </>
   )
 }
 
