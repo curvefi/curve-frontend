069e0d4142b4ad834d3ffb504525adb56f2d579c
JustJousting <justjousting2moon@gmail.com>
2024-05-03 14:36:36 +0200

    task: add formatNumberWithSuffix util

--- apps/dao/src/utils/utilsVotes.ts
+++
@@ -1,9 +0,0 @@
-export const formatVotingPower = (votingPower: number): string => {
-  if (votingPower >= 1000000) {
-    return `${(votingPower / 1000000).toFixed(2)}M`
-  } else if (votingPower >= 1000) {
-    return `${(votingPower / 1000).toFixed(2)}K`
-  } else {
-    return votingPower.toFixed(0)
-  }
-}
