c8084ed3a3235ea91e5f54b6c0d504c25afd0b9c
Amy Tsang <amy0tsang@gmail.com>
2024-06-07 06:28:39 -0700

    task: add create loan arbitrum test

---
+++ tests/cypress/e2e/lend/1-arbitrum/0-crv/arbitrum-crv-crvusd-loan.cy.ts
@@ -0,0 +1,54 @@
+import { ethers } from 'ethers'
+
+import createLoanSettings from '@/fixtures/create-loan-settings.json'
+import markets from '@/fixtures/markets.json'
+import tokens from '@/fixtures/tokens.json'
+
+const CHAIN = 'arbitrum'
+const CHAIN_ID = 42161
+const MARKET_ID = 'CRV-crvUSD'
+describe(`Lend ${MARKET_ID} ${CHAIN} market`, () => {
+  const market = markets[CHAIN][MARKET_ID]
+  const settings = createLoanSettings[CHAIN][market.id]
+  const collateralToken = tokens[CHAIN][market.collateral]
+  const borrowToken = tokens[CHAIN][market.borrow]
+
+  beforeEach(() => {
+    // prepare wallet
+    cy.createJsonRpcProvider(CHAIN_ID)
+      .as('jsonRpcProvider', { type: 'static' })
+      .createRandomWallet('1', [{ symbol: collateralToken.symbol, amount: settings.collateralTokenToAllocate }])
+      .as('wallet', { type: 'static' })
+      .prepareMetamaskWallet()
+
+    // prepare page
+    cy.intercept('GET', 'https://api.curve.fi/api/**').as('getAPI')
+    cy.visit(market.createUrl)
+    cy.wait('@getAPI').its('response.statusCode').should('equal', 200)
+
+    // connect metamask
+    cy.get('@wallet').connectMetamask()
+  })
+
+  it('Create max amount soft liquidation loan', () => {
+    cy.dataTestId('btn-approval').as('btnApproval').should('be.disabled')
+    cy.dataTestId('btn-create').as('btnCreate').should('be.disabled')
+
+    cy.inputMaxCollateral(settings.collateralTokenToAllocate)
+
+    cy.inputMaxBorrow()
+
+    cy.approveSpending()
+
+    cy.createSofLiquidationLoan()
+
+    cy.get<ethers.HDNodeWallet>('@wallet')
+      .tokenBalance(borrowToken.address)
+      .then(($tokenBalance) => {
+        cy.get<string>('@debtAmount').then(($debtAmount) =>
+          expect($tokenBalance).to.be.equal(ethers.parseUnits($debtAmount, borrowToken.decimals))
+        )
+      })
+    cy.get<ethers.HDNodeWallet>('@wallet').tokenBalance(collateralToken.address).should('eq', BigInt(0))
+  })
+})
