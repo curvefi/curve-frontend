824a015c80d7e83b4737c6256cc8736d80bc0295
JustJousting <justjousting2moon@gmail.com>
2024-06-10 14:48:10 +0300

    task: enable twocrypto pool creation on x-layer

--- apps/main/src/networks.ts
+++ apps/main/src/networks.ts
@@ -476,7 +476,7 @@ const networks: Record<ChainId, NetworkConfig> = {
       toAddress: '0x74b7f16337b8972027f6196a17a631ac6de26d22',
     },
     stableswapFactory: true,
-    twocryptoFactory: false,
+    twocryptoFactory: true,
     tricryptoFactory: true,
     hasFactory: true,
   },
