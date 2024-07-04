f75abefbf084a3df03d864664c371621ef3a694e
JustJousting <justjousting2moon@gmail.com>
2024-07-04 15:34:39 +0300

    task: updated readme and seed data for correct lend addresses

--- apps/lend/src/components/PageMarketList/components/TableRowViewContentTable/index.tsx
+++ apps/lend/src/components/PageMarketList/components/TableRowViewContentTable/index.tsx
@@ -5,6 +5,7 @@ import type {
   TableRowProps,
   TableRowSettings,
 } from '@/components/PageMarketList/types'
+import type { CampaignRewardsMapper } from '@/ui/CampaignRewards/types'
 import type { Params, NavigateFunction } from 'react-router-dom'
 
 import React, { useMemo } from 'react'
