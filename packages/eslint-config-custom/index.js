a1470f147e13603fe07b7cede4b944794b1fde6c
Ignat <ign.romanov@gmail.com>
2024-07-08 15:25:30 +0300

    build: add eslint tanstack-query config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,9 +1,11 @@
 module.exports = {
   extends: ['next', 'turbo', 'prettier'],
+  plugins: ['@tanstack/query'],
   rules: {
     '@next/next/no-img-element': 'off',
     '@next/next/no-html-link-for-pages': 'off',
     'react/jsx-key': 'off',
+    '@tanstack/query/exhaustive-deps': 'off',
   },
   parserOptions: {
     babelOptions: {
