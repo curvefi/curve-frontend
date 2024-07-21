669ca384a0d3b5d962a7703abb4051c643370dae
JustJousting <justjousting2moon@gmail.com>
2024-07-21 04:23:34 +0300

    task: add top locker sorting options

---
+++ apps/dao/src/components/PageVeCrv/constants.ts
@@ -0,0 +1,5 @@
+export const TOP_LOCKER_FILTERS: { key: TopLockerFilter; label: string }[] = [
+  { key: 'weight', label: 'Weight' },
+  { key: 'locked', label: 'Locked' },
+  { key: 'weight_ratio', label: 'Weight Ratio' },
+]
