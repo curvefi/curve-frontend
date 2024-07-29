ff1905e2a1581a877993941e6c9a8576a4d744e0
JustJousting <justjousting2moon@gmail.com>
2024-07-29 17:04:50 +0300

    cleanup

--- packages/external-rewards/src/campaigns/index.ts
+++ packages/external-rewards/src/campaigns/index.ts
@@ -1,29 +1,15 @@
 // This file is auto-generated. Do not edit.
 
-import DLCLink from './DLCLink.json';
-import Eigenlayer from './Eigenlayer.json';
-import Elixir from './Elixir.json';
-import Ethena from './Ethena.json';
-import Etherfi from './Etherfi.json';
-import Kelpdao from './Kelpdao.json';
-import Mellow from './Mellow.json';
-import PufferFinance from './PufferFinance.json';
-import Renzo from './Renzo.json';
-import Symbiotic from './Symbiotic.json';
-import Usual from './Usual.json';
-import YieldNest from './YieldNest.json';
+import DLCLink from './DLCLink.json'
+import Eigenlayer from './Eigenlayer.json'
+import Elixir from './Elixir.json'
+import Ethena from './Ethena.json'
+import Etherfi from './Etherfi.json'
+import Kelpdao from './Kelpdao.json'
+import Mellow from './Mellow.json'
+import PufferFinance from './PufferFinance.json'
+import Renzo from './Renzo.json'
+import Symbiotic from './Symbiotic.json'
+import Usual from './Usual.json'
 
-export {
-  DLCLink,
-  Eigenlayer,
-  Elixir,
-  Ethena,
-  Etherfi,
-  Kelpdao,
-  Mellow,
-  PufferFinance,
-  Renzo,
-  Symbiotic,
-  Usual,
-  YieldNest
-};
+export { DLCLink, Eigenlayer, Elixir, Ethena, Etherfi, Kelpdao, Mellow, PufferFinance, Renzo, Symbiotic, Usual }
