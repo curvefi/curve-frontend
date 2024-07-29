a15032d8a77ef1f589de04ebc49735a20884f077
JustJousting <justjousting2moon@gmail.com>
2024-07-29 16:26:47 +0300

    task: add support to banner and lend

--- apps/lend/src/components/CampaignRewardsBanner.tsx
+++ apps/lend/src/components/CampaignRewardsBanner.tsx
@@ -7,9 +7,25 @@ import CampaignBannerComp from 'ui/src/CampaignRewards/CampaignBannerComp'
 const CampaignRewardsBanner: React.FC<{ poolAddress: string }> = ({ poolAddress }) => {
   const campaignRewardsPool = useCampaignRewardsMapper()[poolAddress]
 
-  return campaignRewardsPool ? (
-    <CampaignBannerComp campaignRewardsPool={campaignRewardsPool} message={t`Borrowing in this pool earns points!`} />
-  ) : null
+  if (!campaignRewardsPool) return null
+
+  const currentTime = new Date().getTime() / 1000
+
+  const filteredRewardItems = campaignRewardsPool.filter((rewardItem) => {
+    // allow campaigns with no set period
+    if (rewardItem.campaignStart === '0' || rewardItem.campaignEnd === '0') {
+      return true
+    }
+
+    // check if current time is within the campaign period
+    const startTime = new Date(+rewardItem.campaignStart).getTime()
+    const endTime = new Date(+rewardItem.campaignEnd).getTime()
+    return currentTime >= startTime && currentTime <= endTime
+  })
+
+  return (
+    <CampaignBannerComp campaignRewardsPool={filteredRewardItems} message={t`Borrowing in this pool earns points!`} />
+  )
 }
 
 export default CampaignRewardsBanner
--- apps/main/src/components/PagePool/components/CampaignRewardsBanner.tsx
+++ apps/main/src/components/PagePool/components/CampaignRewardsBanner.tsx
@@ -7,16 +7,30 @@ import CampaignBannerComp from 'ui/src/CampaignRewards/CampaignBannerComp'
 const CampaignRewardsBanner: React.FC<{ poolAddress: string }> = ({ poolAddress }) => {
   const campaignRewardsPool = useCampaignRewardsMapper()[poolAddress]
 
+  if (!campaignRewardsPool) return null
+
   const isPoints = campaignRewardsPool && campaignRewardsPool.some((rewardItem) => rewardItem.tags.includes('points'))
+
+  const currentTime = new Date().getTime() / 1000
+
+  const filteredRewardItems = campaignRewardsPool.filter((rewardItem) => {
+    // allow campaigns with no set period
+    if (rewardItem.campaignStart === '0' || rewardItem.campaignEnd === '0') {
+      return true
+    }
+
+    // check if current time is within the campaign period
+    const startTime = new Date(+rewardItem.campaignStart).getTime()
+    const endTime = new Date(+rewardItem.campaignEnd).getTime()
+    return currentTime >= startTime && currentTime <= endTime
+  })
 
   const bannerMessage = () => {
     if (isPoints) return t`Liquidity providers in this pool also earn points!`
     return t`Liquidity providers in this pool also earn additional tokens!`
   }
 
-  return campaignRewardsPool ? (
-    <CampaignBannerComp campaignRewardsPool={campaignRewardsPool} message={bannerMessage()} />
-  ) : null
+  return <CampaignBannerComp campaignRewardsPool={filteredRewardItems} message={bannerMessage()} />
 }
 
 export default CampaignRewardsBanner
