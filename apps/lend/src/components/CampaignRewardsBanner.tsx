a2a9a3337ad5f5fae5df6b885d3bba49d36d707f
JustJousting <justjousting2moon@gmail.com>
2024-07-31 04:02:45 +0300

    fix: init campaign rewards from all lend pages

--- apps/lend/src/components/CampaignRewardsBanner.tsx
+++ apps/lend/src/components/CampaignRewardsBanner.tsx
@@ -15,6 +15,15 @@ const CampaignRewardsBanner: React.FC<CampaignRewardsBannerProps> = ({ borrowAdd
 
   if (!supplyCampaignRewardsPool && !borrowCampaignRewardsPool) return null
 
+  const campaignRewardsPools = () => {
+    if (supplyCampaignRewardsPool && borrowCampaignRewardsPool) {
+      return [...supplyCampaignRewardsPool, ...borrowCampaignRewardsPool]
+    }
+    if (supplyCampaignRewardsPool) return supplyCampaignRewardsPool
+    if (borrowCampaignRewardsPool) return borrowCampaignRewardsPool
+    return []
+  }
+
   const message =
     supplyCampaignRewardsPool && borrowCampaignRewardsPool
       ? t`Supplying and borrowing in this pool earns points!`
@@ -22,12 +31,7 @@ const CampaignRewardsBanner: React.FC<CampaignRewardsBannerProps> = ({ borrowAdd
       ? t`Supplying in this pool earns points!`
       : t`Borrowing in this pool earns points!`
 
-  return (
-    <CampaignBannerComp
-      campaignRewardsPool={[...supplyCampaignRewardsPool, ...borrowCampaignRewardsPool]}
-      message={message}
-    />
-  )
+  return <CampaignBannerComp campaignRewardsPool={campaignRewardsPools()} message={message} />
 }
 
 export default CampaignRewardsBanner
