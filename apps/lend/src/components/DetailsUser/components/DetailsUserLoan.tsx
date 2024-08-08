6f3b184976b01a992ef8aa3899f6e832e76f1970
Amy Tsang <amy0tsang@gmail.com>
2024-08-08 13:48:28 -0700

    task: add Loss to Lend

--- apps/lend/src/components/DetailsUser/components/DetailsUserLoan.tsx
+++ apps/lend/src/components/DetailsUser/components/DetailsUserLoan.tsx
@@ -1,4 +1,4 @@
-import type { Detail } from '@/components/DetailsMarket/types'
+import type { TooltipProps } from '@/ui/Tooltip/types'
 
 import React from 'react'
 import { t } from '@lingui/macro'
@@ -9,25 +9,33 @@ import { breakpoints } from '@/ui/utils'
 import useStore from '@/store/useStore'
 import networks from '@/networks'
 
-import {
-  Content,
-  ContentStat,
-  ContentStats,
-  ContentStatTitle,
-  ContentStatValue,
-} from '@/components/DetailsMarket/styles'
+import { Content } from '@/components/DetailsMarket/styles'
 import AlertNoLoanFound from '@/components/AlertNoLoanFound'
 import Box from '@/ui/Box'
 import CellHealthStatus from '@/components/SharedCellData/CellHealthStatus'
 import CellUserMain from '@/components/SharedCellData/CellUserMain'
+import CellLlammaBalances from '@/components/SharedCellData/CellLlammaBalances'
 import CellLiquidationRange from '@/components/SharedCellData/CellLiquidationRange'
 import CellLoanState from '@/components/SharedCellData/CellLoanState'
+import CellLoss from '@/components/SharedCellData/CellLoss'
 import CellRate from '@/components/SharedCellData/CellRate'
+import ExternalLink from 'ui/src/Link/ExternalLink'
 import DetailsConnectWallet from '@/components/DetailsUser/components/DetailsConnectWallet'
 import DetailsUserLoanAlertSoftLiquidation from '@/components/DetailsUser/components/DetailsUserLoanAlertSoftLiquidation'
 import DetailsUserLoanChartBandBalances from '@/components/DetailsUser/components/DetailsUserLoanChartBandBalances'
 import DetailsUserLoanChartLiquidationRange from '@/components/DetailsUser/components/DetailsUserLoanChartLiquidationRange'
 import ChartOhlcWrapper from '@/components/ChartOhlcWrapper'
+import ListInfoItem, { ListInfoItems, ListInfoItemsWrapper } from '@/ui/ListInfo'
+import TextCaption from '@/ui/TextCaption'
+
+type Content = {
+  title: React.ReactNode | string
+  value: React.ReactNode
+  tooltip?: React.ReactNode | string
+  tooltipProps?: TooltipProps
+  className?: string
+  show?: boolean
+}
 
 const DetailsUserLoan = (pageProps: PageContentProps) => {
   const { rChainId, rOwmId, api, owmDataCachedOrApi, userActiveKey } = pageProps
@@ -40,6 +48,7 @@ const DetailsUserLoan = (pageProps: PageContentProps) => {
   // TODO: handle error
   const { details: userLoanDetails } = userLoanDetailsResp ?? {}
   const { signerAddress } = api ?? {}
+  const { symbol: collateralSymbol = '' } = owmDataCachedOrApi?.owm?.collateral_token ?? {}
 
   const showConnectWallet = typeof signerAddress !== 'undefined' && !signerAddress
   const foundLoan = typeof loanExistsResp !== 'undefined' && loanExistsResp.loanExists
@@ -55,10 +64,25 @@ const DetailsUserLoan = (pageProps: PageContentProps) => {
   }
 
   // prettier-ignore
-  const stats: Detail[][] = [
+  const stats: Content[][] = [
     [
-      { title: t`Status`, value: <CellHealthStatus {...cellProps}  type="status"  /> },
-      { title: t`Health`, value: <CellHealthStatus {...cellProps} type="percent"  /> },
+      { title: t`Status`, value: <CellHealthStatus {...cellProps} type="status" /> },
+      {
+        title: t`Health`,
+        tooltip: (
+          <Box grid gridGap={2}>
+            <p>{t`The loan metric indicates the current health of your position.`}</p>
+            <p>
+              {t`Hard liquidation is triggered when health is 0 or below.`}{' '}
+              <ExternalLink href="https://resources.curve.fi/lending/overview/#health-hard-liquidation" $noStyles>
+                Click here to learn more.
+              </ExternalLink>
+            </p>
+          </Box>
+        ),
+        tooltipProps: { minWidth: '250px' },
+        value: <CellHealthStatus {...cellProps} type="percent"  />
+      },
       { title: t`Borrow APY`, value: <CellRate {...cellProps} type="borrow" /> }
     ],
     [
@@ -67,7 +91,12 @@ const DetailsUserLoan = (pageProps: PageContentProps) => {
       { title: t`Range %`, value: <CellLiquidationRange {...cellProps} type='bandPct' />, show: isAdvanceMode },
     ],
     [
-      { title: t`Collateral composition`, value: <CellLoanState {...cellProps} />, className: 'isRow' },
+      { title: <>{t`Collateral`} <TextCaption isCaps={false}>({collateralSymbol})</TextCaption><br/>{t`Current balance (est.) / Deposited`}</>, tooltip: t`Current balance (est.) is current balance minus losses.`, value: <CellLoanState {...cellProps} />, className: 'isRow' },
+      { title: t`Loss amount`, value: <CellLoss {...cellProps} type='amount' />, className: 'isRow' },
+      { title: t`% Lost`, tooltip: t`This metric measures the loss in collateral value caused by LLAMMA's soft liquidation process, which is activated when the oracle price falls within a user’s liquidation range.`, tooltipProps: {minWidth: '250px'}, value: <CellLoss {...cellProps} type='percent' />, className: 'isRow' },
+    ],
+    [
+      { title: t`LLAMMA Balances`, value: <CellLlammaBalances {...cellProps} /> }
     ]
   ]
 
@@ -76,7 +105,7 @@ const DetailsUserLoan = (pageProps: PageContentProps) => {
       {showConnectWallet ? (
         <DetailsConnectWallet />
       ) : foundLoan ? (
-        <Wrapper>
+        <div>
           {isSoftLiquidation && (
             <AlertContent>
               <DetailsUserLoanAlertSoftLiquidation {...pageProps} />
@@ -88,24 +117,20 @@ const DetailsUserLoan = (pageProps: PageContentProps) => {
               <CellUserMain {...cellProps} type="borrow" />
 
               {/* stats */}
-              <Box>
-                {stats.map((detailSection) => {
-                  return (
-                    <ContentStats key={detailSection[0].title}>
-                      {detailSection.map(({ className = '', title, value, show }) => {
-                        return (
-                          _showContent(show) && (
-                            <ContentStat className={className} key={`detail-${title}`}>
-                              <ContentStatTitle>{title}</ContentStatTitle>
-                              <ContentStatValue>{value}</ContentStatValue>
-                            </ContentStat>
-                          )
-                        )
-                      })}
-                    </ContentStats>
-                  )
-                })}
-              </Box>
+              <ListInfoItemsWrapper>
+                {stats.map((detailSection, idx) => (
+                  <ListInfoItems key={`info-${idx}`}>
+                    {detailSection.map(({ value, show, ...props }, idx) => {
+                      if (!_showContent(show)) return null
+                      return (
+                        <ListInfoItem key={`info-${idx}`} {...props}>
+                          {value}
+                        </ListInfoItem>
+                      )
+                    })}
+                  </ListInfoItems>
+                ))}
+              </ListInfoItemsWrapper>
             </StatsWrapper>
           </Content>
 
@@ -122,7 +147,7 @@ const DetailsUserLoan = (pageProps: PageContentProps) => {
               <DetailsUserLoanChartLiquidationRange {...pageProps} />
             )}
           </Content>
-        </Wrapper>
+        </div>
       ) : (
         <Content paddingTop>
           <AlertNoLoanFound alertType="" owmId={rOwmId} />
@@ -136,11 +161,6 @@ const AlertContent = styled(Content)`
   & {
     padding-top: var(--spacing-normal);
     padding-bottom: 0;
-  }
-`
-
-const Wrapper = styled.div`
-  @media (min-width: ${breakpoints.sm}rem) {
   }
 `
 
