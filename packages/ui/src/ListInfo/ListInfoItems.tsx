4d90fa0bdc09c292e195b6432a347241b46888a5
Amy Tsang <amy0tsang@gmail.com>
2024-08-08 13:40:54 -0700

    refactor: add ListInfo to UI

---
+++ packages/ui/src/ListInfo/ListInfoItems.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const ListInfoItems = styled.ul`
+  > li:not(:last-of-type) {
+    margin-right: var(--spacing-wide);
+  }
+`
+
+export default ListInfoItems
