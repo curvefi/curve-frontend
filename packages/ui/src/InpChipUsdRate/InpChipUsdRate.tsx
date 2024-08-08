be8973905bf7f5f967744b8e7c316894b8973a6e
Amy Tsang <amy0tsang@gmail.com>
2024-08-08 13:41:33 -0700

    refactor: add InpChipUsdRate to UI

---
+++ packages/ui/src/InpChipUsdRate/InpChipUsdRate.tsx
@@ -0,0 +1,46 @@
+import type { ChipProps } from 'ui/src/Typography/types'
+
+import React, { useMemo } from 'react'
+import styled from 'styled-components'
+
+import { FORMAT_OPTIONS, formatNumber } from 'ui/src/utils'
+import Chip from 'ui/src/Typography/Chip'
+import TextCaption from 'ui/src/TextCaption'
+
+export type InpChipUsdRateProps = ChipProps & {
+  className?: string
+  amount: string | undefined
+  hideRate?: boolean
+  usdRate: string | number | undefined
+}
+
+const InpChipUsdRate = ({ className = '', amount, hideRate, usdRate, ...props }: InpChipUsdRateProps) => {
+  const amountUsd = useMemo(() => {
+    if (typeof usdRate === 'undefined' || typeof amount === 'undefined') return undefined
+    return +amount * +usdRate
+  }, [usdRate, amount])
+
+  const formattedAmountUsd = formatNumber(amountUsd, { ...FORMAT_OPTIONS.USD, defaultValue: '-' })
+
+  return (
+    <>
+      {hideRate ? (
+        <TextCaption className={className} {...props}>
+          {formattedAmountUsd}
+        </TextCaption>
+      ) : (
+        <StyledInpChip className={className} size="xs">
+          x {formatNumber(usdRate)} ≈{formattedAmountUsd}
+        </StyledInpChip>
+      )}
+    </>
+  )
+}
+
+const StyledInpChip = styled(Chip)<{ noPadding?: boolean }>`
+  padding: ${({ noPadding }) => (noPadding ? '0' : '0 0.3125rem')}; // 5px
+  min-height: 0.875rem; // 14px
+  opacity: 0.9;
+`
+
+export default InpChipUsdRate
