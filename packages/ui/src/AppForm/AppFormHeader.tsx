55af32cf6d0e942b49cf9b4accbf0e9225d075e6
Ignat <ign.romanov@gmail.com>
2024-08-14 16:33:42 +0300

    feat: implemented sandwich menu

--- packages/ui/src/AppForm/AppFormHeader.tsx
+++ packages/ui/src/AppForm/AppFormHeader.tsx
@@ -1,17 +1,20 @@
 import styled from 'styled-components'
 
-import { breakpoints } from 'ui/src/utils'
 import IconButton from 'ui/src/IconButton'
 import Tabs, { Tab } from 'ui/src/Tab'
+import { breakpoints } from 'ui/src/utils'
+import SandwichMenu from './ui/SandwichMenu'
 
 const AppFormHeader = ({
   formTypes,
   activeFormKey,
   handleClick,
+  showMenuButton = false,
 }: {
   formTypes: { key: string; label: string }[]
   activeFormKey: string
   handleClick(formKey: string): void
+  showMenuButton?: boolean
 }) => {
   return (
     <Header>
@@ -22,19 +25,22 @@ const AppFormHeader = ({
         </>
       ) : (
         <Tabs>
-          {formTypes.map(({ key, label }) => {
-            const isActiveForm = !activeFormKey && key === activeFormKey
-            return (
-              <StyledTab
-                key={key}
-                className={isActiveForm ? 'active' : activeFormKey === key ? 'active' : ''}
-                disabled={isActiveForm || activeFormKey === key}
-                onClick={() => handleClick(key)}
-              >
-                {label}
-              </StyledTab>
-            )
-          })}
+          <>
+            {formTypes.map(({ key, label }) => {
+              const isActiveForm = !activeFormKey && key === activeFormKey
+              return (
+                <StyledTab
+                  key={key}
+                  className={isActiveForm ? 'active' : activeFormKey === key ? 'active' : ''}
+                  disabled={isActiveForm || activeFormKey === key}
+                  onClick={() => handleClick(key)}
+                >
+                  {label}
+                </StyledTab>
+              )
+            })}
+            {showMenuButton && <SandwichMenu onItemClick={handleClick} />}
+          </>
         </Tabs>
       )}
       <IconButton hidden />
