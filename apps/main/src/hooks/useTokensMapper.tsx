2da5492069da8ba84bd1bd4cafe18615654f4e27
Ignat <ign.romanov@gmail.com>
2024-08-23 15:00:15 +0300

    fix: useTokensMapper bug

--- apps/main/src/hooks/useTokensMapper.tsx
+++ apps/main/src/hooks/useTokensMapper.tsx
@@ -7,10 +7,7 @@ import { useChainId } from '@/entities/chain'
 const useTokensMapper = (rChainId?: ChainId) => {
   const { data: chainId } = useChainId()
   const _rChainId = rChainId ?? chainId
-  // const cached = useStore((state) => state.storeCache.tokensMapper[_rChainId])
-  const api = useStore((state) => state.tokens.tokensMapper[_rChainId])
-  // const tokensMapper = useMemo(() => api ?? cached ?? {}, [api, cached])
-  const tokensMapper = useMemo(() => api ?? {}, [api])
+  const tokensMapper = useStore((state) => state.tokens.tokensMapper[_rChainId] ?? {})
   const tokensMapperStr = useMemo(() => getTokensMapperStr(tokensMapper), [tokensMapper])
   return { tokensMapper, tokensMapperStr }
 }
