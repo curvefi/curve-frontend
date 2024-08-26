95d327db71ca2fd6f0e97c4cc1e770945a4ad73a
Ignat <ign.romanov@gmail.com>
2024-08-26 11:33:42 +0300

    chore: remove unused fn

--- apps/main/src/entities/gauge/lib/extract-params.ts
+++
@@ -1,6 +0,0 @@
-import { extractQueryParams } from '@/shared/lib/queries/extract-params'
-import type { QueryKey } from '@tanstack/react-query'
-
-export function extractGaugeParams<T extends QueryKey>(queryKey: T, skipCount = 4) {
-  return extractQueryParams(queryKey, skipCount)
-}
--- apps/main/src/shared/lib/queries/extract-params.ts
+++
@@ -1,5 +0,0 @@
-import type { QueryKey } from '@tanstack/react-query'
-
-export function extractQueryParams<T extends QueryKey>(queryKey: T, skipCount: number): T[number][] {
-  return queryKey.slice(skipCount)
-}
