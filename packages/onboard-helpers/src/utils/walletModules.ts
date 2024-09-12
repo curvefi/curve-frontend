2cd332df59d3313352acf95fe7bba0527a8833d9
Ignat <ign.romanov@gmail.com>
2024-09-12 22:42:08 +0500

    fix: remove `portis` wallet

--- packages/onboard-helpers/src/utils/walletModules.ts
+++ packages/onboard-helpers/src/utils/walletModules.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import injectedModule from '@web3-onboard/injected-wallets'
 import trezorModule from '@web3-onboard/trezor'
 import ledgerModule from '@web3-onboard/ledger'
@@ -6,7 +5,6 @@ import gnosisModule from '@web3-onboard/gnosis'
 import coinbaseWalletModule from '@web3-onboard/coinbase'
 import fortmaticModule from '@web3-onboard/fortmatic'
 import walletConnectModule from '@web3-onboard/walletconnect'
-import portisModule from '@web3-onboard/portis'
 import torusModule from '@web3-onboard/torus'
 import phantomModule from '@web3-onboard/phantom'
 import frontierModule from '@web3-onboard/frontier'
@@ -28,13 +26,11 @@ export const gnosis = gnosisModule()
 export const coinbaseWalletSdk = coinbaseWalletModule({ darkMode: true })
 export const fortmatic = fortmaticModule({ apiKey: 'pk_live_190B10CE18F47DCD' })
 
-// see https://onboard.blocknative.com/docs/wallets/walletconnect for additional options
 export const walletConnect = walletConnectModule({
   projectId: WALLET_CONNECT_PROJECT_ID,
   dappUrl: 'https://curve.fi',
 })
 
-export const portis = portisModule({ apiKey: 'a3bb2525-5101-4a9c-b300-febc6319c3b4' })
 export const torus = torusModule()
 export const phantom = phantomModule()
 export const frontier = frontierModule()
