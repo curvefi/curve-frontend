d7ad74084116f694d62c7fe4770575e5c94e2086
mimaklas <161614979+mimaklas@users.noreply.github.com>
2024-09-14 16:55:13 +0300

    Update index.ts

--- packages/external-rewards/src/campaigns/index.ts
+++ packages/external-rewards/src/campaigns/index.ts
@@ -13,6 +13,7 @@ import YieldNest from './YieldNest.json'
 import Fraxtal from './Fraxtal.json'
 import Swell from './Swell.json';
 import Resolv from './Resolv.json';
+import Lombard from './Lombard.json';
 
 export {
   DLCLink,
@@ -29,5 +30,6 @@ export {
   YieldNest,
   Fraxtal,
   Swell,
-  Resolv
+  Resolv,
+  Lombard
 }
