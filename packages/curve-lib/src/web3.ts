7cfb6591c3a5df9f7dea2ebcb0dcaa1a4f1ccbb2
Amy Tsang <amy0tsang@gmail.com>
2024-09-19 14:32:42 -0700

    task: add waitForTxStatuses

--- packages/curve-lib/src/web3.ts
+++ packages/curve-lib/src/web3.ts
@@ -1,3 +1,7 @@
+import type { BrowserProvider } from 'ethers'
+
+import { TxHashError } from 'ui/src/TxInfoBar'
+
 export function gweiToEther(gwei: number) {
   return gwei / 1e9
 }
@@ -16,4 +20,43 @@ export function weiToGwei(wai: number) {
 
 export function getDecimalLength(val: string) {
   return val.includes('.') ? val.split('.')[1].length : 0
+}
+
+export enum TxStatus {
+  'failed' = 'failed',
+  'succeeded' = 'succeeded',
+  'pending' = 'pending',
+}
+
+export type WaitForTxReceiptResp = {
+  success: string[]
+  failed: string[]
+}
+
+export async function waitForTxStatuses(hashes: string[], provider: BrowserProvider): Promise<WaitForTxReceiptResp> {
+  const resp = (await Promise.all(hashes.map((hash) => provider.waitForTransaction(hash)))).reduce(
+    (prev, txReceipt) => {
+      if (txReceipt) {
+        const txStatus = getTxStatus(txReceipt.status)
+        if (txStatus === TxStatus.succeeded) prev.success.push(txReceipt.hash)
+        if (txStatus === TxStatus.failed) prev.failed.push(txReceipt.hash)
+      }
+      return prev
+    },
+    { success: [], failed: [] } as WaitForTxReceiptResp
+  )
+
+  if (!!resp.success.length && !!resp.failed.length) {
+    throw new TxHashError(`Some transactions statuses failed.`, resp)
+  }
+  if (resp.failed.length > 1) throw new TxHashError(`Transactions statuses failed.`, resp)
+  if (resp.failed.length === 1) throw new TxHashError('Transaction status failed.', resp)
+  return resp
+}
+
+// helpers
+function getTxStatus(status: number | null | undefined) {
+  if (status === 0) return TxStatus.failed
+  if (status === 1) return TxStatus.succeeded
+  return TxStatus.pending
 }
