e9fb72bbae29d7f582ab10dac14cf43a5d52d3d7
Amy Tsang <amy0tsang@gmail.com>
2024-09-24 07:29:26 -0700

    fix: wallet connect useEffect

--- apps/loan/src/hooks/usePageOnMount.ts
+++ apps/loan/src/hooks/usePageOnMount.ts
@@ -205,11 +205,10 @@ function usePageOnMount(params: Params, location: Location, navigate: NavigateFu
   useEffect(() => {
     if (
       (isSuccess(connectState) || isFailure(connectState)) &&
-      !!curve &&
-      !!walletChainId &&
-      (curve.chainId !== walletChainId || curve.signerAddress?.toLowerCase() !== walletSignerAddress?.toLowerCase())
+      (!!walletChainId || !!walletChainId || !!curve) &&
+      (curve?.chainId !== walletChainId || curve?.signerAddress?.toLowerCase() !== walletSignerAddress?.toLowerCase())
     ) {
-      if (curve.signerAddress.toLowerCase() !== walletSignerAddress?.toLowerCase()) {
+      if (curve?.signerAddress.toLowerCase() !== walletSignerAddress?.toLowerCase()) {
         updateConnectState('loading', CONNECT_STAGE.CONNECT_API, [walletChainId, true])
       } else if (curve?.chainId !== walletChainId) {
         const foundNetwork = networks[walletChainId as ChainId]?.id
@@ -222,7 +221,7 @@ function usePageOnMount(params: Params, location: Location, navigate: NavigateFu
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [walletChainId, walletSignerAddress])
+  }, [connectState.status, walletChainId, walletSignerAddress])
 
   // locale switched
   useEffect(() => {
