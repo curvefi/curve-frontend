4b69aae9eff2531738c5206dd0b8608064777b69
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-10-08 15:04:25 +0200

    refactor: create generic `createGetQueryData`

--- apps/lend/src/entities/token/lib/query-data.ts
+++ apps/lend/src/entities/token/lib/query-data.ts
@@ -1,6 +1,5 @@
 import { tokenKeys } from '../model'
-import { queryClient } from '@/shared/api/query-client'
-import { TokenKey } from '@/entities/token'
+import { createGetQueryData } from '@/shared/lib/queries'
 
-export const getTokenQueryData = <Result, Key extends TokenKey = TokenKey>(key: TokenKey, keyData: Parameters<typeof tokenKeys[Key]>[0]) =>
-  queryClient.getQueryData<Result>(tokenKeys[key](keyData))
+
+export const getTokenQueryData = createGetQueryData(tokenKeys)
