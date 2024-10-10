b926aeb78b76a8bc67a478d740637e0e18914d58
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-10-10 11:22:03 +0200

    chore: update fds rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,27 +4,32 @@ module.exports = {
   extends: ['custom'],
   settings: {
     next: {
-      rootDir: ['apps/*/', 'packages/ui/*/', 'packages/onboard-helpers/*/'],
-    },
+      rootDir: ['apps/*/', 'packages/ui/*/', 'packages/onboard-helpers/*/']
+    }
   },
-  overrides: [{
-    files: [
-      'apps/{loan,lend}/src/{entities,features}/**/*.{ts,tsx}',
-      "packages/curve-lib/src/shared/**/*.ts",
-    ],
-    rules: {
-      'import/order': 'error', // feature-sliced/import-order
-      'import/no-internal-modules': 'error', // feature-sliced/public-api
-      'boundaries/element-types': 'error' // feature-sliced/layers-slices
+  overrides: [
+    // enforce Feature Sliced rules for loan app
+    {
+      files: [
+        'apps/loan/src/{app,processes,pages,widgets,features,entities,shared}/**/*.{ts,tsx}',
+        'packages/curve-lib/src/shared/**/*.ts'
+      ],
+      rules: {
+        'import/order': 'error', // feature-sliced/import-order
+        'import/no-internal-modules': 'error', // feature-sliced/public-api
+        'boundaries/element-types': 'error' // feature-sliced/layers-slices
+      }
     },
-  }, {
-    files: [
-      'apps/main/src/{entities,features}/**/*.{ts,tsx}'
-    ],
-    rules: {
-      'import/order': 'warn', // feature-sliced/import-order
-      'import/no-internal-modules': 'warn', // feature-sliced/public-api
-      'boundaries/element-types': 'warn' // feature-sliced/layers-slices
-    },
-  }],
+    // warn about Feature Sliced rules for main and lend apps
+    {
+      files: [
+        'apps/{main,lend}/src/{app,processes,pages,widgets,features,entities,shared}/**/*.{ts,tsx}'
+      ],
+      rules: {
+        'import/order': 'warn', // feature-sliced/import-order
+        'import/no-internal-modules': 'warn', // feature-sliced/public-api
+        'boundaries/element-types': 'warn' // feature-sliced/layers-slices
+      }
+    }
+  ]
 }
