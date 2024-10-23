47bd425f3dbbaaf0095a14787521744a898eb448
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-10-23 15:17:39 +0200

    fix: eslintrc
    Co-authored-by: Ignat Romanov <ign.romanov@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   extends: ['custom'],
   settings: {
     next: {
-      rootDir: ['apps/*/', 'packages/ui/*/', 'packages/features/*/']
+      rootDir: ['apps/*/', 'packages/ui/*/', 'packages/curve-features/*/']
     }
   },
   overrides: [
