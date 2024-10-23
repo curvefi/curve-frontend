03da037258395679950d6914b3fb0999740c049a
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-10-23 13:19:53 +0200

    fix: hotfix validation
    Better fix will be in a separate PR (commit 1dd4f4fb0)

--- apps/lend/src/entities/token/lib/validation.ts
+++ apps/lend/src/entities/token/lib/validation.ts
@@ -1,6 +1,6 @@
 import { CombinedTokenParams } from '@/entities/token'
-import { assertValidity, checkValidity, ValidatedData } from '@/shared/lib/validation'
 import { tokenValidationSuite } from '@/entities/token/model/validation'
+import { assertValidity, checkValidity } from '@/shared/lib/validation'
 
 export const assertTokenValidity = <T extends CombinedTokenParams>(data: T, fields?: Extract<keyof T, string>[]) =>
   assertValidity(tokenValidationSuite, data, fields)
