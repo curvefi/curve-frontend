f3b99b3bd72df6316b3a21874805c38293b12ac6
JustJousting <justjousting2moon@gmail.com>
2024-11-11 22:19:01 +0200

    task: update copy

--- apps/loan/src/components/PageCrvUsdStaking/StatsBanner/index.tsx
+++ apps/loan/src/components/PageCrvUsdStaking/StatsBanner/index.tsx
@@ -7,6 +7,7 @@ import { formatNumber } from '@/ui/utils'
 
 import Box from '@/ui/Box'
 import Loader from '@/ui/Loader'
+import Tooltip from '@/ui/Tooltip'
 
 type StatsBannerProps = {
   className?: string
@@ -28,11 +29,18 @@ const StatsBanner: React.FC<StatsBannerProps> = ({ className }) => {
     <Wrapper className={className}>
       <Box>
         <Title>{t`SCRVUSD IS CURVE’S YIELD-BEARING STABLECOIN`}</Title>
-        <Description>{t`With $100k of scrvUSD held you could get.`}</Description>
+        <Description>{t`With $100k of scrvUSD held you could get`}</Description>
       </Box>
       <StatsRow>
         <StatsItem>
-          <StatsItemTitle>{t`30 Days Projection`}</StatsItemTitle>
+          <StatsTitleWrapper>
+            <StatsItemTitle>{t`30 Days Projection`}</StatsItemTitle>
+            <Tooltip
+              showIcon
+              minWidth="250px"
+              tooltip={t`This is an indicator based on the historical yield of the crvUSD Savings Vault. It does not guarantee any future yield.`}
+            />
+          </StatsTitleWrapper>
           {isLoadingPricesYieldData ? (
             <Loader isLightBg skeleton={[120, 26]} />
           ) : (
@@ -40,7 +48,14 @@ const StatsBanner: React.FC<StatsBannerProps> = ({ className }) => {
           )}
         </StatsItem>
         <StatsItem>
-          <StatsItemTitle>{t`1 Year Projection`}</StatsItemTitle>
+          <StatsTitleWrapper>
+            <StatsItemTitle>{t`1 Year Projection`}</StatsItemTitle>
+            <Tooltip
+              showIcon
+              minWidth="250px"
+              tooltip={t`This is an indicator based on the historical yield of the crvUSD Savings Vault. It does not guarantee any future yield.`}
+            />
+          </StatsTitleWrapper>
           {isLoadingPricesYieldData ? (
             <Loader isLightBg skeleton={[120, 26]} />
           ) : (
@@ -48,7 +63,15 @@ const StatsBanner: React.FC<StatsBannerProps> = ({ className }) => {
           )}
         </StatsItem>
         <StatsItem>
-          <StatsItemTitle>{t`scrvUSD APY`}</StatsItemTitle>
+          <StatsTitleWrapper>
+            <StatsItemTitle>{t`Estimated APY`}</StatsItemTitle>
+            <Tooltip
+              showIcon
+              minWidth="250px"
+              tooltip={t`Annual percentage yield (APY) refers to how much interest is distributed on savings and takes compounded interest into account. 
+This value is an indicator based on the historical yield of the crvUSD Savings Vault. It does not guarantee any future yield.`}
+            />
+          </StatsTitleWrapper>
           {isLoadingPricesYieldData ? (
             <Loader isLightBg skeleton={[120, 26]} />
           ) : (
@@ -69,6 +92,15 @@ const Wrapper = styled.div`
   padding: var(--spacing-3);
   background-color: #d4f7e3;
   border: 1px solid #1fa25e;
+`
+
+const StatsTitleWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: baseline;
+  @media (min-width: 26.625rem) {
+    gap: var(--spacing-1);
+  }
 `
 
 const Title = styled.h3`
