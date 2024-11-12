81cf66bb7bda5d52f4d2dce9be4a5608d013c383
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-11-12 14:10:38 +0100

    chore: revert ui package change

--- packages/ui/src/Select/SelectNetwork.tsx
+++ packages/ui/src/Select/SelectNetwork.tsx
@@ -28,29 +28,36 @@ export const SelectNetwork: React.FC<SelectNetworkProps> = ({
   isDarkTheme,
   items,
   ...props
-}) => (
-  <Select {...props} className={className} items={items} aria-label="Select network" label="">
-    {({ chainId, src, srcDark, label }) => {
-      return (
-        <Item key={chainId} textValue={chainId.toString()}>
-          {!hideIcon && (
-            <IconWrapper>
-              <Image
-                alt={label}
-                // onError={(evt) => setTimeout(() => handleOnError(evt, src), 0)}
-                src={isDarkTheme ? srcDark : src}
-                loading="lazy"
-                width="18"
-                height="18"
-              />
-            </IconWrapper>
-          )}
-          <strong>{label}</strong>
-        </Item>
-      )
-    }}
-  </Select>
-)
+}) => {
+  // todo: defaultSrc should be used when the image is not found, but it's not working yet
+  const handleOnError = (evt: React.SyntheticEvent<HTMLImageElement, Event>, defaultSrc: string) => {
+    ;(evt.target as HTMLImageElement).src = defaultSrc
+  }
+
+  return (
+    <Select {...props} className={className} items={items} aria-label="Select network" label="">
+      {({ chainId, src, srcDark, label }) => {
+        return (
+          <Item key={chainId} textValue={chainId.toString()}>
+            {!hideIcon && (
+              <IconWrapper>
+                <Image
+                  alt={label}
+                  // onError={(evt) => setTimeout(() => handleOnError(evt, src), 0)}
+                  src={isDarkTheme ? srcDark : src}
+                  loading="lazy"
+                  width="18"
+                  height="18"
+                />
+              </IconWrapper>
+            )}
+            <strong>{label}</strong>
+          </Item>
+        )
+      }}
+    </Select>
+  )
+}
 
 SelectNetwork.displayName = 'SelectNetwork'
 const IconWrapper = styled.span`
