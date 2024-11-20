56289d245a633bcaee9f94e2563e195d87c94a06
0xtutti <0xtutti@protonmail.com>
2024-11-20 11:50:02 +0100

    Adds Minecraft font and updates typography in curve-ui-kit typography

--- packages/curve-ui-kit/src/themes/typography/fonts.ts
+++ packages/curve-ui-kit/src/themes/typography/fonts.ts
@@ -1,8 +1,18 @@
 import localFont from 'next/font/local'
 
 export const monaSans = localFont({ src: '../../../public/fonts/Mona-Sans.woff2' })
-export const minecraft = localFont({ src: '../../../public/fonts/Minecraft.woff2' })
-
+export const minecraft = localFont({
+  src: [
+    {
+      path: '../../../public/fonts/Minecraft-Regular.otf',
+      weight: '400',
+    },
+    {
+      path: '../../../public/fonts/Minecraft-Bold.otf',
+      weight: '700',
+    },
+  ],
+})
 export const FontFamilyMapping = {
   'Mona Sans': [monaSans.style.fontFamily, '"Helvetica Neue"', 'sans-serif'].join(','),
   'Minecraft': [minecraft.style.fontFamily, '"SF Mono Regular 11"', '"Ubuntu Mono"', 'monospace'].join(','),
