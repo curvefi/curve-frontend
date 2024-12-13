a8cd1a8e018f30233030e8f66b41cf3c1b2f9d8a
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-12-13 11:07:27 +0100

    chore: dont retrieve snapshots yet

--- apps/loan/src/components/PageLlamaMarkets/cells/BorrowRateCell.tsx
+++ apps/loan/src/components/PageLlamaMarkets/cells/BorrowRateCell.tsx
@@ -1,10 +1,9 @@
 import { LendingVaultFromApi } from '@/entities/vaults'
-import { useLendingSnapshots } from '@/entities/lending'
 
 export const BorrowRateCell = ({ data }: { data: LendingVaultFromApi }) => {
-  const { data: snapshots } = useLendingSnapshots({
-    blockchainId: data.blockchainId,
-    contractAddress: data.controllerAddress,
-  })
+  // const { data: snapshots } = useLendingSnapshots({
+  //   blockchainId: data.blockchainId,
+  //   contractAddress: data.controllerAddress,
+  // })
   return data.rates.borrowApyPcent.toPrecision(4) + '%'
 }
