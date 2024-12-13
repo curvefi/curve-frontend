0b6e2ced23e930f06f629ea9f6ba07a51fcf0d7a
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-12-13 16:55:30 +0100

    fix: missing slash

--- packages/curve-ui-kit/src/shared/routes.ts
+++ packages/curve-ui-kit/src/shared/routes.ts
@@ -91,5 +91,5 @@ function getAppRoot(productionHost: string, previewPrefix: string, developmentPo
   return `https://${productionHost}`
 }
 
-export const externalAppUrl = (route: string, networkName: string, app: AppName) =>
-  app ? `${APP_LINK[app].root}/#${networkName ? networkName + '/' : ''}${route}` : `/#${route}`
+export const externalAppUrl = (route: string, networkName: string | null, app: AppName) =>
+  app ? `${APP_LINK[app].root}/#${networkName ? `/${networkName}` : ''}${route}` : `/#${route}`
