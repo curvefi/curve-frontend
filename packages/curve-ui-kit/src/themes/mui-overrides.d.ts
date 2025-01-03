0ff91e93bd6fc3b2e648f921d40915150cf9ce15
Alunara <>
2025-01-03 19:12:57 +0100

    feat: add toggle button styling

--- packages/curve-ui-kit/src/themes/mui-overrides.d.ts
+++ packages/curve-ui-kit/src/themes/mui-overrides.d.ts
@@ -14,27 +14,3 @@ declare module '@mui/material/styles' {
     highlight: string
   }
 }
-
-declare module '@mui/material/Button' {
-  type Buttons = Omit<DesignSystem['Button'], 'Focus_Outline'>
-  type ButtonColors = {
-    [K in keyof Buttons as Lowercase<string & K>]: true
-  }
-
-  export interface ButtonPropsColorOverrides extends ButtonColors {}
-
-  export interface ButtonPropsSizeOverrides {
-    extraSmall: true
-  }
-
-  export interface ButtonClasses {
-    sizeExtraSmall: string
-  }
-
-  export interface ButtonPropsVariantOverrides {
-    text: true
-    contained: true
-    outlined: true
-    link: true
-  }
-}
