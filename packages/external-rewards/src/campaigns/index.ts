98e9e942a1d4366414af6539168e1483f8bc47a0
mimaklas <161614979+mimaklas@users.noreply.github.com>
2025-01-08 13:02:44 +0200

    Update index.ts

--- packages/external-rewards/src/campaigns/index.ts
+++ packages/external-rewards/src/campaigns/index.ts
@@ -24,6 +24,7 @@ import MindNetwork from './MindNetwork.json'
 import DTrinity from './DTrinity.json'
 import YieldFi from './YieldFi.json'
 import Babylon from './Babylon.json'
+import Vnx from './Vnx.json'
 
 export {
   DLCLink,
@@ -52,4 +53,5 @@ export {
   DTrinity,
   YieldFi,
   Babylon,
+  Vnx,
 }
