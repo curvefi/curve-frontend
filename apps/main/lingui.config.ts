9fe048623cff3eb4867a95b92308dcd3aa154c17
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-01-17 15:45:25 +0100

    fix: lingui config for multiple apps

--- apps/main/lingui.config.ts
+++ apps/main/lingui.config.ts
@@ -1,1 +1,23 @@
-export { config as default } from '../../lingui.config'
+import { config } from '../../lingui.config'
+
+const mainConfig = {
+  ...config,
+  catalog: {
+    path: 'src/locales/{locale}/messages',
+    include: [
+      ...['dao', 'dex', 'loan', 'lend'].flatMap((app) => [
+        `src/${app}/components`,
+        `src/${app}/entities`,
+        `src/${app}/features`,
+        `src/${app}/hooks`,
+        `src/${app}/layout`,
+        `src/${app}/pages`,
+        `src/${app}/store`,
+        `src/${app}/widgets`,
+      ]),
+      '../../packages/curve-ui-kit',
+    ],
+  },
+}
+
+export default mainConfig
