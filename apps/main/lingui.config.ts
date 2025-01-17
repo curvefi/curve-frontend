527e99d25c6ef92ee76da7c2cb3587c2140e9ba6
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-01-17 15:57:50 +0100

    fix: cypress script, lingui config

--- apps/main/lingui.config.ts
+++ apps/main/lingui.config.ts
@@ -2,22 +2,24 @@ import { config } from '../../lingui.config'
 
 const mainConfig = {
   ...config,
-  catalog: {
-    path: 'src/locales/{locale}/messages',
-    include: [
-      ...['dao', 'dex', 'loan', 'lend'].flatMap((app) => [
-        `src/${app}/components`,
-        `src/${app}/entities`,
-        `src/${app}/features`,
-        `src/${app}/hooks`,
-        `src/${app}/layout`,
-        `src/${app}/pages`,
-        `src/${app}/store`,
-        `src/${app}/widgets`,
-      ]),
-      '../../packages/curve-ui-kit',
-    ],
-  },
+  catalogs: [
+    {
+      path: 'src/locales/{locale}/messages',
+      include: [
+        ...['dao', 'dex', 'loan', 'lend'].flatMap((app) => [
+          `src/${app}/components`,
+          `src/${app}/entities`,
+          `src/${app}/features`,
+          `src/${app}/hooks`,
+          `src/${app}/layout`,
+          `src/${app}/pages`,
+          `src/${app}/store`,
+          `src/${app}/widgets`,
+        ]),
+        '../../packages/curve-ui-kit',
+      ],
+    },
+  ],
 }
 
 export default mainConfig
