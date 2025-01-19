67dfd0bf96399a968796121fa907850e9ef82340
JustJousting <91265399+OnlyJousting@users.noreply.github.com>
2025-01-20 00:25:03 +0200

    Line chart tooltip

--- apps/loan/src/components/PageCrvUsdStaking/Statistics/ChartTooltip.tsx
+++ apps/loan/src/components/PageCrvUsdStaking/Statistics/ChartTooltip.tsx
@@ -3,18 +3,38 @@ import { TooltipProps } from 'recharts'
 import { t } from '@lingui/macro'
 import { Paper, Stack, Typography } from '@mui/material'
 import { SizesAndSpaces } from '@ui-kit/themes/design/1_sizes_spaces'
-import { formatDateFromTimestamp, formatNumber } from '@/ui/utils/utilsFormat'
+import { formatDateFromTimestamp } from '@/ui/utils/utilsFormat'
+import { useTheme } from '@mui/material/styles'
 
 const { Spacing } = SizesAndSpaces
 
-const DataSet = ({ label, value }: { label: string; value: string }) => (
-  <Stack direction="row" justifyContent="space-between">
-    <Typography variant="bodySRegular">{label}</Typography>
+const DataSet = ({
+  label,
+  value,
+  lineColor,
+  dash,
+}: {
+  label: string
+  value: string
+  lineColor: string
+  dash?: string
+}) => (
+  <Stack direction="row" justifyContent="space-between" alignItems="center">
+    <Stack direction="row" spacing={2} alignItems="center">
+      <svg width="20" height="2">
+        <line x1="0" y1="1" x2="20" y2="1" stroke={lineColor} strokeWidth={2} strokeDasharray={dash} />
+      </svg>
+      <Typography variant="bodySRegular">{label}</Typography>
+    </Stack>
     <Typography variant="bodySBold">{value}</Typography>
   </Stack>
 )
 
 const CustomTooltip = ({ active, payload }: TooltipProps<ValueType, NameType>) => {
+  const {
+    design: { Color },
+  } = useTheme()
+
   if (active && payload && payload.length) {
     const { timestamp, proj_apy, proj_apy_7d_avg, proj_apy_total_avg } = payload[0].payload
 
@@ -34,13 +54,23 @@ const CustomTooltip = ({ active, payload }: TooltipProps<ValueType, NameType>) =
           sx={{
             marginTop: Spacing.sm,
             padding: Spacing.sm,
-            gap: 0,
+            gap: 1,
             backgroundColor: (theme) => theme.design.Layer[2].Fill,
           }}
         >
-          <DataSet label={t`Historical APR`} value={proj_apy.toFixed(2) + '%'} />
-          <DataSet label={t`7-day Average APR`} value={proj_apy_7d_avg.toFixed(2) + '%'} />
-          <DataSet label={t`Average APR`} value={proj_apy_total_avg.toFixed(2) + '%'} />
+          <DataSet label={t`APR`} value={proj_apy.toFixed(2) + '%'} lineColor={Color.Primary[500]} />
+          <DataSet
+            label={t`7-day MA APR`}
+            value={proj_apy_7d_avg.toFixed(2) + '%'}
+            lineColor={Color.Secondary[500]}
+            dash="2 2"
+          />
+          <DataSet
+            label={t`Average APR`}
+            value={proj_apy_total_avg.toFixed(2) + '%'}
+            lineColor={Color.Tertiary[400]}
+            dash="4 4"
+          />
         </Stack>
       </Paper>
     )
