5f6088b0d9e40d029a8e27996bc5e8ea8a555d90
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-01-22 12:52:12 +0100

    refactor: self-review

--- packages/curve-ui-kit/src/features/connect-wallet/lib/hooks.ts
+++ packages/curve-ui-kit/src/features/connect-wallet/lib/hooks.ts
@@ -1,13 +1,15 @@
 import { useCallback } from 'react'
 import { useConnectWallet as useOnboardWallet } from '@web3-onboard/react'
 import { useWalletStore } from '../store'
-import { useLocalStorage } from '@ui-kit/hooks/useLocalStorage'
+import { getFromLocalStorage, useLocalStorage } from '@ui-kit/hooks/useLocalStorage'
 
 export { useSetChain, useSetLocale } from '@web3-onboard/react'
 
 export const useConnectWallet = () => {
   const [{ wallet, connecting }, connectWallet, disconnectWallet] = useOnboardWallet()
-  const [walletName, setWalletName] = useLocalStorage('walletName')
+  const [storedWalletName, setWalletName] = useLocalStorage('walletName')
+  // todo: remove this after a while. It tries to read the walletName from the old cache
+  const walletName = storedWalletName || getFromLocalStorage<{ walletName: string }>('curve-app-cache')?.walletName
 
   const chooseWallet = useWalletStore((s) => s.chooseWallet)
   const connect: typeof connectWallet = useCallback(
