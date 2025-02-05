10967f5877c2cfa4df3112a2a6b20a39c734fa71
Alunara <>
2025-02-05 14:52:57 +0100

    fix: OHLC charts not showing times in local timezone

--- apps/main/src/global-extensions.ts
+++ apps/main/src/global-extensions.ts
@@ -3,6 +3,7 @@ import type { UTCTimestamp } from 'lightweight-charts'
 declare global {
   interface Date {
     getUTCTimestamp(): UTCTimestamp
+    getLocalTimestamp(): UTCTimestamp
   }
 
   interface BigInt {
@@ -12,6 +13,10 @@ declare global {
 
 Date.prototype.getUTCTimestamp = function () {
   return (this.getTime() / 1000) as UTCTimestamp
+}
+
+Date.prototype.getLocalTimestamp = function () {
+  return ((this.getTime() - this.getTimezoneOffset() * 60000) / 1000) as UTCTimestamp
 }
 
 BigInt.prototype.fromWei = function () {
