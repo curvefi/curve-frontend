6bbe59b9ee1fad00229916ddfe633cee68adea45
JustJousting <justjousting2moon@gmail.com>
2025-02-10 10:14:48 +0200

    refactor: StatsBanner to use new components/theme

--- apps/main/src/loan/components/PageCrvUsdStaking/StatsBanner/index.tsx
+++ apps/main/src/loan/components/PageCrvUsdStaking/StatsBanner/index.tsx
@@ -1,157 +1,70 @@
-import styled from 'styled-components'
 import { t } from '@lingui/macro'
 
 import useStore from '@/loan/store/useStore'
 import { isReady } from '@/loan/components/PageCrvUsdStaking/utils'
-import { formatNumber } from '@ui/utils'
 
-import Box from '@ui/Box'
-import Loader from '@ui/Loader'
-import Tooltip from '@ui/Tooltip'
-import { Sizing, Greens } from '@ui-kit/themes/design/0_primitives'
+import { useTheme } from '@mui/material/styles'
+import { Typography } from '@mui/material'
+import { Stack } from '@mui/material'
+import { Sizing } from '@ui-kit/themes/design/0_primitives'
 import { SizesAndSpaces } from '@ui-kit/themes/design/1_sizes_spaces'
+import { Metric } from '@ui-kit/shared/ui/Metric'
 
-const { MaxWidth } = SizesAndSpaces
+const { MaxWidth, Spacing } = SizesAndSpaces
 
-type StatsBannerProps = {
-  className?: string
-}
-
-const StatsBanner: React.FC<StatsBannerProps> = ({ className }) => {
+const StatsBanner = () => {
+  const {
+    design: { Color },
+  } = useTheme()
   const pricesYieldData = useStore((state) => state.scrvusd.pricesYieldData)
 
   const exampleBalance = 100000
-
   const isLoadingPricesYieldData = !isReady(pricesYieldData.fetchStatus)
-
   const scrvUsdApy = pricesYieldData.data?.proj_apr || 0
-  const oneMonthProjYield = formatNumber((scrvUsdApy / 100 / 12) * exampleBalance, {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  })
-  const oneYearProjYield = formatNumber((scrvUsdApy / 100) * exampleBalance, {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  })
+  const oneMonthProjYield = (scrvUsdApy / 100 / 12) * exampleBalance
+  const oneYearProjYield = (scrvUsdApy / 100) * exampleBalance
 
   return (
-    <Wrapper className={className}>
-      <Box>
-        <Title>{t`Your stablecoins could do more`}</Title>
-        <Description>{t`With $100k of scrvUSD held you could get`}</Description>
-      </Box>
-      <StatsRow>
-        <StatsItem>
-          <StatsTitleWrapper>
-            <StatsItemTitle>{t`30 Days Projection`}</StatsItemTitle>
-            <Tooltip
-              showIcon
-              minWidth="250px"
-              tooltip={t`This is an indicator based on the historical yield of the crvUSD Savings Vault. It does not guarantee any future yield.`}
-            />
-          </StatsTitleWrapper>
-          {isLoadingPricesYieldData ? (
-            <Loader isLightBg skeleton={[120, 26]} />
-          ) : (
-            <StatsItemValue>~${oneMonthProjYield}</StatsItemValue>
-          )}
-        </StatsItem>
-        <StatsItem>
-          <StatsTitleWrapper>
-            <StatsItemTitle>{t`1 Year Projection`}</StatsItemTitle>
-            <Tooltip
-              showIcon
-              minWidth="250px"
-              tooltip={t`This is an indicator based on the historical yield of the crvUSD Savings Vault. It does not guarantee any future yield.`}
-            />
-          </StatsTitleWrapper>
-          {isLoadingPricesYieldData ? (
-            <Loader isLightBg skeleton={[120, 26]} />
-          ) : (
-            <StatsItemValue>~${oneYearProjYield}</StatsItemValue>
-          )}
-        </StatsItem>
-        <StatsItem>
-          <StatsTitleWrapper>
-            <StatsItemTitle>{t`Estimated APY`}</StatsItemTitle>
-            <Tooltip
-              showIcon
-              minWidth="250px"
-              tooltip={t`Annual percentage yield (APY) refers to how much interest is distributed on savings and takes compounded interest into account. 
+    <Stack
+      direction="column"
+      gap={Spacing.md}
+      padding={Spacing.lg}
+      sx={{
+        backgroundColor: Color.Secondary[100],
+        border: `1px solid ${Color.Secondary[500]}`,
+        maxWidth: `calc(${MaxWidth.actionCard} + ${Sizing[200]} + ${MaxWidth.section})`, // action card + gap + section
+      }}
+    >
+      <Stack direction="column">
+        <Typography variant="headingSBold">{t`Your stablecoins could do more`}</Typography>
+        <Typography variant="bodyMRegular">{t`With $100k of scrvUSD held you could get`}</Typography>
+      </Stack>
+      <Stack direction="row" gap={Sizing[200]} justifyContent="space-between">
+        <Metric
+          label={t`30 Days Projection`}
+          unit="dollar"
+          value={oneMonthProjYield}
+          loading={isLoadingPricesYieldData}
+          tooltip={t`This is an indicator based on the historical yield of the crvUSD Savings Vault. It does not guarantee any future yield.`}
+        />
+        <Metric
+          label={t`1 Year Projection`}
+          unit="dollar"
+          value={oneYearProjYield}
+          loading={isLoadingPricesYieldData}
+          tooltip={t`This is an indicator based on the historical yield of the crvUSD Savings Vault. It does not guarantee any future yield.`}
+        />
+        <Metric
+          label={t`Estimated APY`}
+          unit="percentage"
+          value={scrvUsdApy}
+          loading={isLoadingPricesYieldData}
+          tooltip={t`Annual percentage yield (APY) refers to how much interest is distributed on savings and takes compounded interest into account. 
 This value is an indicator based on the historical yield of the crvUSD Savings Vault. It does not guarantee any future yield.`}
-            />
-          </StatsTitleWrapper>
-          {isLoadingPricesYieldData ? (
-            <Loader isLightBg skeleton={[120, 26]} />
-          ) : (
-            <StatsItemValue>
-              ~{formatNumber(scrvUsdApy, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}%
-            </StatsItemValue>
-          )}
-        </StatsItem>
-      </StatsRow>
-    </Wrapper>
+        />
+      </Stack>
+    </Stack>
   )
 }
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: var(--spacing-3);
-  padding: var(--spacing-3);
-  margin: 0 auto;
-  background-color: ${Greens[100]};
-  border: 1px solid ${Greens[500]};
-  align-self: flex-start;
-  width: 100%;
-  max-width: calc(${MaxWidth.actionCard} + ${Sizing[200]} + ${MaxWidth.section}); // action card + gap + section
-`
-
-const StatsTitleWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: baseline;
-  @media (min-width: 26.625rem) {
-    gap: var(--spacing-1);
-  }
-`
-
-const Title = styled.h3`
-  font-size: var(--font-size-5);
-  color: var(--black-100);
-`
-
-const Description = styled.p`
-  color: var(--black-100);
-  span {
-    font-weight: bold;
-  }
-`
-
-const StatsRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  gap: var(--spacing-3);
-  flex-wrap: wrap;
-  color: var(--black-100);
-`
-
-const StatsItem = styled.div`
-  display: flex;
-  margin-right: auto;
-  flex-direction: column;
-  min-width: 240px;
-`
-
-const StatsItemTitle = styled.h5`
-  font-size: var(--font-size-1);
-  opacity: 0.5;
-`
-
-const StatsItemValue = styled.p`
-  font-size: var(--font-size-5);
-  font-weight: bold;
-`
 
 export default StatsBanner
