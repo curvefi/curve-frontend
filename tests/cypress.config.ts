1793d2a0d11e3104e6902e829eb33981d40dbb77
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-02-25 10:26:34 +0100

    chore: different browsers

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   defaultCommandTimeout: 5000,
-  pageLoadTimeout: 30000, // todo: for some reason the page load is slow even with pre-building
+  pageLoadTimeout: 60000, // todo: for some reason the page load is slow even with pre-building
   retries: { runMode: 0, openMode: 0 },
   video: true,
   videosFolder: 'cypress/screenshots',
