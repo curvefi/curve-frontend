c750de86c641c57387d14e0c2fa39fbb358399ea
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-02-25 12:07:57 +0100

    chore: run tests with production build

--- tests/cypress.config.ts
+++ tests/cypress.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from 'cypress'
 
-// the app can hang sometimes during the CI run, with error [HMR] Invalid message: {"action":"appIsrManifest","data":{}} TypeError: Cannot read properties of undefined (reading 'pathname')
-// todo: disable HMR during tests
-const retries = { runMode: 2, openMode: 0 }
-
 export default defineConfig({
   defaultCommandTimeout: 5000,
   pageLoadTimeout: 20000,
