7340153c25385693ccf46c86cf57158429258868
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-02-26 11:10:32 +0100

    fix: use nth-of-type
    fixes the error: The pseudo class ":nth-child" is potentially unsafe when doing server-side rendering.
    https://github.com/emotion-js/emotion/issues/1059#issuecomment-444566635

--- packages/ui/src/Spinner/Spinner.tsx
+++ packages/ui/src/Spinner/Spinner.tsx
@@ -49,13 +49,13 @@ const StyledSpinner = styled.div<StyledSpinnerProps>`
     border-color: ${({ isDisabled }) => getSpinnerColor(isDisabled)} transparent transparent transparent;
   }
 
-  div:nth-child(1) {
+  div:nth-of-type(1) {
     animation-delay: -0.45s;
   }
-  div:nth-child(2) {
+  div:nth-of-type(2) {
     animation-delay: -0.3s;
   }
-  div:nth-child(3) {
+  div:nth-of-type(3) {
     animation-delay: -0.15s;
   }
 `
