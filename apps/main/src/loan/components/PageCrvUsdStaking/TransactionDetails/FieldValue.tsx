f15f014f7adbec267113b7382f501964804c4f5e
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-02-26 11:30:14 +0100

    fix: typecheck

--- apps/main/src/loan/components/PageCrvUsdStaking/TransactionDetails/FieldValue.tsx
+++ apps/main/src/loan/components/PageCrvUsdStaking/TransactionDetails/FieldValue.tsx
@@ -19,7 +19,7 @@ type FieldValueProps = {
   }
 }
 
-const FieldValue = ({ value, fetchStatus, gas = null, symbol = null }: FieldValueProps) => {
+const FieldValue = ({ value, fetchStatus, gas, symbol }: FieldValueProps) => {
   if (gas) {
     return (
       <TransactionFieldValue>
@@ -47,7 +47,7 @@ const FieldValue = ({ value, fetchStatus, gas = null, symbol = null }: FieldValu
 
   return (
     <TransactionFieldValue>
-      {formatNumber(value, { minimumFractionDigits: 2, maximumFractionDigits: 4 })} {symbol && symbol}
+      {formatNumber(value, { minimumFractionDigits: 2, maximumFractionDigits: 4 })} {symbol}
     </TransactionFieldValue>
   )
 }
