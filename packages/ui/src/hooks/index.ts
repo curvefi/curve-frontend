b453b83b438dff241ea40757bb007536add0815b
Alunara <>
2025-03-07 00:42:55 +0100

    refactor: move useHeightResizeObserver to ui-kit, add docs and options parameter

--- packages/ui/src/hooks/index.ts
+++ packages/ui/src/hooks/index.ts
@@ -1,4 +1,3 @@
-export { default as useHeightResizeObserver } from './useHeightResizeObserver'
 export { default as useIntersectionObserver } from './useIntersectionObserver'
 export { default as usePageVisibleInterval } from './usePageVisibleInterval'
 export { default as useSlideTabState } from './useSlideTabState'
