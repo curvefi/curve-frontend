06fdfaedf64530c9039aa7ed522f8644a27bde11
JustJousting <justjousting2moon@gmail.com>
2025-03-12 18:27:50 +0200

    task: add campaign rewards for crvusd mint markets

---
+++ apps/main/src/loan/components/CampaignRewardsRow.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+import CampaignRewardsComp from 'ui/src/CampaignRewards/CampaignRewardsComp'
+import type { RewardsPool } from 'ui/src/CampaignRewards/types'
+
+interface Props {
+  rewardItems: RewardsPool[]
+  mobile?: boolean
+}
+
+const CampaignRewardsRow = ({ rewardItems, mobile = false }: Props) => (
+  <Container mobile={mobile}>
+    {rewardItems.map((rewardItem: RewardsPool, index: number) => (
+      <CampaignRewardsComp
+        key={`${rewardItem.platform}-${rewardItem.description}-${index}`}
+        rewardsPool={rewardItem}
+        mobile={mobile}
+      />
+    ))}
+  </Container>
+)
+
+const Container = styled.div<{ mobile: boolean }>`
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 12rem;
+  gap: var(--spacing-1);
+  margin-left: var(--spacing-2);
+`
+
+export default CampaignRewardsRow
