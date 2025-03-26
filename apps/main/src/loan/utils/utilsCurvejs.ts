925509500165876c85a30b033219d323fbdd60cb
Alunara <>
2025-03-26 18:15:18 +0100

    fix: chainID was 0 for stablecoin api

--- apps/main/src/loan/utils/utilsCurvejs.ts
+++ apps/main/src/loan/utils/utilsCurvejs.ts
@@ -18,6 +18,8 @@ export async function initStableJs(chainId: ChainId, wallet: Wallet): Promise<Cu
   const { networkId } = networks[chainId]
   const api = cloneDeep((await import('@curvefi/stablecoin-api')).default) as Curve
   await api.init('Web3', { network: networkId, externalProvider: getWalletProvider(wallet) }, { chainId })
+  // Explicitly set chainId to 1 (Ethereum mainnet) to prevent default value of 0 causing issues
+  api.chainId = 1
   return api
 }
 
