9a6ee6a5cdbb55a919c0f668f61894efca6abd97
Alunara <>
2025-03-26 13:22:48 +0100

    fix: don't filter 0

--- packages/curve-ui-kit/src/lib/logging.ts
+++ packages/curve-ui-kit/src/lib/logging.ts
@@ -101,7 +101,7 @@ export function log(key: LogKey, status?: LogStatus | unknown, ...args: unknown[
 
   const hasDefinedStatus = status && Object.values(LogStatus).includes(status as LogStatus)
   const [formattedKeyString, keyStyles] = formatKeyArray(keyArray)
-  const restArgs = (hasDefinedStatus ? args : [status, ...args]).filter(Boolean)
+  const restArgs = (hasDefinedStatus ? args : [status, ...args]).filter((x) => x != null)
   const argsFormat = restArgs.length ? `%c (%c${restArgs.map((i) => argToString(i)).join(', ')}%c)` : ''
   const format = `%cDApp%c @ %c${timestamp}%c -> ${hasDefinedStatus ? `%c${status}%c ` : ''}${formattedKeyString}${argsFormat}`
   const styles = [
