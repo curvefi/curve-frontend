2d9c5d1b57db536e5b6cfe6aecf1f7441e3359ea
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-04-10 16:39:25 +0200

    chore: update WalletConnect project ID

--- apps/main/src/app/.well-known/walletconnect.txt/route.ts
+++ apps/main/src/app/.well-known/walletconnect.txt/route.ts
@@ -1,16 +1,8 @@
 import { headers } from 'next/headers'
-import {
-  CURVE_DOMAIN_VERIFICATION,
-  VERCEL_DOMAIN_VERIFICATION,
-  WALLET_CONNECT_ACCOUNT,
-} from '@ui-kit/features/connect-wallet/lib/utils/walletModules'
+import { getVercelDomainVerification } from '@ui-kit/features/connect-wallet/lib/utils/walletModules'
 
 export const GET = async () => {
   const head = await headers()
   const host = head.get('host')
-  return new Response(
-    [WALLET_CONNECT_ACCOUNT, host.endsWith('vercel.app') ? VERCEL_DOMAIN_VERIFICATION : CURVE_DOMAIN_VERIFICATION].join(
-      '=',
-    ),
-  )
+  return new Response(getVercelDomainVerification(host))
 }
