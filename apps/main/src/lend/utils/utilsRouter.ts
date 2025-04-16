cbcc0c5d4e46ba3dc4e165d9661a7ffde7355177
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-04-16 21:20:13 +0200

    fix: rest full pathname

--- apps/main/src/lend/utils/utilsRouter.ts
+++ apps/main/src/lend/utils/utilsRouter.ts
@@ -16,4 +16,8 @@ export const getLoanManagePathname = (params: UrlParams, owmId: string, formType
 export const getVaultPathname = (params: UrlParams, owmId: string, formType: string) =>
   getPath(params, `${ROUTE.PAGE_MARKETS}/${owmId}${ROUTE.PAGE_VAULT}${formType === 'vault' ? '' : `/${formType}`}`)
 
-export const getRestFullPathname = () => window.location.pathname.substring(1).split('/').slice(1).join('/')
+/**
+ * Get the part of a path after the network, removing the leading slash and the first two parts.
+ * For example /:app/:network/:page/:id => `:page/:id`
+ */
+export const getRestFullPathname = () => window.location.pathname.substring(1).split('/').slice(2).join('/')
