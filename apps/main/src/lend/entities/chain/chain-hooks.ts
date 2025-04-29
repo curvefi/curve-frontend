26925a2b70ee30662229ca1be849b1941243db60
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-04-29 13:47:39 +0200

    fix: lend market data refresh

--- apps/main/src/lend/entities/chain/chain-hooks.ts
+++ apps/main/src/lend/entities/chain/chain-hooks.ts
@@ -2,21 +2,20 @@ import { useMemo } from 'react'
 import networks from '@/lend/networks'
 import { ChainId } from '@/lend/types/lend.types'
 import { OneWayMarketTemplate } from '@curvefi/lending-api/lib/markets'
-import { ChainParams } from '@ui-kit/lib/model/query'
+import { ChainParams } from '@ui-kit/lib/model'
 import { useApiStore } from '@ui-kit/shared/useApiStore'
 import { useOneWayMarketNames } from './chain-query'
 
-export const useOneWayMarketMapping = (params: ChainParams<ChainId>) => {
-  const { chainId } = params
-  const { data: marketNames, ...rest } = useOneWayMarketNames(params)
+export const useOneWayMarketMapping = ({ chainId }: ChainParams<ChainId>) => {
+  const { data: marketData, ...rest } = useOneWayMarketNames({ chainId })
   const api = useApiStore((state) => state.lending)
   const isLoadingApi = useApiStore((state) => state.isLoadingLending)
   const apiChainId = api?.chainId
   const data: Record<string, OneWayMarketTemplate> | undefined = useMemo(
     () =>
-      marketNames && api && chainId == apiChainId && !isLoadingApi
+      marketData && api && chainId == apiChainId && !isLoadingApi
         ? Object.fromEntries(
-            marketNames
+            marketData.markets
               .filter((marketName) => !networks[chainId!].hideMarketsInUI[marketName])
               .map((name) => [name, api.getOneWayMarket(name)] as const)
               .flatMap(([name, market]) => [
@@ -25,7 +24,7 @@ export const useOneWayMarketMapping = (params: ChainParams<ChainId>) => {
               ]),
           )
         : undefined,
-    [api, apiChainId, chainId, marketNames, isLoadingApi],
+    [api, apiChainId, chainId, isLoadingApi, marketData],
   )
   return { data, ...rest }
 }
