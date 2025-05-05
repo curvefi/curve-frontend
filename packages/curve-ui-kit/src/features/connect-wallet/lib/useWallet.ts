6ded7ba9c6445e71a5b1f2bec7ab116bd8065370
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-05-05 17:22:21 +0200

    fix: ens name in wallet's account

--- packages/curve-ui-kit/src/features/connect-wallet/lib/useWallet.ts
+++ packages/curve-ui-kit/src/features/connect-wallet/lib/useWallet.ts
@@ -1,6 +1,6 @@
 import { BrowserProvider } from 'ethers'
-import { useCallback, useMemo } from 'react'
-import { useConnect, useConnectorClient, useDisconnect } from 'wagmi'
+import { useCallback, useEffect, useMemo } from 'react'
+import { useConnect, useConnectorClient, useDisconnect, useEnsName } from 'wagmi'
 import { useGlobalState } from '@ui-kit/hooks/useGlobalState'
 import type { Wallet } from './types'
 import { connectors } from './wagmi/connectors'
@@ -20,36 +20,34 @@ const useWallet = () => {
   const closeModal = useCallback(() => setShowModal(false), [setShowModal])
   const { data: client } = useConnectorClient()
 
-  const wallet =
-    useMemo(
-      () =>
-        client?.transport.request && {
-          provider: { request: client.transport.request },
-          // no ensName in account, resolving ens triggers re-initialization of the app. Value didn't seem to be used
-          account: { address: client.account.address },
-          chainId: client.chain.id,
-        },
-      [client?.account.address, client?.chain.id, client?.transport.request],
-    ) ?? null
+  const address = client?.account.address
+  const { wallet, provider } =
+    useMemo(() => {
+      const wallet =
+        address && client?.transport.request
+          ? {
+              provider: { request: client.transport.request },
+              account: { address }, // the ensName is set later when detected
+              chainId: client.chain.id,
+            }
+          : null
+      state.wallet = wallet
+      state.provider = wallet ? new BrowserProvider(wallet.provider) : null
+      return state
+    }, [address, client?.chain.id, client?.transport.request]) ?? null
 
-  // important: use the async functions so we can properly handle the promise failures
+  // use the async functions so we can properly handle the promise failures. We could instead use query state in the future.
   const { connectAsync } = useConnect()
   const { disconnectAsync } = useDisconnect()
 
-  /**
-   * Disconnects the wallet from the application.
-   *
-   * This function performs the following steps:
-   * 1. Marks the wallet as manually disconnecting to prevent reconnection attempts
-   * 2. Calls disconnectAsync twice to ensure the useAccount hooks properly reset
-   *    Bro I don't even know why, but if you call it once the useAccount states won't update...
-   */
-  const disconnect = async () => {
+  const disconnect = useCallback(async () => {
+    // Call disconnectAsync twice to ensure the useAccount hooks properly reset
+    // Bro I don't even know why, but if you call it once the useAccount states won't update...
     await disconnectAsync()
     await disconnectAsync()
-  }
+  }, [disconnectAsync])
 
-  const connectWagmi = useCallback(
+  const connect = useCallback(
     async (connector?: (typeof supportedWallets)[number]['connector']) => {
       if (!connector) {
         setShowModal(true)
@@ -69,20 +67,13 @@ const useWallet = () => {
     [connectAsync, setShowModal],
   )
 
-  const { provider: browserProvider } = useMemo(() => {
-    state.wallet = wallet
-    state.provider = state.wallet?.provider ? new BrowserProvider(state.wallet.provider) : null
-    return state
-  }, [wallet])
+  const { data: ensName } = useEnsName({ address })
+  useEffect(() => {
+    // not changing the object reference, so we avoid reinitializing the app
+    state.wallet && (state.wallet.account.ensName = ensName ?? undefined)
+  }, [ensName])
 
-  return {
-    wallet,
-    connect: connectWagmi,
-    disconnect,
-    provider: browserProvider,
-    showModal,
-    closeModal,
-  }
+  return { wallet, connect, disconnect, provider, showModal, closeModal }
 }
 useWallet.getState = () => ({ wallet: state.wallet, provider: state.provider })
 
