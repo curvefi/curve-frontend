8ee21b473f5769d0b7be2d37d074f11272aa8d5c
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-05-14 12:01:49 +0200

    feat: add warning banner for domain change

--- packages/curve-ui-kit/src/shared/ui/GlobalBanner.tsx
+++ packages/curve-ui-kit/src/shared/ui/GlobalBanner.tsx
@@ -1,11 +1,14 @@
 import { forwardRef, Ref } from 'react'
-import { useChainId, useSwitchChain } from 'wagmi'
-import { useAccount } from 'wagmi'
+import { useAccount, useChainId, useSwitchChain } from 'wagmi'
+import { Stack, Typography } from '@mui/material'
+import AlertTitle from '@mui/material/AlertTitle'
 import Box from '@mui/material/Box'
+import Link from '@mui/material/Link'
 import { CONNECT_STAGE, isFailure, useConnection } from '@ui-kit/features/connect-wallet'
 import type { WagmiChainId } from '@ui-kit/features/connect-wallet/lib/wagmi/chains'
 import { useBetaFlag } from '@ui-kit/hooks/useLocalStorage'
 import { t } from '@ui-kit/lib/i18n'
+import { ExclamationTriangleIcon } from '@ui-kit/shared/icons/ExclamationTriangleIcon'
 import { LlamaIcon } from '@ui-kit/shared/icons/LlamaIcon'
 import { Banner } from '@ui-kit/shared/ui/Banner'
 import { SizesAndSpaces } from '@ui-kit/themes/design/1_sizes_spaces'
@@ -17,7 +20,7 @@ export type GlobalBannerProps = {
   ref: Ref<HTMLDivElement>
 }
 
-const { IconSize } = SizesAndSpaces
+const { IconSize, Spacing } = SizesAndSpaces
 
 // Update `NEXT_PUBLIC_MAINTENANCE_MESSAGE` environment variable value to display a global message in app.
 const maintenanceMessage = process.env.NEXT_PUBLIC_MAINTENANCE_MESSAGE
@@ -31,12 +34,17 @@ export const GlobalBanner = forwardRef<HTMLDivElement, Omit<GlobalBannerProps, '
     const { switchChain } = useSwitchChain()
     const { connectState } = useConnection()
     const showConnectApiErrorMessage = isFailure(connectState, CONNECT_STAGE.CONNECT_API)
+    const showDomainChangeMessage = new Date() < new Date('2025-06-01') // TODO: remove code line after this date
     const walletChainId = useChainId()
     const showSwitchNetworkMessage =
       (isConnected && walletChainId != chainId) || isFailure(connectState, CONNECT_STAGE.SWITCH_NETWORK)
 
     return (
-      (showSwitchNetworkMessage || showConnectApiErrorMessage || maintenanceMessage || showBetaBanner) && (
+      (showSwitchNetworkMessage ||
+        showConnectApiErrorMessage ||
+        maintenanceMessage ||
+        showBetaBanner ||
+        showDomainChangeMessage) && (
         <Box ref={ref}>
           {showBetaBanner && (
             <Banner onClick={() => setIsBeta(false)} buttonText={t`Disable Beta Mode`}>
@@ -57,6 +65,29 @@ export const GlobalBanner = forwardRef<HTMLDivElement, Omit<GlobalBannerProps, '
           {showConnectApiErrorMessage && (
             <Banner severity="alert">
               {t`There is an issue connecting to the API. You can try switching your RPC or, if you are connected to a wallet, please switch to a different one.`}
+            </Banner>
+          )}
+          {showDomainChangeMessage && (
+            <Banner severity="warning">
+              <Stack direction="row" alignItems="end" gap={Spacing.xxs}>
+                <ExclamationTriangleIcon />
+                <AlertTitle>{t`Domain Change`}</AlertTitle>
+              </Stack>
+              <Typography color="textSecondary" variant="bodyMRegular" sx={{ textTransform: 'none' }}>
+                {t`Curve Finance has moved to a new domain: `}
+                <Link href="https://curve.finance" target="_blank" color="textSecondary">
+                  {t`curve.finance`}
+                </Link>
+                {t`. Always make sure you are on the right domain. `}
+                {t`Read the announcement `}
+                <Link
+                  href="https://x.com/CurveFinance/status/1922210827362349546"
+                  target="_blank"
+                  color="textSecondary"
+                >
+                  {t`tweet.`}
+                </Link>
+              </Typography>
             </Banner>
           )}
         </Box>
