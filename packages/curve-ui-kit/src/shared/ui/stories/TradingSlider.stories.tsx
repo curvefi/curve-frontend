1d49940b7189c495d2590dce3b3524c3f577fb4d
Alunara <>
2025-05-17 17:50:32 +0200

    feat: add trading slider widget

---
+++ packages/curve-ui-kit/src/shared/ui/stories/TradingSlider.stories.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import Box from '@mui/material/Box'
+import type { Meta, StoryObj } from '@storybook/react'
+import { TradingSlider } from '../TradingSlider'
+
+const TradingSliderComponent = (props: React.ComponentProps<typeof TradingSlider>) => {
+  const [percentage, setPercentage] = useState<number>(props.percentage || 50)
+
+  return (
+    <Box sx={{ width: '400px' }}>
+      <TradingSlider
+        {...props}
+        percentage={percentage}
+        onPercentageChange={(newPercentage) => {
+          setPercentage(newPercentage)
+          props.onPercentageChange?.(newPercentage)
+        }}
+        onPercentageCommitted={(newPercentage) => {
+          setPercentage(newPercentage)
+          props.onPercentageCommitted?.(newPercentage)
+        }}
+      />
+    </Box>
+  )
+}
+
+const meta: Meta<typeof TradingSlider> = {
+  title: 'UI Kit/Widgets/TradingSlider',
+  component: TradingSliderComponent,
+  argTypes: {
+    percentage: {
+      control: 'number',
+      description: 'The current percentage value (0-100)',
+    },
+    step: {
+      control: 'number',
+      description: 'Step increment for the slider and input',
+    },
+    onPercentageChange: {
+      description: 'Callback when percentage changes on the slider',
+    },
+    onPercentageCommitted: {
+      description: 'Callback when percentage changes by releasing the slider or entering a number',
+    },
+  },
+  args: {
+    percentage: 50,
+    step: 1,
+    onPercentageChange: (x) => console.info('Percentage changing:', x),
+    onPercentageCommitted: (x) => console.info('Percentage committed:', x),
+  },
+}
+
+type Story = StoryObj<typeof TradingSlider>
+
+export const Default: Story = {}
+
+export default meta
