bec7833f57a6e87e90a7e440903af809f6bf7250
Alunara <>
2025-06-18 16:58:14 +0200

    refactor: don't render an unnecessary div if there's no token label

--- packages/curve-ui-kit/src/shared/ui/Token.tsx
+++ packages/curve-ui-kit/src/shared/ui/Token.tsx
@@ -1,4 +1,3 @@
-import type { ImgHTMLAttributes } from 'react'
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
@@ -17,6 +16,8 @@ const { Spacing, IconSize } = SizesAndSpaces
 // where 'sm' differs from MUI's 'sm'. At the moment of writing this refactor is out of scope.
 type Size = 'sm' | 'mui-sm' | 'mui-md' | 'lg' | 'xl'
 
+const DEFAULT_SIZE: Size = 'sm'
+
 const LABEL_SPACING = {
   sm: 'sm',
   'mui-sm': 'sm',
@@ -25,65 +26,77 @@ const LABEL_SPACING = {
   xl: 'md',
 } satisfies Record<Size, keyof typeof Spacing>
 
-export interface TokenProps extends ImgHTMLAttributes<HTMLImageElement> {
+type TokenIconProps = {
   className?: string
   blockchainId?: string
   tooltip?: string
-  address?: string | null
   size?: Size
-  label?: string
+  address?: string | null
   sx?: SxProps
 }
 
-export const Token = ({
+const TokenIcon = ({
   className = '',
   blockchainId = '',
   tooltip = '',
-  size = 'sm',
+  size = DEFAULT_SIZE,
   address,
-  label,
   sx,
 }: TokenProps) => (
-  <Stack direction="row" gap={Spacing[LABEL_SPACING[size]]} alignItems="center">
-    <Tooltip title={tooltip} placement="top">
-      <Box
-        component="img"
-        data-testid={`token-icon-${tooltip}`}
-        className={`${className}`}
-        alt={tooltip}
-        onError={({ currentTarget }) => {
-          currentTarget.src = DEFAULT_IMAGE
-        }}
-        src={address ? `${getImageBaseUrl(blockchainId ?? '')}${address.toLowerCase()}.png` : DEFAULT_IMAGE}
-        loading="lazy"
-        sx={(theme) => ({
-          borderRadius: '50%',
-          // The original 'sm' size with a 400 breakpoint is a remainder from legacy code.
-          // I didn't want to break the existing interface as it's used everywhere.
-          ...(size === 'sm' && {
-            width: '1.75rem',
-            height: '1.75rem',
-            [theme.breakpoints.down(400)]: {
-              width: '1.5rem',
-              height: '1.5rem',
-            },
-          }),
-          ...(size === 'mui-sm' && handleBreakpoints({ width: IconSize['sm'], height: IconSize['sm'] })),
-          ...(size === 'mui-md' && handleBreakpoints({ width: IconSize['md'], height: IconSize['md'] })),
-          ...(size === 'lg' && handleBreakpoints({ width: IconSize['lg'], height: IconSize['lg'] })),
-          ...(size === 'xl' && handleBreakpoints({ width: IconSize['xl'], height: IconSize['xl'] })),
-          ...applySxProps(sx, theme),
-        })}
-      />
-    </Tooltip>
+  <Tooltip title={tooltip} placement="top">
+    <Box
+      component="img"
+      data-testid={`token-icon-${tooltip}`}
+      className={`${className}`}
+      alt={tooltip}
+      onError={({ currentTarget }) => {
+        currentTarget.src = DEFAULT_IMAGE
+      }}
+      src={address ? `${getImageBaseUrl(blockchainId ?? '')}${address.toLowerCase()}.png` : DEFAULT_IMAGE}
+      loading="lazy"
+      sx={(theme) => ({
+        borderRadius: '50%',
+        // The original 'sm' size with a 400 breakpoint is a remainder from legacy code.
+        // I didn't want to break the existing interface as it's used everywhere.
+        ...(size === 'sm' && {
+          width: '1.75rem',
+          height: '1.75rem',
+          [theme.breakpoints.down(400)]: {
+            width: '1.5rem',
+            height: '1.5rem',
+          },
+        }),
+        ...(size === 'mui-sm' && handleBreakpoints({ width: IconSize['sm'], height: IconSize['sm'] })),
+        ...(size === 'mui-md' && handleBreakpoints({ width: IconSize['md'], height: IconSize['md'] })),
+        ...(size === 'lg' && handleBreakpoints({ width: IconSize['lg'], height: IconSize['lg'] })),
+        ...(size === 'xl' && handleBreakpoints({ width: IconSize['xl'], height: IconSize['xl'] })),
+        ...applySxProps(sx, theme),
+      })}
+    />
+  </Tooltip>
+)
 
-    {label && (
-      // Lineheight is unset, as setting the line height to a certain size (as is default ehavior for Typography)
-      // causes the text not to be perfectly centered vertically for unknown reason. Setting the 'vertical-align'
-      // property to 'middle' does not appear to fix it, only unsetting the lineheight does.
-      <Typography variant="bodyMBold" sx={{ '&': { lineHeight: 'unset' } }}>
+type TokenProps = TokenIconProps & {
+  label?: string
+}
+
+export const Token = ({ label, ...tokenIconProps }: TokenProps) =>
+  // Only render the stack if there's a label, otherwise we needlessly render an extra div.
+  label ? (
+    <Stack direction="row" gap={Spacing[LABEL_SPACING[tokenIconProps.size ?? DEFAULT_SIZE]]} alignItems="center">
+      <TokenIcon {...tokenIconProps} />
+      <Typography
+        variant="bodyMBold"
+        sx={{
+          // Lineheight is unset, as setting the line height to a certain size (as is default ehavior for Typography)
+          // causes the text not to be perfectly centered vertically for unknown reason. Setting the 'vertical-align'
+          // property to 'middle' does not appear to fix it, only unsetting the lineheight does.
+          '&': { lineHeight: 'unset' },
+        }}
+      >
         {label}
       </Typography>
-    )}
-  </Stack>
-)
+    </Stack>
+  ) : (
+    <TokenIcon {...tokenIconProps} />
+  )
