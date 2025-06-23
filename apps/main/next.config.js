a5bc692cb6c1d140dc2d88af85543db6eb89993b
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-06-23 10:43:38 +0200

    fix: svg imports

--- apps/main/next.config.js
+++ apps/main/next.config.js
@@ -23,6 +23,13 @@ const nextConfiguration = {
       },
     },
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack']
+    })
+    return config
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
