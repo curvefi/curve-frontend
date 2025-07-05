94f9a910c56ffa24908c404c385e8e42993f6636
0xAlunara <88008691+0xAlunara@users.noreply.github.com>
2025-07-05 23:22:34 +0200

    refactor: rename 'ActionCard' to 'ManageSoftLiquidation'

--- packages/curve-ui-kit/src/features/manage-soft-liquidation/index.tsx
+++ packages/curve-ui-kit/src/features/manage-soft-liquidation/index.tsx
@@ -1,3 +1,3 @@
-export { ManageSoftLiquidation, type Props } from './ui/ActionCard'
+export { ManageSoftLiquidation, type Props } from './ui/ManageSoftLiquidation'
 export type { Props as ImproveHealthProps } from './ui/tabs/ImproveHealth'
 export type { Props as ClosePositionProps } from './ui/tabs/ClosePosition'
