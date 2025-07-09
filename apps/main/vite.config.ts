d0b9829dac2ad41da96dfa975882d422291bd137
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-07-09 12:39:19 +0200

    wip: try to fix mui and styled components

--- apps/main/vite.config.ts
+++ apps/main/vite.config.ts
@@ -3,11 +3,17 @@ import { reactRouter } from '@react-router/dev/vite'
 import { resolve } from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: { port: 3000 },
-  plugins: [reactRouter({})],
-  ssr: {
-    noExternal: ['styled-components'],
+export default defineConfig(({ command, mode }) => ({
+  server: {
+    port: 3000,
+    hmr: true,
+  },
+  plugins: [reactRouter()],
+  // ssr: {
+  //   noExternal: ['styled-components', '@mui/material', '@mui/icons-material'],
+  // },
+  optimizeDeps: {
+    include: ['styled-components', '@mui/material', '@mui/icons-material'],
   },
   resolve: {
     alias: [
@@ -19,4 +25,7 @@ export default defineConfig({
       { find: '@curvefi/prices-api/', replacement: resolve(__dirname, '../../packages/prices-api/src/') },
     ],
   },
-})
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+  },
+}))
