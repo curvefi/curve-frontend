df5264538f94d634d2ec3c6043365c4af72930eb
0xAlunara <88008691+0xAlunara@users.noreply.github.com>
2025-07-10 23:34:32 +0200

    feat: show message when repay input is too large

---
+++ packages/curve-ui-kit/src/features/manage-soft-liquidation/ui/alerts/AlertRepayBalanceTooHigh.tsx
@@ -0,0 +1,50 @@
+import Alert from '@mui/material/Alert'
+import AlertTitle from '@mui/material/AlertTitle'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
+import { t } from '@ui-kit/lib/i18n'
+import { SizesAndSpaces } from '@ui-kit/themes/design/1_sizes_spaces'
+
+const { Spacing } = SizesAndSpaces
+
+type Props = {
+  symbol: string
+  input: number
+  userBalance: number | undefined
+  debt: number | undefined
+}
+
+export const AlertRepayBalanceTooHigh = ({ symbol, input, userBalance, debt }: Props) => {
+  const showBalanceMessage = userBalance && input > userBalance
+  const showDebtMessage = debt && input > debt
+  const showAnyMessage = showBalanceMessage || showDebtMessage
+
+  if (!showAnyMessage) return null
+
+  return (
+    <Alert severity="error" variant="outlined" sx={{ boxShadow: 'none' }}>
+      <AlertTitle>{t`Repay amount too large`}</AlertTitle>
+
+      <Stack gap={Spacing.sm}>
+        {showBalanceMessage && (
+          <Typography variant="bodySRegular" color="textSecondary">
+            {t`You're trying to repay ${input} `}
+            <strong>{symbol}</strong>
+            {t`, but your wallet only contains ${userBalance} `}
+            <strong>{symbol}</strong>
+            {t`. Please enter a smaller amount.`}
+          </Typography>
+        )}
+
+        {showDebtMessage && !showBalanceMessage && (
+          <Typography variant="bodySRegular" color="textSecondary">
+            {t`You're trying to repay ${input} `}
+            <strong>{symbol}</strong>
+            {t`, but you only owe ${debt} `}
+            <strong>{symbol}</strong>
+          </Typography>
+        )}
+      </Stack>
+    </Alert>
+  )
+}
