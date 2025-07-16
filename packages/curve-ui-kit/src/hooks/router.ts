1520eb5fb590233cd639b6a7c6aee49bcdb5d659
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-07-16 09:16:34 +0200

    fix: import

--- packages/curve-ui-kit/src/hooks/router.ts
+++ packages/curve-ui-kit/src/hooks/router.ts
@@ -1,6 +1,4 @@
-import type { UrlObject } from 'url'
-import { useSearchParams as useOriginalSearchParams } from 'next/navigation'
-import { useRouter } from 'next/router'
+import { useRouter, useSearchParams as useOriginalSearchParams } from 'next/navigation'
 import { useCallback } from 'react'
 
 export { useParams, usePathname } from 'next/navigation'
@@ -8,7 +6,7 @@ export { useParams, usePathname } from 'next/navigation'
 export const useNavigate = () => {
   const { push, replace } = useRouter()
   return useCallback(
-    (url: UrlObject | string, { replace: shouldReplace }: { replace?: boolean } = {}): void =>
+    (url: string, { replace: shouldReplace }: { replace?: boolean } = {}): void =>
       void (shouldReplace ? replace : push)(url),
     [push, replace],
   )
