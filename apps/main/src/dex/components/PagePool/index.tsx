2452fbe2f138ec532f4fe30d99623d71720b6dcb
0xAlunara <88008691+0xAlunara@users.noreply.github.com>
2025-07-25 20:06:28 +0200

    Revert "refactor: remove commented-out gauge manager code"
    This reverts commit e900604626d6387662b0bacf5ed9b0105bd6a81c.

--- apps/main/src/dex/components/PagePool/index.tsx
+++ apps/main/src/dex/components/PagePool/index.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import { styled } from 'styled-components'
+// import { type Address, isAddressEqual } from 'viem'
 import CampaignRewardsBanner from '@/dex/components/PagePool/components/CampaignRewardsBanner'
 import Deposit from '@/dex/components/PagePool/Deposit'
 import PoolInfoData from '@/dex/components/PagePool/PoolDetails/ChartOhlcWrapper'
@@ -18,12 +19,14 @@ import type {
 import MySharesStats from '@/dex/components/PagePool/UserDetails'
 import Withdraw from '@/dex/components/PagePool/Withdraw'
 import { ROUTE } from '@/dex/constants'
+// import { useGaugeManager } from '@/dex/entities/gauge'
 import usePoolAlert from '@/dex/hooks/usePoolAlert'
 import useTokensMapper from '@/dex/hooks/useTokensMapper'
 import { getUserPoolActiveKey } from '@/dex/store/createUserSlice'
 import useStore from '@/dex/store/useStore'
 import { getChainPoolIdActiveKey } from '@/dex/utils'
 import { getPath } from '@/dex/utils/utilsRouter'
+// import { ManageGauge } from '@/dex/widgets/manage-gauge'
 import AlertBox from '@ui/AlertBox'
 import { AppFormContent, AppFormContentWrapper, AppFormHeader } from '@ui/AppForm'
 import {
@@ -38,6 +41,7 @@ import Box from '@ui/Box'
 import Button from '@ui/Button'
 import Icon from '@ui/Icon'
 import { ExternalLink } from '@ui/Link'
+// import { BlockSkeleton } from '@ui/skeleton'
 import Tabs, { Tab } from '@ui/Tab'
 import TextEllipsis from '@ui/TextEllipsis'
 import { breakpoints } from '@ui/utils/responsive'
@@ -86,6 +90,12 @@ const Transfer = (pageTransferProps: PageTransferProps) => {
   const { initCampaignRewards, initiated } = useStore((state) => state.campaigns)
 
   const storeMaxSlippage = useUserProfileStore((state) => state.maxSlippage[chainIdPoolId])
+
+  // Hidden until gauge manager is fixed
+  // const { data: gaugeManager, isPending: isPendingGaugeManager } = useGaugeManager({
+  //   chainId: rChainId,
+  //   poolId: poolData?.pool.id!,
+  // })
 
   const [selectedTab, setSelectedTab] = useState<DetailInfoTypes>('pool')
   const [seed, setSeed] = useState(DEFAULT_SEED)
@@ -155,6 +165,16 @@ const Transfer = (pageTransferProps: PageTransferProps) => {
     }
   }, [rChainId, poolId, signerAddress, curve, fetchUserPoolInfo])
 
+  // Hidden until gauge manager is fixed
+  // const isAvailableManageGauge = useMemo(
+  //   () =>
+  //     !isPendingGaugeManager &&
+  //     !!signerAddress &&
+  //     !!gaugeManager &&
+  //     isAddressEqual(gaugeManager, signerAddress as Address),
+  //   [isPendingGaugeManager, signerAddress, gaugeManager],
+  // )
+
   const ACTION_TABS: { key: TransferFormType; label: string }[] = [
     { key: 'deposit', label: t`Deposit` },
     { key: 'withdraw', label: t`Withdraw/Claim` },
@@ -167,6 +187,13 @@ const Transfer = (pageTransferProps: PageTransferProps) => {
     },
     [push, params],
   )
+
+  // Hidden until gauge manager is fixed
+  // useEffect(() => {
+  //   if (!isAvailableManageGauge && rFormType === 'manage-gauge') {
+  //     toggleForm('deposit')
+  //   }
+  // }, [isAvailableManageGauge, rFormType, toggleForm])
 
   const TitleComp = () => (
     <AppPageFormTitleWrapper>
@@ -212,6 +239,8 @@ const Transfer = (pageTransferProps: PageTransferProps) => {
               formTypes={ACTION_TABS}
               activeFormKey={!rFormType ? 'deposit' : (rFormType as string)}
               handleClick={(key: string) => toggleForm(key as TransferFormType)}
+              showMenuButton={false} // Always false until gauge manager is fixed
+              // showMenuButton={isAvailableManageGauge}
             />
 
             <AppFormContentWrapper>
@@ -257,7 +286,15 @@ const Transfer = (pageTransferProps: PageTransferProps) => {
                   userPoolBalances={userPoolBalances}
                   userPoolBalancesLoading={userPoolBalancesLoading}
                 />
-              ) : null}
+              ) : // Hidden until gauge manager is fixed
+              // : rFormType === 'manage-gauge' ? (
+              //   poolData ? (
+              //     <ManageGauge poolId={poolData.pool.id} chainId={rChainId} />
+              //   ) : (
+              //     <BlockSkeleton width={339} />
+              //   )
+              // )
+              null}
             </AppFormContentWrapper>
           </AppFormContent>
         </AppPageFormsWrapper>
