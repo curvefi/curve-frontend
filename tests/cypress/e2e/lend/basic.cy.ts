0007034ebe16d0a4c758ea02467aa3db0c36a24f
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-08-12 12:43:05 +0200

    fix: redirect from root

--- tests/cypress/e2e/lend/basic.cy.ts
+++ tests/cypress/e2e/lend/basic.cy.ts
@@ -14,7 +14,7 @@ describe('Basic Access Test', () => {
 
   it('should redirect from the old nested URL successfully', () => {
     cy.visit('/lend/#/ethereum/disclaimer?tab=lend')
-    cy.url(LOAD_TIMEOUT).should('match', /http:\/\/localhost:\d+\/lend\/ethereum\/disclaimer\/\?tab=lend$/)
+    cy.url(LOAD_TIMEOUT).should('match', /http:\/\/localhost:\d+\/lend\/ethereum\/disclaimer\/?\?tab=lend$/)
     cy.title().should('equal', 'Risk Disclaimer - Curve Lend')
   })
 })
