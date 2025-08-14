1b53696f69d04ce7e10866f2c73d1a4235dfd9cc
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-08-14 11:03:37 +0200

    fix: mock urls

--- tests/cypress/support/helpers/minting-mocks.ts
+++ tests/cypress/support/helpers/minting-mocks.ts
@@ -4,6 +4,6 @@ export const mockMintMarkets = () =>
   })
 
 export const mockMintSnapshots = () =>
-  cy.intercept('https://prices.curve.finance/v1/crvusd/markets/*/*/snapshots?agg=none', {
+  cy.intercept('https://prices.curve.finance/v1/crvusd/markets/*/*/snapshots?agg=none&fetch_on_chain=true', {
     fixture: 'minting-snapshots.json',
   })
