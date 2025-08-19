74295ad96ae0e7ccbace03f25ec1755bd07201cc
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-08-19 14:30:32 +0200

    chore: test api route

--- apps/main/vite.config.ts
+++ apps/main/vite.config.ts
@@ -3,15 +3,25 @@ import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import vercel from 'vite-plugin-vercel'
+import { getEntriesFromFs } from 'vite-plugin-vercel/utils'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig(async ({ command }) => ({
   server: {
     port: 3000,
     hmr: true,
   },
   preview: { port: 3000 },
-  plugins: [react(), svgr(), vercel()],
+  plugins: [
+    react(),
+    svgr(),
+    vercel({
+      // Scan `_api` directory for entries, and map them to `/api/*`
+      entries: await getEntriesFromFs('_api', {
+        destination: 'api',
+      }),
+    }),
+  ],
   optimizeDeps: {
     include: ['styled-components', '@mui/material', '@mui/icons-material'],
   },
