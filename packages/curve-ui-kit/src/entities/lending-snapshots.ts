d101050800b023acb51546b27f82e56e44653b22
JustJousting <justjousting2moon@gmail.com>
2025-08-19 23:13:31 +0300

    fix: cleanup

--- packages/curve-ui-kit/src/entities/lending-snapshots.ts
+++ packages/curve-ui-kit/src/entities/lending-snapshots.ts
@@ -8,7 +8,7 @@ type QueryParams = ContractParams & { agg?: 'none' | 'day' | 'week'; limit?: num
 type Query = ContractQuery & { agg?: 'none' | 'day' | 'week'; limit?: number }
 
 export const { useQuery: useLendingSnapshots } = queryFactory({
-  queryKey: (params: QueryParams) => [...rootKeys.contract(params), 'lendingSnapshots', 'v3'] as const,
+  queryKey: (params: QueryParams) => [...rootKeys.contract(params), 'lendingSnapshots', 'v4'] as const,
   queryFn: async ({ blockchainId, contractAddress, agg, limit }: Query): Promise<LendingSnapshot[]> => {
     // todo: pass {sort_by: 'DATE_ASC, start: now-week} and remove reverse (backend is timing out)
     const response = await getSnapshots(blockchainId, contractAddress, { agg, fetch_on_chain: true, limit })
