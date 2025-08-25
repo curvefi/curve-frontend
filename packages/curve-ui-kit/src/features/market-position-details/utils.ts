1b90a34da732ba7236c0865ddee220228a669fd5
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-08-25 14:47:52 +0200

    chore: another ux color review

--- packages/curve-ui-kit/src/features/market-position-details/utils.ts
+++ packages/curve-ui-kit/src/features/market-position-details/utils.ts
@@ -1,5 +1,5 @@
 import type { Theme } from '@mui/material'
-import { Blues, Reds } from '@ui-kit/themes/design/0_primitives'
+import { Blues, Greens, Reds } from '@ui-kit/themes/design/0_primitives'
 
 export const calculateRangeToLiquidation = (upperLiquidationPrice: number, oraclePrice: number) =>
   ((oraclePrice - upperLiquidationPrice) / upperLiquidationPrice) * 100
@@ -14,5 +14,23 @@ export const getHealthValueColor = (health: number, { design: { Color, Text } }:
   return health < 100 ? Text.TextColors.Primary : Color.Secondary[600]
 }
 
-export const getHealthTrackColor = (health: number | undefined | null, softLiquidation: boolean | undefined | null) =>
-  softLiquidation ? Reds[500] : health == null || health >= 5 ? Blues[500] : Reds[400]
+const Yellow = Reds[300]
+const Orange = Reds[400]
+const Red = Reds[600]
+const Blue = Blues[500]
+const Green = Greens[400]
+
+export const getHealthTrackColor = (health: number | undefined | null, softLiquidation: boolean | undefined | null) => {
+  if (health == null) {
+    return softLiquidation ? Orange : Blue
+  }
+  if (softLiquidation) {
+    if (health < 5) return Red
+    if (health < 40) return Orange
+    return Yellow
+  }
+  if (health < 3) return Orange
+  if (health < 10) return Yellow
+  if (health < 50) return Blue
+  return Green
+}
