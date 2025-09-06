bbf3bf9db5f2b67cce7d64216f90ebc509a8c65e
Alunara <alunara@curve.finance>
2025-09-07 00:12:41 +0200

    feat: use tabswitcher for dex deposit

--- apps/main/src/dex/components/PagePool/Deposit/index.tsx
+++ apps/main/src/dex/components/PagePool/Deposit/index.tsx
@@ -1,73 +1,43 @@
-import { Fragment, useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import FormDeposit from '@/dex/components/PagePool/Deposit/components/FormDeposit'
 import FormDepositStake from '@/dex/components/PagePool/Deposit/components/FormDepositStake'
 import FormStake from '@/dex/components/PagePool/Deposit/components/FormStake'
 import type { FormType } from '@/dex/components/PagePool/Deposit/types'
 import { DEFAULT_FORM_STATUS } from '@/dex/components/PagePool/Deposit/utils'
-import { StyledTabSlide } from '@/dex/components/PagePool/styles'
 import type { TransferProps } from '@/dex/components/PagePool/types'
 import useStore from '@/dex/store/useStore'
-import { isValidAddress } from '@/dex/utils'
 import AlertBox from '@ui/AlertBox'
-import { SlideTab, SlideTabs } from '@ui/TabSlide'
 import { t } from '@ui-kit/lib/i18n'
+import { TabsSwitcher } from '@ui-kit/shared/ui/TabsSwitcher'
 
 const Deposit = ({ hasDepositAndStake, ...transferProps }: TransferProps & { hasDepositAndStake: boolean }) => {
-  const tabsRef = useRef<HTMLDivElement>(null)
-
   const { poolAlert, poolData, poolDataCacheOrApi } = transferProps
-  const { rChainId } = transferProps.routerParams
   const formType = useStore((state) => state.poolDeposit.formType)
   const resetState = useStore((state) => state.poolDeposit.resetState)
   const setStateByKeys = useStore((state) => state.poolDeposit.setStateByKeys)
 
-  const [tabPositions, setTabPositions] = useState<{ left: number; width: number; top: number }[]>([])
-  const [selectedTabIdx, setSelectedTabIdx] = useState(0)
+  const TABS: { value: FormType; label: string }[] = [
+    { value: 'DEPOSIT', label: t`Deposit` },
+    { value: 'STAKE', label: t`Stake` },
+    { value: 'DEPOSIT_STAKE', label: t`Deposit & Stake` },
+  ]
 
-  const TABS = useMemo<{ label: string; formType: FormType }[]>(
-    () =>
-      [
-        { label: t`Deposit`, formType: 'DEPOSIT' },
-        { label: t`Stake`, formType: 'STAKE' },
-        { label: t`Deposit & Stake`, formType: 'DEPOSIT_STAKE' },
-      ] as const,
-    [],
-  )
-
-  // tabs positions
-  useEffect(() => {
-    if (!tabsRef.current) return
-
-    const tabsNode = tabsRef.current
-    const tabsDOMRect = tabsNode.getBoundingClientRect()
-    const updatedTabPositions = Array.from(tabsNode.childNodes as NodeListOf<HTMLInputElement>)
-      .filter((n) => n.classList.contains('tab'))
-      .map((n, idx) => {
-        const domRect = n.getBoundingClientRect()
-        const left = idx ? domRect.left - tabsDOMRect.left : 0
-        const top = domRect.bottom - tabsDOMRect.top
-        return { left, width: domRect.width, top }
-      })
-
-    setTabPositions(updatedTabPositions)
-  }, [selectedTabIdx])
+  const [selectedTab, setSelectedTab] = useState<FormType>('DEPOSIT')
 
   const handleTabChange = useCallback(
-    (idx: number) => {
+    (tab: FormType) => {
       setStateByKeys({
         formStatus: DEFAULT_FORM_STATUS,
-        formType: TABS[idx].formType,
+        formType: tab,
       })
-      setSelectedTabIdx(idx)
+      setSelectedTab(tab)
     },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     [setStateByKeys],
   )
 
-  // onMount
   useEffect(() => {
     if (poolData) {
-      handleTabChange(0)
+      handleTabChange('DEPOSIT')
       resetState(poolData, 'DEPOSIT')
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -75,29 +45,19 @@ const Deposit = ({ hasDepositAndStake, ...transferProps }: TransferProps & { has
 
   return (
     <>
-      {isValidAddress(poolDataCacheOrApi.pool.gauge.address) && rChainId && (
-        <StyledTabSlide activeIdx={selectedTabIdx}>
-          <SlideTabs ref={tabsRef}>
-            {TABS.map(({ formType, label }, idx) => {
-              if (formType === 'DEPOSIT_STAKE' && !hasDepositAndStake) {
-                return <Fragment key={label}></Fragment>
-              }
-
-              return (
-                <SlideTab
-                  key={label}
-                  tabLeft={tabPositions[idx]?.left}
-                  tabWidth={tabPositions[idx]?.width}
-                  tabTop={tabPositions[idx]?.top}
-                  onChange={() => handleTabChange(idx)}
-                  tabIdx={idx}
-                  label={label}
-                />
-              )
-            })}
-          </SlideTabs>
-        </StyledTabSlide>
-      )}
+      <TabsSwitcher
+        variant="underlined"
+        size="small"
+        value={selectedTab}
+        onChange={handleTabChange}
+        options={TABS}
+        sx={{
+          backgroundColor: (t) => t.design.Layer[1].Fill,
+          // Undo the padding from AppFormContentWrapper, not the cleanest but will have to do for now.
+          marginInline: 'calc(-1 * var(--spacing-3))',
+          marginTop: '-0.5rem',
+        }}
+      />
 
       {poolAlert && poolAlert.isDisableDeposit ? (
         <AlertBox {...poolAlert}>{poolAlert.message}</AlertBox>
