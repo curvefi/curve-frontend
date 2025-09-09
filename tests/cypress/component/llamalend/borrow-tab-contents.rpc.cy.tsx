6f88cf2f454f3e44910f1e864bf7ace599fbffe7
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-09-09 16:25:56 +0200

    test: check that all loan information is loaded as expected
    We need to investigate how to mint coins (with tenderly) and sign transactions with the wagmi test connector.

--- tests/cypress/component/llamalend/borrow-tab-contents.rpc.cy.tsx
+++ tests/cypress/component/llamalend/borrow-tab-contents.rpc.cy.tsx
@@ -24,27 +24,42 @@ describe('BorrowTabContents Component Tests', () => {
     },
   }))
 
-  const BorrowTabTestWrapper = () => {
-    const vnet = getVirtualNetwork()
-    if (!vnet) throw new Error('Unable to get Virtual Network, check your tenderly configuration')
-    return (
-      <ClientWrapper config={createTestWagmiConfigFromVNet({ vnet })} autoConnect>
-        <ConnectionProvider app="llamalend" network={network} onChainUnavailable={console.error}>
-          <Box sx={{ maxWidth: 500 }}>
-            <BorrowTabTest />
-          </Box>
-        </ConnectionProvider>
-      </ClientWrapper>
-    )
-  }
+  const BorrowTabTestWrapper = () => (
+    <ClientWrapper config={createTestWagmiConfigFromVNet({ vnet: getVirtualNetwork() })} autoConnect>
+      <ConnectionProvider app="llamalend" network={network} onChainUnavailable={console.error}>
+        <Box sx={{ maxWidth: 500 }}>
+          <BorrowTabTest />
+        </Box>
+      </ConnectionProvider>
+    </ClientWrapper>
+  )
+
+  const getActionValue = (name: string) => cy.get(`[data-testid="${name}-value"]`)
 
   it('calculates max debt and health', () => {
     cy.mount(<BorrowTabTestWrapper />)
-    cy.get('[data-testid="borrow-debt-input"] [data-testid="balance-value"]').should('have.text', '?')
+    cy.get('[data-testid="borrow-debt-input"] [data-testid="balance-value"]').should('exist')
     cy.get('[data-testid="borrow-collateral-input"] input[type="text"]').first().type('1')
     cy.get('[data-testid="borrow-debt-input"] [data-testid="balance-value"]').should('not.contain.text', '?')
-    cy.get('[data-testid="borrow-health-value"]').should('have.text', '∞')
+    getActionValue('borrow-health').should('have.text', '∞')
     cy.get('[data-testid="borrow-debt-input"] input[type="text"]').first().type('100')
-    cy.get('[data-testid="borrow-health-value"]').should('not.contain.text', '∞')
+    getActionValue('borrow-health').should('not.contain.text', '∞')
+
+    // open borrow advanced settings and check all fields
+    cy.contains('button', 'Health').click()
+
+    getActionValue('borrow-price-impact').contains('%')
+    getActionValue('borrow-band-range').should('exist') // bands are giving an error for now, don't check the content
+    getActionValue('borrow-price-range')
+      .invoke('text')
+      .should('match', /(\d(\.\d+)?)( - )(\d(\.\d+)?)/)
+    getActionValue('borrow-apr').contains('%')
+    getActionValue('borrow-ltv').contains('%')
+    getActionValue('borrow-slippage').contains('%')
+
+    // todo: mint some coins with tenderly so we can actually do a transaction
+    cy.get('[data-testid="borrow-form-errors"]').contains('Exceeds maximum collateral of 0 lbtc')
+    // todo: actually sign a transaction with the wagmi test connector and start a loan
+    cy.get('[data-testid="borrow-submit-button"]').should('be.disabled')
   })
 })
