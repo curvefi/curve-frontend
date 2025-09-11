0747047c752e40f8a562faa4c6544088bcde2b31
Alunara <alunara@curve.finance>
2025-09-11 16:17:39 +0200

    styling: increase tab size for my borrow positions for consistency

--- apps/main/src/llamalend/LlamaMarketsPage/UserPositionTabs.tsx
+++ apps/main/src/llamalend/LlamaMarketsPage/UserPositionTabs.tsx
@@ -20,7 +20,7 @@ export const UserPositionsTabs = (props: Omit<UserPositionsTableProps, 'tab'>) =
   const [tab, setTab] = useState<MarketRateType>(defaultTab)
   return (
     <Stack>
-      <TabsSwitcher value={tab} onChange={setTab} variant="contained" size="small" options={tabs} />
+      <TabsSwitcher value={tab} onChange={setTab} variant="contained" size="medium" options={tabs} />
       <UserPositionsTable {...props} tab={tab} />
     </Stack>
   )
