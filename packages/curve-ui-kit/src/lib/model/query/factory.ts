12bb945edd71264a8acef33aa091f3e0d6a04f5d
Alunara <alunara@curve.finance>
2025-09-23 16:15:45 +0200

    refactor: terser refetchQuery

--- packages/curve-ui-kit/src/lib/model/query/factory.ts
+++ packages/curve-ui-kit/src/lib/model/query/factory.ts
@@ -56,14 +56,11 @@ export function queryFactory<
     prefetchQuery: (params, staleTime = 0) => queryClient.prefetchQuery({ ...getQueryOptions(params), staleTime }),
     fetchQuery: (params, options) => queryClient.fetchQuery({ ...getQueryOptions(params), ...options }),
     /**
-     * Function that is like fetchQuery, but first invalidates the query to ensure fresh data is fetched.
-     * Primary use case is for Zustand stores where want to to both use queries and ensure freshness.
+     * Function that is like fetchQuery, but sets staleTime to 0 to ensure fresh data is fetched.
+     * Primary use case is for Zustand stores where want to both use queries and ensure freshness.
      * I suspect this will be the only case, and once Zustand refactoring to Tanstack is complete, we may delete this.
      */
-    refetchQuery: async (params) => {
-      await queryClient.invalidateQueries({ queryKey: queryKey(params) })
-      return queryClient.fetchQuery({ ...getQueryOptions(params), ...options })
-    },
+    refetchQuery: (params) => queryClient.fetchQuery({ ...getQueryOptions(params), ...options, staleTime: 0 }),
     useQuery: (params, condition) => useQuery(getQueryOptions(params, condition)),
     invalidate: (params) => queryClient.invalidateQueries({ queryKey: queryKey(params) }),
   }
