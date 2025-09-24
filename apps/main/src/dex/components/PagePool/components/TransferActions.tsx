1684ea54bc12eade264f252dec778bcdb46b8be5
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-09-24 13:04:43 +0200

    fix: unable to get user balances

--- apps/main/src/dex/components/PagePool/components/TransferActions.tsx
+++ apps/main/src/dex/components/PagePool/components/TransferActions.tsx
@@ -7,7 +7,7 @@ import useTokenAlert from '@/dex/hooks/useTokenAlert'
 import useStore from '@/dex/store/useStore'
 import { getChainPoolIdActiveKey } from '@/dex/utils'
 import AlertBox from '@ui/AlertBox'
-import { isLoading as isConnectLoading, useConnection } from '@ui-kit/features/connect-wallet'
+import { useConnection } from '@ui-kit/features/connect-wallet'
 import { t } from '@ui-kit/lib/i18n'
 
 const TransferActions = ({
@@ -24,7 +24,7 @@ const TransferActions = ({
   const { data: signerAddress } = useSignerAddress()
   const { rChainId, rPoolId } = routerParams
   const alert = useTokenAlert(poolData?.tokenAddressesAll ?? [])
-  const { connectState } = useConnection()
+  const { curveApi } = useConnection()
   const currencyReserves = useStore((state) => state.pools.currencyReserves[getChainPoolIdActiveKey(rChainId, rPoolId)])
   const walletBalancesLoading = useStore((state) => state.user.walletBalancesLoading)
 
@@ -32,7 +32,7 @@ const TransferActions = ({
     loading ||
     typeof poolData === 'undefined' ||
     typeof currencyReserves === 'undefined' ||
-    isConnectLoading(connectState) ||
+    !curveApi?.hydrated ||
     !seed.loaded
 
   return (
