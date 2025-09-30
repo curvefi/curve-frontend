cc3d4fa7ae90cf3b71cbf17d1d92bbbb37687627
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-09-30 14:45:30 +0200

    feat: restore old DetailInfo for stable mode

--- packages/ui/src/DetailInfo/DetailInfo.tsx
+++ packages/ui/src/DetailInfo/DetailInfo.tsx
@@ -1,7 +1,12 @@
 import { ReactNode } from 'react'
+import { styled } from 'styled-components'
 import Divider from '@mui/material/Divider'
+import { useReleaseChannel } from '@ui-kit/hooks/useLocalStorage'
 import ActionInfo, { ActionInfoProps } from '@ui-kit/shared/ui/ActionInfo'
 import { SizesAndSpaces } from '@ui-kit/themes/design/1_sizes_spaces'
+import { ReleaseChannel } from '@ui-kit/utils'
+import Box from 'ui/src/Box/Box'
+import Loader from 'ui/src/Loader/Loader'
 
 const { Spacing } = SizesAndSpaces
 
@@ -27,7 +32,38 @@ type Props = {
   variant?: Variant
 }
 
-const DetailInfo = ({
+const OldDetailInfo = ({
+  className,
+  isBold,
+  isDivider,
+  label,
+  loading,
+  loadingSkeleton,
+  tooltip,
+  variant,
+  children,
+}: Props) => (
+  <Wrapper
+    className={`${className} ${isDivider ? 'divider' : ''}`}
+    grid
+    gridAutoFlow="column"
+    gridColumnGap={2}
+    isDivider={isDivider}
+    fillWidth
+  >
+    {label && <DetailLabel>{label}</DetailLabel>}
+    <DetailValue haveLabel={!!label} isBold={isBold} variant={variant}>
+      {loading && <Loader skeleton={loadingSkeleton} />}
+      {!loading && (
+        <>
+          {children || '-'} {!!tooltip && tooltip}
+        </>
+      )}
+    </DetailValue>
+  </Wrapper>
+)
+
+const NewDetailInfo = ({
   className,
   isBold,
   isDivider,
@@ -50,8 +86,86 @@ const DetailInfo = ({
       error={variant === 'error'}
       loading={loading && (loadingSkeleton || true)}
       copyValue={typeof children === 'string' ? children : ''}
+      size={isBold ? 'large' : 'medium'}
     />
   </>
 )
 
-export default DetailInfo
+export const DetailLabel = styled.span`
+  display: inline-block;
+  font-weight: bold;
+`
+
+type DetailValeProps = {
+  haveLabel: boolean
+  isBold?: boolean | null
+  variant?: Variant
+}
+
+const DetailValue = styled.div<DetailValeProps>`
+  align-items: center;
+  display: flex;
+  justify-content: flex-end;
+
+  font-weight: ${({ isBold }) => (isBold ? '700' : 'inherit')};
+  text-align: ${({ haveLabel }) => (haveLabel ? 'right' : 'left')};
+
+  color: ${({ variant }) => {
+    if (variant === 'error') {
+      return 'var(--danger-400)'
+    } else if (variant === 'warning') {
+      return 'var(--warning-text-400)'
+    } else if (variant === 'success') {
+      return 'var(--success-400)'
+    } else {
+      return 'inherit'
+    }
+  }};
+`
+
+interface WrapperProps extends Pick<Props, 'isDivider' | 'isMultiLine'> {}
+
+const Wrapper = styled(Box)<WrapperProps>`
+  align-items: center;
+  min-height: 1.7rem; // 27px
+  font-size: var(--font-size-3);
+
+  .svg-tooltip {
+    margin-top: 0.25rem;
+    top: 0.1rem;
+  }
+
+  .svg-arrow {
+    position: relative;
+    top: 0.1875rem; // 3px
+    opacity: 0.7;
+  }
+
+  ${({ isDivider }) => {
+    if (isDivider) {
+      return `
+        margin-top: var(--spacing-1);
+        padding-top: var(--spacing-1);
+        border-color: inherit;
+        border-top: 1px solid var(--border-400);
+      `
+    }
+  }}
+  ${({ isMultiLine }) => {
+    if (isMultiLine) {
+      return `
+        grid-auto-flow: row;
+      `
+    }
+  }}
+
+  .svg-tooltip {
+    top: 0.2rem;
+  }
+`
+
+export default function DetailInfo(props: Props) {
+  const [releaseChannel] = useReleaseChannel()
+  const DetailInfo = releaseChannel === ReleaseChannel.Beta ? NewDetailInfo : OldDetailInfo
+  return <DetailInfo {...props} />
+}
