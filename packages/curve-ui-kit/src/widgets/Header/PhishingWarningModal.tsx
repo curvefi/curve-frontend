028e5a5115b2b0fd741c5bd0358f06025fce6139
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-09-30 09:59:41 +0200

    feat: add bullets, docs & news urls

--- packages/curve-ui-kit/src/widgets/Header/PhishingWarningModal.tsx
+++ packages/curve-ui-kit/src/widgets/Header/PhishingWarningModal.tsx
@@ -9,6 +9,17 @@ import { useSwitch } from '@ui-kit/hooks/useSwitch'
 import { t } from '@ui-kit/lib/i18n'
 import { ExclamationTriangleIcon } from '@ui-kit/shared/icons/ExclamationTriangleIcon'
 import { ModalDialog } from '@ui-kit/shared/ui/ModalDialog'
+import { SizesAndSpaces } from '@ui-kit/themes/design/1_sizes_spaces'
+
+const { Spacing } = SizesAndSpaces
+
+const urls = [
+  'https://www.curve.finance',
+  'https://resources.curve.finance',
+  'https://gov.curve.finance',
+  'https://docs.curve.finance',
+  'https://news.curve.finance',
+]
 
 /**
  * Displays a modal warning users about phishing risks and encourages them to verify they are on the official Curve domains.
@@ -39,24 +50,27 @@ export const PhishingWarningModal = () => {
       <Stack gap={2}>
         <Typography variant="bodyMBold">{t`Make sure you’re on the right domain`}</Typography>
         <Typography>
-          {t`Leading DeFi apps are currently being targeted by phishing and clone sites, and Curve is a high-value target. Please double-check that you are on the official domain, either:`}
+          {t`Leading DeFi apps are currently being targeted by phishing and clone sites, and Curve is a high-value target. Please double-check that you are on the official domain, one of:`}
         </Typography>
-        <List sx={{ listStyleType: 'disc', a: { textDecoration: 'none' } }}>
-          <ListItem>
-            <Link target="_blank" href="https://curve.finance">
-              curve.finance
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link target="_blank" href="https://resources.curve.finance">
-              resources.curve.finance
-            </Link>
-          </ListItem>
-          <ListItem>
-            <Link target="_blank" href="https://gov.curve.finance">
-              gov.curve.finance
-            </Link>
-          </ListItem>
+        <List
+          sx={{
+            li: {
+              '&::before': {
+                content: '"•"',
+                color: (t) => t.design.Text.TextColors.Primary,
+                paddingInlineEnd: Spacing.sm,
+              },
+            },
+            a: { textDecoration: 'none' },
+          }}
+        >
+          {urls.map((url) => (
+            <ListItem key={url}>
+              <Link target="_blank" href={url}>
+                {url.replace(/https?:\/\/(www\.)?/, '')}
+              </Link>
+            </ListItem>
+          ))}
         </List>
         <Typography>
           Never trust links from DMs or unofficial sources. Fake sites may try to steal your funds.
