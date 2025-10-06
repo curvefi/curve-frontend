2ce5fefbe8dca80d0d4b39ad35e6fc70b67d8b83
Pearce <pierre.vinet17@gmail.com>
2025-10-06 15:59:29 +0200

    feat: cypress end to end test for legal page

--- tests/cypress/e2e/all/legal.cy.ts
+++ tests/cypress/e2e/all/legal.cy.ts
@@ -8,44 +8,42 @@ import {
   oneViewport,
 } from '@cy/support/ui'
 
-describe('Disclaimers', () => {
-  describe('Footer link', () => {
+describe('Legal', () => {
+  describe('Footer Link', () => {
     const viewport = oneOf(oneDesktopViewport, oneTabletViewport)()
-    it(`should contain footer disclaimer links for ${viewport.join('x')}`, () => {
+    it(`it should contain footer legal link for ${viewport.join('x')}`, () => {
       cy.viewport(...viewport)
       cy.visit(`/${oneAppPath() || 'dex'}/`, {
         onBeforeLoad: (win) => win.localStorage.setItem('phishing-warning-dismissed', `"${new Date().toISOString()}"`),
       })
 
-      // Navigate to risk disclaimer from footer.
+      //Navigate to legal route from footer link
       cy.get(`[data-testid='footer']`, LOAD_TIMEOUT).should('be.visible')
-      cy.get(`[data-testid='footer'] a`).contains('disclaimer', { matchCase: false }).click()
-      cy.url(API_LOAD_TIMEOUT).should('match', /\/disclaimer\/?(\?tab=(lend|crvusd))?$/)
-      cy.get(`[data-testid='disclaimer']`, LOAD_TIMEOUT).should('be.visible')
+      cy.get(`[data-testid='footer'] a`).contains('legal', { matchCase: false }).click()
+      cy.url(API_LOAD_TIMEOUT).should('match', /\/legal\/?$/)
+      cy.get(`[data-testid='legal-page']`, LOAD_TIMEOUT).should('be.visible')
     })
   })
 
   describe('Navigation', () => {
     const [width, height] = oneViewport()
-
     it(`should contain multiple tabs for ${width}x${height}`, () => {
       cy.viewport(width, height)
-      cy.visit(`/${oneAppPath() || 'dex'}/ethereum/disclaimer`, {
+      cy.visit(`/${oneAppPath() || 'dex'}/ethereum/legal`, {
         onBeforeLoad: (win) => win.localStorage.setItem('phishing-warning-dismissed', `"${new Date().toISOString()}"`),
       })
 
       // Make sure there's tabs available and click one.
-      cy.get(`[data-testid='disclaimer']`, LOAD_TIMEOUT).should('be.visible')
+      cy.get(`[data-testid='legal-page']`, LOAD_TIMEOUT).should('be.visible')
+      const tabSelector = "[data-testid='legal-page'] [role='tablist'] [role='tab']"
+      cy.get(tabSelector).should('have.length', 3)
 
-      const tabSelector = "[data-testid='disclaimer'] [role='tablist'] [role='tab']"
-      cy.get(tabSelector).should('have.length', 4)
-
-      // scrvusd tab should not be open. Find it, click it, and its contents should have a link to the scrvusd docs.
-      cy.get('div[role="tabpanel"] a')
-        .filter('[href="https://docs.curve.finance/scrvusd/overview/"]')
-        .should('not.exist')
+      // Navigate to the Disclaimer tab and check if the other tabs load. Then click the scrvusd tab
       cy.get(tabSelector).last().click()
-      cy.url(LOAD_TIMEOUT).should('include', '?tab=scrvusd')
+      cy.url(LOAD_TIMEOUT).should('include', '?tab=disclaimers')
+      const subTabsSelector = "[data-testid='legal-page'] [role='tablist'] [role='tab']"
+      cy.get(subTabsSelector).last().click()
+      cy.url(LOAD_TIMEOUT).should('include', '?tab=disclaimers&subtab=scrvusd')
       cy.get('div[role="tabpanel"] a')
         .filter('[href="https://docs.curve.finance/scrvusd/overview/"]')
         .should('be.visible')
