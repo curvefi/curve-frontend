e4a5021a11afb886298cef284fc478d7d514b70b
Alunara <alunara@curve.finance>
2025-10-10 15:39:00 +0200

    refactor: change !. to ?.

--- apps/main/src/dex/hooks/useChainId.ts
+++ apps/main/src/dex/hooks/useChainId.ts
@@ -3,5 +3,5 @@ import { useNetworks } from '../entities/networks'
 
 export const useChainId = (networkId: NetworkEnum): ChainId => {
   const { data: networks } = useNetworks()
-  return Object.values(networks).find((network) => network.networkId === networkId)!.chainId as ChainId
+  return Object.values(networks).find((network) => network.networkId === networkId)?.chainId as ChainId
 }
