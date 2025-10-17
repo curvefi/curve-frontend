148e6cdbf90c263416e5061dd45b95d54ae39157
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-10-17 15:22:47 +0200

    fix: use api data for crvusd app stats

--- apps/main/src/loan/entities/appstats-total-crvusd-supply.ts
+++ apps/main/src/loan/entities/appstats-total-crvusd-supply.ts
@@ -1,12 +1,12 @@
-import networks from '@/loan/networks'
-import type { ChainId } from '@/loan/types/loan.types'
-import { requireLib } from '@ui-kit/features/connect-wallet'
-import type { ChainParams, ChainQuery } from '@ui-kit/lib/model/query'
+import { EmptyValidationSuite } from '@ui-kit/lib'
 import { queryFactory } from '@ui-kit/lib/model/query'
-import { llamaApiValidationSuite } from '@ui-kit/lib/model/query/curve-api-validation'
+import { decimal } from '@ui-kit/utils'
 
 export const { useQuery: useAppStatsTotalCrvusdSupply } = queryFactory({
-  queryKey: (params: ChainParams<ChainId>) => ['appStatsTotalCrvusdSupply', { chainId: params.chainId }] as const,
-  queryFn: ({ chainId }: ChainQuery<ChainId>) => networks[chainId].api.helpers.getTotalSupply(requireLib('llamaApi')),
-  validationSuite: llamaApiValidationSuite,
+  queryKey: () => ['getCrvusdTotalSupplyNumber'] as const,
+  queryFn: async () => {
+    const resp = await fetch('https://api.curve.finance/api/getCrvusdTotalSupplyNumber')
+    return decimal(await resp.text())
+  },
+  validationSuite: EmptyValidationSuite,
 })
