e32fcbe905ecfde2dd5dbc5c9566982854613a3e
Alunara <alunara@curve.finance>
2025-10-17 15:35:07 +0200

    refactor: use decimal function

--- packages/curve-ui-kit/src/shared/ui/LargeTokenInput.tsx
+++ packages/curve-ui-kit/src/shared/ui/LargeTokenInput.tsx
@@ -8,7 +8,7 @@ import { useUniqueDebounce } from '@ui-kit/hooks/useDebounce'
 import { t } from '@ui-kit/lib/i18n'
 import { Duration, TransitionFunction } from '@ui-kit/themes/design/0_primitives'
 import { SizesAndSpaces } from '@ui-kit/themes/design/1_sizes_spaces'
-import { formatNumber, type Decimal } from '@ui-kit/utils'
+import { decimal, formatNumber, type Decimal } from '@ui-kit/utils'
 import { Balance, type Props as BalanceProps } from './Balance'
 import { NumericTextField } from './NumericTextField'
 import { TradingSlider } from './TradingSlider'
@@ -293,13 +293,13 @@ export const LargeTokenInput = ({
     (newBalance: string | undefined) => {
       // The number may not be a valid number, e.g. if the user is typing "5."
       // If so, cancel the debounce such that the input won't reset to the last valid value (5)
-      const bigNum = new BigNumber(newBalance!)
-      if (bigNum.isNaN()) {
+      if (decimal(newBalance) == null) {
         cancelSetBalance()
         return
       }
 
-      // We're treating the newBalance string as a Decimal to keep original formatting such as "5." or "0.00".
+      // We're treating the newBalance string as a Decimal to keep original formatting such as "5." or "0.00",
+      // given that bignumber.js removes trailing decimal points and zeros. Usually good, but not in this case.
       // We've already verified above that it's a valid number.
       const decimalBalance = newBalance as Decimal
       setBalance(decimalBalance)
--- packages/curve-ui-kit/src/shared/ui/TradingSlider.tsx
+++ packages/curve-ui-kit/src/shared/ui/TradingSlider.tsx
@@ -4,7 +4,7 @@ import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import { CLASS_BORDERLESS, SLIDER_BACKGROUND_VAR } from '@ui-kit/themes/components/slider'
 import { SizesAndSpaces } from '@ui-kit/themes/design/1_sizes_spaces'
-import { type Decimal } from '@ui-kit/utils'
+import { decimal, type Decimal } from '@ui-kit/utils'
 import { NumericTextField } from './NumericTextField'
 
 const { Spacing } = SizesAndSpaces
@@ -59,11 +59,7 @@ export const TradingSlider = ({ percentage, onChange, onCommit, step = 1, textAl
       value={percentage}
       min="0"
       max="100"
-      onChange={(newPercentage) => {
-        // Temporary values may not be valid bignums, and we only want to trigger the change event when it is.
-        const bigNum = new BigNumber(newPercentage!)
-        return onChange?.(bigNum.isNaN() ? undefined : (bigNum.toString() as Decimal))
-      }}
+      onChange={(newPercentage) => onChange?.(decimal(newPercentage))}
       onBlur={(newPercentage) => onCommit?.(newPercentage)}
       disabled={disabled}
       slotProps={{
