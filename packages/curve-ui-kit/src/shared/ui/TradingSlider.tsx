19ba6638b51e7b2875f9edb7eff1d243e3ccc8bc
Pearce <pierre.vinet17@gmail.com>
2025-10-22 17:09:45 +0200

    fix: trading slider onChange Decimal type

--- packages/curve-ui-kit/src/shared/ui/TradingSlider.tsx
+++ packages/curve-ui-kit/src/shared/ui/TradingSlider.tsx
@@ -37,8 +37,8 @@ export const TradingSlider = ({ percentage, onChange, onCommit, step = 1, textAl
       railBackground="danger"
       size="medium"
       value={percentage ? +percentage : 0}
-      onChange={(_event, newValue) => onChange?.(Array.isArray(newValue) ? newValue[0] : newValue)}
-      onChangeCommitted={(_event, newValue) => onCommit?.(Array.isArray(newValue) ? newValue[0] : newValue)}
+      onChange={(_event, newValue) => onChange?.(Array.isArray(newValue) ? `${newValue[0]}` : `${newValue}`)}
+      onChangeCommitted={(_event, newValue) => onCommit?.(Array.isArray(newValue) ? `${newValue[0]}` : `${newValue}`)}
       min={0}
       max={100}
       step={step}
