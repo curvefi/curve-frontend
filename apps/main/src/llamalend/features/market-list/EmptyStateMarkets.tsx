8cd3a7a71eb808d6b12978c9e62af52bd0a70fb7
Pearce <pierre.vinet17@gmail.com>
2025-11-06 14:35:09 +0100

    chore: format + lint

--- apps/main/src/llamalend/features/market-list/EmptyStateMarkets.tsx
+++ apps/main/src/llamalend/features/market-list/EmptyStateMarkets.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import { ReactNode } from 'react'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import { LlamaIcon } from '@ui-kit/shared/icons/LlamaIcon'
@@ -19,24 +19,24 @@ export const EmptyStateMarkets = ({
   subtitle?: string
   action?: ReactNode
 }) => (
-    <EmptyStateRow table={table}>
-      <Stack direction="row" justifyContent="center">
-        {/* Needed to be centered in the row because of the max width */}
-        <Stack direction="column" gap={Spacing.sm} alignItems="center" sx={{ maxWidth: '440px' }}>
-          <LlamaIcon sx={{ width: IconSize.xxl, height: IconSize.xxl }} />
-          {/* Needed because no gap between the title and subtitle */}
-          <Stack direction="column">
-            <Typography component="span" variant="headingSBold">
-              {title}
+  <EmptyStateRow table={table}>
+    <Stack direction="row" justifyContent="center">
+      {/* Needed to be centered in the row because of the max width */}
+      <Stack direction="column" gap={Spacing.sm} alignItems="center" sx={{ maxWidth: '440px' }}>
+        <LlamaIcon sx={{ width: IconSize.xxl, height: IconSize.xxl }} />
+        {/* Needed because no gap between the title and subtitle */}
+        <Stack direction="column">
+          <Typography component="span" variant="headingSBold">
+            {title}
+          </Typography>
+          {subtitle && (
+            <Typography component="span" variant="bodyMRegular" color="text.secondary">
+              {subtitle}
             </Typography>
-            {subtitle && (
-              <Typography component="span" variant="bodyMRegular" color="text.secondary">
-                {subtitle}
-              </Typography>
-            )}
-          </Stack>
-          {action}
+          )}
         </Stack>
+        {action}
       </Stack>
-    </EmptyStateRow>
-  )
+    </Stack>
+  </EmptyStateRow>
+)
