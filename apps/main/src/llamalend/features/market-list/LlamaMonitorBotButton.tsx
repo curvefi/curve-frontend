a2cc7caed345fcc728ca8a501d8d091e103e82d6
Pearce <pierre.vinet17@gmail.com>
2025-11-07 13:59:05 +0100

    feat: bell icon to llama monitor bot

--- apps/main/src/llamalend/features/market-list/LlamaMonitorBotButton.tsx
+++ apps/main/src/llamalend/features/market-list/LlamaMonitorBotButton.tsx
@@ -2,21 +2,23 @@ import Button from '@mui/material/Button'
 import Link from '@mui/material/Link'
 import { t } from '@ui-kit/lib/i18n'
 import { ArrowTopRightIcon } from '@ui-kit/shared/icons/ArrowTopRightIcon'
+import { BellIcon } from '@ui-kit/shared/icons/BellIcon'
 
 // TODO: move it to constants / links file
 const LLAMA_MONITOR_BOT_URL = 'https://t.me/LlamalendMonitorBot'
 
 export const LlamaMonitorBotButton = () => (
-    <Button
-      color="ghost"
-      size="extraSmall"
-      component={Link}
-      sx={{ textDecoration: 'underline', textUnderlineOffset: '2px', '&:hover': { textDecoration: 'underline' } }}
-      endIcon={<ArrowTopRightIcon />}
-      href={LLAMA_MONITOR_BOT_URL}
-      target="_blank"
-      rel="noreferrer noopener"
-    >
-      {t`Get notified with llamamonitorbot`}
-    </Button>
-  )
+  <Button
+    color="ghost"
+    size="extraSmall"
+    component={Link}
+    sx={{ textDecoration: 'underline', textUnderlineOffset: '2px', '&:hover': { textDecoration: 'underline' } }}
+    startIcon={<BellIcon fontSize={'small'} />}
+    endIcon={<ArrowTopRightIcon fontSize={'small'} />}
+    href={LLAMA_MONITOR_BOT_URL}
+    target="_blank"
+    rel="noreferrer noopener"
+  >
+    {t`Get notified with llamamonitorbot`}
+  </Button>
+)
