9caf965d260d1d4c542c377455e98fce77b7f634
Pearce <pierre.vinet17@gmail.com>
2025-11-07 17:18:41 +0100

    feat: llamabot button link for mobile

--- apps/main/src/llamalend/features/market-list/LlamaMonitorBotButton.tsx
+++ apps/main/src/llamalend/features/market-list/LlamaMonitorBotButton.tsx
@@ -1,24 +1,42 @@
 import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
 import Link from '@mui/material/Link'
+import { useIsMobile } from '@ui-kit/hooks/useBreakpoints'
 import { t } from '@ui-kit/lib/i18n'
 import { ArrowTopRightIcon } from '@ui-kit/shared/icons/ArrowTopRightIcon'
 import { BellIcon } from '@ui-kit/shared/icons/BellIcon'
+import { SizesAndSpaces } from '@ui-kit/themes/design/1_sizes_spaces'
 
+const { Spacing } = SizesAndSpaces
 // TODO: move it to constants / links file
 const LLAMA_MONITOR_BOT_URL = 'https://t.me/LlamalendMonitorBot'
 
-export const LlamaMonitorBotButton = () => (
-  <Button
-    color="ghost"
-    size="extraSmall"
-    component={Link}
-    sx={{ textDecoration: 'underline', textUnderlineOffset: '2px', '&:hover': { textDecoration: 'underline' } }}
-    startIcon={<BellIcon fontSize={'small'} />}
-    endIcon={<ArrowTopRightIcon fontSize={'small'} />}
-    href={LLAMA_MONITOR_BOT_URL}
-    target="_blank"
-    rel="noreferrer noopener"
-  >
-    {t`Get notified with llamamonitorbot`}
-  </Button>
-)
+export const LlamaMonitorBotButton = () => {
+  const isMobile = useIsMobile()
+  return isMobile ? (
+    <IconButton
+      href={LLAMA_MONITOR_BOT_URL}
+      target="_blank"
+      rel="noreferrer noopener"
+      component={Link}
+      size="extraSmall"
+      sx={{ marginInlineEnd: Spacing.lg }}
+    >
+      <BellIcon />
+    </IconButton>
+  ) : (
+    <Button
+      color="ghost"
+      size="extraSmall"
+      component={Link}
+      sx={{ textDecoration: 'underline', textUnderlineOffset: '2px', '&:hover': { textDecoration: 'underline' } }}
+      startIcon={<BellIcon fontSize={'small'} />}
+      endIcon={<ArrowTopRightIcon fontSize={'small'} />}
+      href={LLAMA_MONITOR_BOT_URL}
+      target="_blank"
+      rel="noreferrer noopener"
+    >
+      {t`Get notified with llamamonitorbot`}
+    </Button>
+  )
+}
