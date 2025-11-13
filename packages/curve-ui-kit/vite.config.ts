f36fdac70451a58dc4cbf329bc3afb1d5be8b837
Alunara <alunara@curve.finance>
2025-11-13 23:46:06 +0100

    fix: storybook unable to import @/llamalend and such

--- packages/curve-ui-kit/vite.config.ts
+++ packages/curve-ui-kit/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      '@': resolve(__dirname, '../../apps/main/src'),
       '@ui': resolve(__dirname, '../ui/src'),
       '@ui-kit': resolve(__dirname, './src'),
       '@external-rewards': resolve(__dirname, '../external-rewards/src/index.ts'),
