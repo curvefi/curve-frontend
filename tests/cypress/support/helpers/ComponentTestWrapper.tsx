14fa9b41dbd5c01accffbc5f0ce2c3eb99ab66bf
Alunara <alunara@curve.finance>
2025-11-14 21:03:08 +0100

    chore: add wallet toast in component tests so notify() works

--- tests/cypress/support/helpers/ComponentTestWrapper.tsx
+++ tests/cypress/support/helpers/ComponentTestWrapper.tsx
@@ -1,6 +1,8 @@
 import { type ReactElement } from 'react'
 import { WagmiProvider, type ResolvedRegister } from 'wagmi'
 import { createMemoryHistory, createRootRoute, createRouter, RouterProvider } from '@tanstack/react-router'
+import { WalletToast } from '@ui-kit/features/connect-wallet'
+import { useLayoutStore } from '@ui-kit/features/layout'
 import { persister, queryClient, QueryProvider } from '@ui-kit/lib/api'
 import { ThemeProvider } from '@ui-kit/shared/ui/ThemeProvider'
 import { WithWrapper } from '@ui-kit/shared/ui/WithWrapper'
@@ -25,12 +27,20 @@ export function ComponentTestWrapper({ config, children, autoConnect }: Props) {
   // Create a minimal router for testing environment
   const router = createRouter({
     routeTree: createRootRoute({
-      component: () => children,
+      component: () => (
+        <>
+          <WalletToast />
+          {children}
+        </>
+      ),
     }),
     history: createMemoryHistory({
       initialEntries: ['/'],
     }),
   })
+
+  const setNavHeight = useLayoutStore((state) => state.setNavHeight)
+  setNavHeight(0) // No header in component tests, so wallet toast should be at top
 
   return (
     <>
