b9b687827a950a0f5ce3027b5ee6a977ca69c1d2
Alunara <alunara@curve.finance>
2025-11-15 16:23:37 +0100

    refactor: move hook usage out of ScrollUpButton

--- apps/main/src/routes/GlobalLayout.tsx
+++ apps/main/src/routes/GlobalLayout.tsx
@@ -10,13 +10,14 @@ import type { IChainId as LlamaChainId } from '@curvefi/llamalend-api/lib/interf
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import type { NetworkDef, NetworkMapping } from '@ui/utils'
+import { useLayoutStore } from '@ui-kit/features/layout'
 import { t } from '@ui-kit/lib/i18n'
 import { APP_LINK, AppMenuOption, type AppName } from '@ui-kit/shared/routes'
+import { ScrollUpButton } from '@ui-kit/shared/ui/ScrollUpButton'
 import { SizesAndSpaces } from '@ui-kit/themes/design/1_sizes_spaces'
 import { ErrorBoundary } from '@ui-kit/widgets/ErrorBoundary'
 import { Footer } from '@ui-kit/widgets/Footer'
 import { Header as Header } from '@ui-kit/widgets/Header'
-import { ScrollUpButton } from '@ui-kit/widgets/ScrollUpButton'
 
 const { MinHeight } = SizesAndSpaces
 
@@ -85,6 +86,6 @@ export const GlobalLayout = <TId extends string, TChainId extends number>({
       <ErrorBoundary title={t`Page error`}>{children}</ErrorBoundary>
     </Box>
     <Footer appName={currentApp} networkId={network.id} />
-    <ScrollUpButton />
+    <ScrollUpButton visible={useLayoutStore((state) => state.showScrollButton)} />
   </Stack>
 )
