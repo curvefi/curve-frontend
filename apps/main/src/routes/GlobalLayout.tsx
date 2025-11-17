c439cc4e2ef539a12249b841871dfa0213ebff32
Alunara <alunara@curve.finance>
2025-11-17 14:27:19 +0100

    feat: use llamalend app stats in lend

--- apps/main/src/routes/GlobalLayout.tsx
+++ apps/main/src/routes/GlobalLayout.tsx
@@ -1,11 +1,9 @@
 import { type ReactNode } from 'react'
 import daoNetworks from '@/dao/networks'
 import { useDexAppStats, useDexRoutes } from '@/dex/hooks/useDexAppStats'
-import { useLendAppStats } from '@/lend/hooks/useLendAppStats'
 import lendNetworks from '@/lend/networks'
 import { useLlamalendAppStats } from '@/llamalend/hooks/useLlamalendAppStats'
 import crvusdNetworks from '@/loan/networks'
-import type { IChainId as LlamaChainId } from '@curvefi/llamalend-api/lib/interfaces'
 import Box from '@mui/material/Box'
 import Stack from '@mui/material/Stack'
 import type { NetworkDef, NetworkMapping } from '@ui/utils'
@@ -24,7 +22,7 @@ const useAppStats = (currentApp: string, network: NetworkDef) =>
   ({
     dao: [],
     crvusd: useLlamalendAppStats({ chainId: network?.chainId }, currentApp === 'crvusd' && network?.chainId === 1),
-    lend: useLendAppStats(currentApp === 'lend' ? (network?.chainId as LlamaChainId) : undefined),
+    lend: useLlamalendAppStats({ chainId: network?.chainId }, currentApp === 'lend'),
     llamalend: useLlamalendAppStats({ chainId: network?.chainId }, currentApp === 'llamalend'),
     dex: useDexAppStats(currentApp === 'dex' ? network : undefined),
   })[currentApp]
