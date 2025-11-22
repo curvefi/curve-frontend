a6273461c0eb9d159536bfe50e864b461c557b42
Alunara <alunara@curve.finance>
2025-11-22 17:40:14 +0100

    refactor: move getChartBandBalancesData to apiCrvusd

--- apps/main/src/loan/lib/apiCrvusd.ts
+++ apps/main/src/loan/lib/apiCrvusd.ts
@@ -4,10 +4,10 @@ import type { MaxRecvLeverage as MaxRecvLeverageForm } from '@/loan/components/P
 import type { FormDetailInfo as FormDetailInfoDeleverage } from '@/loan/components/PageLoanManage/LoanDeleverage/types'
 import networks from '@/loan/networks'
 import type { LiqRange, MaxRecvLeverage, Provider } from '@/loan/store/types'
-import { ChainId, LlamaApi, Llamma, UserLoanDetails } from '@/loan/types/loan.types'
+import { ChainId, LlamaApi, Llamma, UserLoanDetails, type BandBalance } from '@/loan/types/loan.types'
 import { fulfilledValue, getErrorMessage, log } from '@/loan/utils/helpers'
-import { getChartBandBalancesData } from '@/loan/utils/utilsCurvejs'
 import type { TGas } from '@curvefi/llamalend-api/lib/interfaces'
+import PromisePool from '@supercharge/promise-pool'
 import { BN } from '@ui/utils'
 import { waitForTransaction, waitForTransactions } from '@ui-kit/lib/ethers'
 
@@ -50,6 +50,56 @@ function parseUserLoss(userLoss: UserLoanDetails['userLoss']) {
   resp.loss_pct = resp.loss_pct && BN(resp.loss_pct).isLessThan(smallAmount) ? '0' : userLoss.loss_pct
 
   return resp
+}
+
+async function getChartBandBalancesData(
+  {
+    bandBalances,
+    bandBalancesArr,
+  }: {
+    bandBalancesArr: { stablecoin: string; collateral: string; band: string }[]
+    bandBalances: BandBalance
+  },
+  liquidationBand: number | null,
+  llamma: Llamma,
+) {
+  // filter out bands that doesn't have stablecoin and collaterals
+  const ns = bandBalancesArr
+    .filter((b) => {
+      const { stablecoin, collateral } = bandBalances[b.band] ?? {}
+      return +stablecoin > 0 || +collateral > 0
+    })
+    .map((b) => b.band)
+
+  // TODO: handle errors
+  const { results } = await PromisePool.for(ns).process(async (n) => {
+    const { collateral, stablecoin } = bandBalances[n]
+    const [p_up, p_down] = await llamma.calcBandPrices(+n)
+    const sqrt = new BN(p_up).multipliedBy(p_down).squareRoot()
+    const pUpDownMedian = new BN(p_up).plus(p_down).dividedBy(2).toFixed(5)
+    const collateralUsd = new BN(collateral).multipliedBy(sqrt)
+
+    return {
+      collateral,
+      collateralUsd: collateralUsd.toString(),
+      isLiquidationBand: liquidationBand ? (liquidationBand === +n ? 'SL' : '') : '',
+      isOraclePriceBand: false, // update this with detail info oracle price
+      isNGrouped: false,
+      n,
+      p_up,
+      p_down,
+      pUpDownMedian,
+      stablecoin,
+      collateralStablecoinUsd: collateralUsd.plus(stablecoin).toNumber(),
+    }
+  })
+
+  const parsedBandBalances = []
+  for (const idx in results) {
+    const r = results[idx]
+    parsedBandBalances.unshift(r)
+  }
+  return parsedBandBalances
 }
 
 // need due to issue with chart label not showing up correctly.
