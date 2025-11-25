6c1cdb67c1c94c4f41e3c3f3f16a2926772f7a98
JustJousting <justjousting2moon@gmail.com>
2025-11-25 21:59:34 +0200

    Revert "refactor: re-run figma json export with script to apply figma updates"
    This reverts commit c4e06f1f2dfa08f2e75227ba2378ea5957be709f.

--- packages/curve-ui-kit/src/themes/design/1_sizes_spaces.ts
+++ packages/curve-ui-kit/src/themes/design/1_sizes_spaces.ts
@@ -1,54 +1,52 @@
 import { Sizing, Spacing } from './0_primitives'
 
 const MappedSpacing = {
-  '450': { mobile: Spacing['450'], tablet: Spacing['450'], desktop: Spacing['450'] },
-  xs: { mobile: Spacing['200'], tablet: Spacing['200'], desktop: Spacing['200'] },
-  sm: { mobile: Spacing['300'], tablet: Spacing['300'], desktop: Spacing['300'] },
-  md: { mobile: Spacing['400'], tablet: Spacing['400'], desktop: Spacing['400'] },
-  lg: { mobile: Spacing['500'], tablet: Spacing['500'], desktop: Spacing['500'] },
-  xl: { mobile: Spacing['600'], tablet: Spacing['600'], desktop: Spacing['600'] },
-  xxl: { mobile: Spacing['700'], tablet: Spacing['700'], desktop: Spacing['700'] },
-  xxs: { mobile: Spacing['100'], tablet: Spacing['100'], desktop: Spacing['100'] },
-  '3xl': { mobile: Spacing['800'], tablet: Spacing['800'], desktop: Spacing['800'] },
   '3xs': { mobile: Spacing['75'], tablet: Spacing['75'], desktop: Spacing['75'] },
+  xxs: { mobile: Spacing['100'], tablet: Spacing['100'], desktop: Spacing['100'] },
+  xs: { mobile: Spacing['100'], tablet: Spacing['200'], desktop: Spacing['200'] },
+  sm: { mobile: Spacing['200'], tablet: Spacing['300'], desktop: Spacing['300'] },
+  md: { mobile: Spacing['300'], tablet: Spacing['400'], desktop: Spacing['400'] },
+  lg: { mobile: Spacing['400'], tablet: Spacing['500'], desktop: Spacing['500'] },
+  xl: { mobile: Spacing['500'], tablet: Spacing['600'], desktop: Spacing['600'] },
+  xxl: { mobile: Spacing['600'], tablet: Spacing['700'], desktop: Spacing['700'] },
+  '3xl': { mobile: Spacing['700'], tablet: Spacing['800'], desktop: Spacing['800'] },
 } as const
 
 const MappedSizing = {
-  '450': { mobile: Sizing['450'], tablet: Sizing['450'], desktop: Sizing['450'] },
-  xxs: { mobile: Sizing['100'], tablet: Sizing['100'], desktop: Sizing['100'] },
-  xs: { mobile: Sizing['200'], tablet: Sizing['200'], desktop: Sizing['200'] },
-  sm: { mobile: Sizing['300'], tablet: Sizing['300'], desktop: Sizing['300'] },
-  md: { mobile: Sizing['400'], tablet: Sizing['400'], desktop: Sizing['400'] },
-  lg: { mobile: Sizing['500'], tablet: Sizing['500'], desktop: Sizing['500'] },
-  xl: { mobile: Sizing['600'], tablet: Sizing['600'], desktop: Sizing['600'] },
-  xxl: { mobile: Sizing['700'], tablet: Sizing['700'], desktop: Sizing['700'] },
-  '3xl': { mobile: Sizing['800'], tablet: Sizing['800'], desktop: Sizing['800'] },
+  xxs: { mobile: Sizing['50'], tablet: Sizing['100'], desktop: Sizing['100'] },
+  xs: { mobile: Sizing['150'], tablet: Sizing['200'], desktop: Sizing['200'] },
+  sm: { mobile: Sizing['200'], tablet: Sizing['300'], desktop: Sizing['300'] },
+  md: { mobile: Sizing['300'], tablet: Sizing['400'], desktop: Sizing['400'] },
+  lg: { mobile: Sizing['400'], tablet: Sizing['500'], desktop: Sizing['500'] },
+  xl: { mobile: Sizing['500'], tablet: Sizing['600'], desktop: Sizing['600'] },
+  xxl: { mobile: Sizing['600'], tablet: Sizing['700'], desktop: Sizing['700'] },
+  '3xl': { mobile: Sizing['700'], tablet: Sizing['800'], desktop: Sizing['800'] },
 } as const
 
 const MappedIconSize = {
-  xxs: { mobile: Sizing['125'], tablet: Sizing['125'], desktop: Sizing['125'] },
-  xs: { mobile: Sizing['150'], tablet: Sizing['150'], desktop: Sizing['150'] },
-  sm: { mobile: Sizing['200'], tablet: Sizing['200'], desktop: Sizing['200'] },
+  xxs: { mobile: Sizing['100'], tablet: Sizing['125'], desktop: Sizing['125'] },
+  xs: { mobile: Sizing['125'], tablet: Sizing['150'], desktop: Sizing['150'] },
+  sm: { mobile: Sizing['150'], tablet: Sizing['200'], desktop: Sizing['200'] },
   md: { mobile: Sizing['250'], tablet: Sizing['250'], desktop: Sizing['250'] },
-  lg: { mobile: Sizing['350'], tablet: Sizing['350'], desktop: Sizing['350'] },
-  xl: { mobile: Sizing['500'], tablet: Sizing['500'], desktop: Sizing['500'] },
-  xxl: { mobile: Sizing['600'], tablet: Sizing['600'], desktop: Sizing['600'] },
-  '3xl': { mobile: Sizing['700'], tablet: Sizing['700'], desktop: Sizing['700'] },
-  '4xl': { mobile: Sizing['800'], tablet: Sizing['800'], desktop: Sizing['800'] },
+  lg: { mobile: Sizing['300'], tablet: Sizing['350'], desktop: Sizing['350'] },
+  xl: { mobile: Sizing['400'], tablet: Sizing['500'], desktop: Sizing['500'] },
+  xxl: { mobile: Sizing['500'], tablet: Sizing['600'], desktop: Sizing['600'] },
+  '3xl': { mobile: Sizing['600'], tablet: Sizing['700'], desktop: Sizing['700'] },
+  '4xl': { mobile: Sizing['700'], tablet: Sizing['800'], desktop: Sizing['800'] },
 } as const
 
 const MappedGrid = {
-  Column_Spacing: { mobile: Spacing['400'], tablet: Spacing['400'], desktop: Spacing['400'] },
-  Row_Spacing: { mobile: Spacing['400'], tablet: Spacing['400'], desktop: Spacing['400'] },
+  Column_Spacing: { mobile: Spacing['300'], tablet: Spacing['400'], desktop: Spacing['500'] },
+  Row_Spacing: { mobile: Spacing['300'], tablet: Spacing['400'], desktop: Spacing['500'] },
 } as const
 
 const MappedFontSize = {
   xs: { mobile: Sizing['125'], tablet: Sizing['125'], desktop: Sizing['125'] },
   sm: { mobile: Sizing['150'], tablet: Sizing['150'], desktop: Sizing['150'] },
   md: { mobile: Sizing['200'], tablet: Sizing['200'], desktop: Sizing['200'] },
-  lg: { mobile: Sizing['300'], tablet: Sizing['300'], desktop: Sizing['300'] },
-  xl: { mobile: Sizing['400'], tablet: Sizing['400'], desktop: Sizing['400'] },
-  xxl: { mobile: Sizing['600'], tablet: Sizing['600'], desktop: Sizing['600'] },
+  lg: { mobile: Sizing['250'], tablet: Sizing['300'], desktop: Sizing['300'] },
+  xl: { mobile: Sizing['350'], tablet: Sizing['400'], desktop: Sizing['400'] },
+  xxl: { mobile: Sizing['500'], tablet: Sizing['600'], desktop: Sizing['700'] },
 } as const
 
 const MappedButtonSize = {
@@ -68,30 +66,54 @@ const SliderThumbWidth = {
 } as const
 
 const MappedFontWeight = {
-  'Extra light': 200,
+  Extra_Light: 200,
   Light: 300,
   Normal: 400,
   Medium: 500,
-  'Semi bold': 600,
+  Semi_Bold: 600,
   Bold: 700,
-  'Extra bold': 800,
+  Extra_Bold: 800,
 } as const
 
 const MappedLineHeight = {
-  xs: { mobile: '0.75rem', /* 12px */ tablet: '0.75rem', /* 12px */ desktop: '0.75rem' /* 12px */ },
-  sm: { mobile: '0.875rem', /* 14px */ tablet: '0.875rem', /* 14px */ desktop: '0.875rem' /* 14px */ },
-  md: { mobile: '1.5rem', /* 24px */ tablet: '1.5rem', /* 24px */ desktop: '1.5rem' /* 24px */ },
-  lg: { mobile: '1.5rem', /* 24px */ tablet: '1.5rem', /* 24px */ desktop: '1.5rem' /* 24px */ },
-  xl: { mobile: '2rem', /* 32px */ tablet: '2rem', /* 32px */ desktop: '2rem' /* 32px */ },
-  xxl: { mobile: '2.5rem', /* 40px */ tablet: '2.5rem', /* 40px */ desktop: '2.5rem' /* 40px */ },
+  xs: {
+    mobile: '0.75rem', // 12px
+    tablet: '0.75rem', // 12px
+    desktop: '0.875rem', // 14px
+  },
+  sm: {
+    mobile: '0.875rem', // 14px
+    tablet: '0.875rem', // 14px
+    desktop: '1rem', // 16px
+  },
+  md: {
+    mobile: '1.5rem', // 24px
+    tablet: '1.5rem', // 24px
+    desktop: '1.5rem', // 24px
+  },
+  lg: {
+    mobile: '1.5rem', // 24px
+    tablet: '1.5rem', // 24px
+    desktop: '2rem', // 32px
+  },
+  xl: {
+    mobile: '2rem', // 32px
+    tablet: '2rem', // 32px
+    desktop: '2.5rem', // 40px
+  },
+  xxl: {
+    mobile: '2.5rem', // 40px
+    tablet: '2.5rem', // 40px
+    desktop: '4rem', // 64px
+  },
 } as const
 
 const MappedModalWidth = {
-  xs: '19rem' /* 304px */,
-  sm: '24rem' /* 384px */,
-  md: '28rem' /* 448px */,
-  lg: '32rem' /* 512px */,
-  xl: '36rem' /* 576px */,
+  xs: '19rem', // 304px
+  sm: '24rem', // 384px
+  md: '28rem', // 448px
+  lg: '32rem', // 512px
+  xl: '36rem', // 576px
 }
 
 const MappedModalHeight = {
@@ -108,7 +130,7 @@ export const SizesAndSpaces = {
   FontSize: MappedFontSize,
   FontWeight: MappedFontWeight,
   LineHeight: MappedLineHeight,
-  OutlineWidth: '0.125rem' /* 2px */,
+  OutlineWidth: '0.125rem', // 2px
   Slider: { Height: SliderHeight, ThumbWidth: SliderThumbWidth },
   Width: { modal: MappedModalWidth, column: MappedColumnWidth },
   MinWidth: { tableHeader: '50rem', select: '5rem', actionCard: '20rem', twoCardLayout: 961 },
