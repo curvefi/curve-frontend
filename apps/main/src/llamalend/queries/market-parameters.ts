faa54bfd517fced7580cdc4f8993af13876dee94
Alunara <alunara@curve.finance>
2025-11-27 16:40:23 +0100

    refactor: shorten code with ternary

--- apps/main/src/llamalend/queries/market-parameters.ts
+++ apps/main/src/llamalend/queries/market-parameters.ts
@@ -9,17 +9,12 @@ export const { useQuery: useMarketParameters } = queryFactory({
   queryKey: (params: MarketParams) => [...rootKeys.market(params), 'market-parameters'] as const,
   queryFn: async ({ marketId }: MarketQuery) => {
     const market = getLlamaMarket(marketId)
-
-    if (market instanceof MintMarketTemplate) {
-      // Mint markets don't have their A value in the parameters
-      // object, but we want it regardless for type similarity
-      // with lend markets.
-      const parameters = await market.stats.parameters()
-      return { ...parameters, A: market.A.toString() }
-    } else {
-      const parameters = await market.stats.parameters()
-      return parameters
-    }
+    // Mint markets don't have their A value in the parameters
+    // object, but we want it regardless for type similarity
+    // with lend markets.
+    return market instanceof MintMarketTemplate
+      ? { ...(await market.stats.parameters()), A: market.A.toString() }
+      : await market.stats.parameters()
   },
   refetchInterval: '1m',
   validationSuite: marketIdValidationSuite,
