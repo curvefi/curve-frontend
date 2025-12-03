39d7ff5c61ec3bdbf3ebe28b68b469d7791c5b37
Daniel Schiavini <daniel.schiavini@gmail.com>
2025-12-03 11:01:52 +0100

    refactor: move lend market pages together

--- apps/main/src/routes/lend.routes.tsx
+++ apps/main/src/routes/lend.routes.tsx
@@ -1,13 +1,13 @@
 import '@/global-extensions'
 import PageIntegrations from '@/lend/components/PageIntegrations/Page'
-import PageLoanCreate from '@/lend/components/PageLoanCreate/Page'
-import PageLoanManage from '@/lend/components/PageLoanManage/Page'
+import { LendMarketPage } from '@/lend/components/PageLendMarket/LendMarketPage'
 import PageVault from '@/lend/components/PageVault/Page'
+import { LendLayout } from '@/lend/LendLayout'
+import type { MarketUrlParams } from '@/lend/types/lend.types'
 import Skeleton from '@mui/material/Skeleton'
 import { createRoute } from '@tanstack/react-router'
 import { SizesAndSpaces } from '@ui-kit/themes/design/1_sizes_spaces'
 import { LegalPage } from '@ui-kit/widgets/Legal'
-import { LendLayout } from '../lend/LendLayout'
 import { rootRoute } from './root.routes'
 import { redirectTo } from './util'
 
@@ -20,6 +20,9 @@ const lendLayoutRoute = createRoute({
 })
 
 const layoutProps = { getParentRoute: () => lendLayoutRoute }
+
+const RedirectToMarketPage = ({ params: { network, market } }: { params: MarketUrlParams }) =>
+  redirectTo(`/lend/${network}/markets/${market}/manage`)
 
 export const lendRoutes = lendLayoutRoute.addChildren([
   createRoute({
@@ -63,35 +66,14 @@ export const lendRoutes = lendLayoutRoute.addChildren([
   }),
   createRoute({
     path: '$network/markets/$market',
-    loader: ({ params: { network, market } }) => redirectTo(`/lend/${network}/markets/${market}/manage`),
-    ...layoutProps,
-  }),
-  createRoute({
-    path: '$network/markets/$market/create/$formType',
-    loader: ({ params: { network, market } }) => redirectTo(`/lend/${network}/markets/${market}/create`),
-    ...layoutProps,
-  }),
-  createRoute({
-    path: '$network/markets/$market/create',
-    component: PageLoanCreate,
+    component: LendMarketPage,
     head: ({ params }) => ({
-      meta: [{ title: `Create - ${params.market} - Curve Llamalend` }],
+      meta: [{ title: `${params.market} - Curve Llamalend` }],
     }),
     ...layoutProps,
   }),
-  createRoute({
-    path: '$network/markets/$market/manage/$formType',
-    loader: ({ params: { network, market } }) => redirectTo(`/lend/${network}/markets/${market}/manage`),
-    ...layoutProps,
-  }),
-  createRoute({
-    path: '$network/markets/$market/manage',
-    component: PageLoanManage,
-    head: ({ params }) => ({
-      meta: [{ title: `Manage - ${params.market} - Curve Llamalend` }],
-    }),
-    ...layoutProps,
-  }),
+  createRoute({ path: '$network/markets/$market/create/{$-formType}', loader: RedirectToMarketPage, ...layoutProps }),
+  createRoute({ path: '$network/markets/$market/manage/{$-formType}', loader: RedirectToMarketPage, ...layoutProps }),
   createRoute({
     path: '$network/markets/$market/vault/$formType',
     loader: ({ params: { network, market } }) => redirectTo(`/lend/${network}/markets/${market}/vault`),
