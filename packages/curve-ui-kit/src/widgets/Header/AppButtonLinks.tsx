cd5e5fee9ab51b45f0bd918bed57fd9486465d92
Alunara <alunara@curve.finance>
2025-12-04 19:24:58 +0100

    refactor: add useVisibleAppLinks hook

--- packages/curve-ui-kit/src/widgets/Header/AppButtonLinks.tsx
+++ packages/curve-ui-kit/src/widgets/Header/AppButtonLinks.tsx
@@ -1,32 +1,26 @@
-import { recordEntries } from '@curvefi/prices-api/objects.util'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
-import { useAnalyticsApp } from '@ui-kit/hooks/useFeatureFlags'
-import { APP_LINK, type AppMenuOption, getInternalUrl } from '@ui-kit/shared/routes'
+import { type AppMenuOption, getInternalUrl } from '@ui-kit/shared/routes'
 import { RouterLink } from '@ui-kit/shared/ui/RouterLink'
+import { useVisibleAppLinks } from './useVisibleAppLinks'
 
 type AppNavAppsProps = { currentMenu: AppMenuOption; onChange: (appName: AppMenuOption) => void; networkId: string }
 
-export const AppButtonLinks = ({ currentMenu, onChange, networkId }: AppNavAppsProps) => {
-  const showAnalytics = useAnalyticsApp()
-  const apps = recordEntries(APP_LINK).filter(([app]) => app !== 'analytics' || showAnalytics)
-
-  return (
-    <Box display="flex" alignItems="center" marginX={[2, 3, 4]} gap={2}>
-      {apps.map(([menu, { label, routes }]) => (
-        <Button
-          key={menu}
-          color="navigation"
-          size="small"
-          className={currentMenu === menu ? 'current' : ''}
-          component={RouterLink}
-          onClick={() => onChange(menu as AppMenuOption)}
-          href={getInternalUrl(routes[0].app, networkId)}
-          data-testid={`app-link-${menu}`}
-        >
-          {label}
-        </Button>
-      ))}
-    </Box>
-  )
-}
+export const AppButtonLinks = ({ currentMenu, onChange, networkId }: AppNavAppsProps) => (
+  <Box display="flex" alignItems="center" marginX={[2, 3, 4]} gap={2}>
+    {useVisibleAppLinks().map(([menu, { label, routes }]) => (
+      <Button
+        key={menu}
+        color="navigation"
+        size="small"
+        className={currentMenu === menu ? 'current' : ''}
+        component={RouterLink}
+        onClick={() => onChange(menu as AppMenuOption)}
+        href={getInternalUrl(routes[0].app, networkId)}
+        data-testid={`app-link-${menu}`}
+      >
+        {label}
+      </Button>
+    ))}
+  </Box>
+)
