a6e33d94dda1184987d4bd6d26888752ce2011c2
Alunara <alunara@curve.finance>
2025-12-13 16:35:04 +0100

    refactor: get rid of useSignerAddress in favor of wagmi useConnection

--- apps/main/src/dex/entities/signer/lib.ts
+++
@@ -1,8 +0,0 @@
-import type { Address } from 'viem'
-import { useWallet } from '@ui-kit/features/connect-wallet'
-
-export const useSignerAddress = (): { data: Address | undefined } => {
-  const { wallet } = useWallet.getState()
-  const signerAddress = wallet?.account?.address
-  return { data: signerAddress }
-}
