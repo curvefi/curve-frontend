dff0c692d0a90fc3a5640d39c004634dad5b3a3b
Pearce <pierre.vinet17@gmail.com>
2025-12-19 14:43:11 +0100

    refactor: usePoolAlert messages with InlineLink

--- apps/main/src/dex/components/pool-alert-messages.tsx
+++ apps/main/src/dex/components/pool-alert-messages.tsx
@@ -1,7 +1,4 @@
-import Link from '@mui/material/Link'
 import Stack from '@mui/material/Stack'
-import { ArrowTopRightIcon } from '@ui-kit/shared/icons/ArrowTopRightIcon'
-import { RouterLink } from '@ui-kit/shared/ui/RouterLink'
 import { SizesAndSpaces } from '@ui-kit/themes/design/1_sizes_spaces'
 
 const { Spacing } = SizesAndSpaces
@@ -18,24 +15,4 @@ export const PoolAlertMessage = ({ children }: { children: React.ReactNode }) =>
   >
     {children}
   </Stack>
-)
-
-export const ExternalLink = ({ href, children }: { href: string; children: React.ReactNode }) => (
-  <Link
-    sx={{
-      color: 'currentColor',
-      '&:hover': { textDecoration: 'none' },
-    }}
-    href={href}
-    target="_blank"
-    rel="noreferrer noopener"
-  >
-    {children} <ArrowTopRightIcon fontSize={'small'} />
-  </Link>
-)
-
-export const InternalLink = ({ href, children }: { href: string; children: React.ReactNode }) => (
-  <RouterLink href={href} sx={{ color: 'currentColor', '&:hover': { textDecoration: 'none' } }}>
-    {children}
-  </RouterLink>
 )
