9cee90976cc904798f5e2e371f2f0a71044fa0e0
Alunara <alunara@curve.finance>
2025-12-20 17:40:38 +0100

    refactor: deduplicate integrations component for lend and loan

--- packages/curve-ui-kit/src/features/integrations/index.ts
+++ packages/curve-ui-kit/src/features/integrations/index.ts
@@ -1,1 +1,4 @@
 export type { Tag, IntegrationApp, IntegrationsTags } from './types'
+export { Integrations } from './components/Integrations'
+export { useIntegrations } from './queries/integrations'
+export { useIntegrationsTags } from './queries/integrations-tags'
