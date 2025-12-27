9c02f32f141eaef99a66bab4e0fca6f56f881462
Pearce <pierre.vinet17@gmail.com>
2025-12-27 18:32:07 +0100

    feat: current and expected leverage for lend markets

--- apps/main/src/llamalend/queries/add-collateral/add-collateral-future-leverage.query.ts
+++ apps/main/src/llamalend/queries/add-collateral/add-collateral-future-leverage.query.ts
@@ -1,8 +1,14 @@
 import { getLlamaMarket } from '@/llamalend/llama.utils'
+import { LendMarketTemplate } from '@curvefi/llamalend-api/lib/lendMarkets'
 import { queryFactory, rootKeys } from '@ui-kit/lib/model'
+import { decimal } from '@ui-kit/utils/decimal'
 import { type CollateralParams, type CollateralQuery } from '../validation/manage-loan.types'
 import { collateralValidationSuite } from '../validation/manage-loan.validation'
 
+/**
+ * Query to get the user's expected leverage value in a position, after adding userCollateral
+ * Note this is ONLY valid for lend market.
+ */
 export const { useQuery: useAddCollateralFutureLeverage } = queryFactory({
   queryKey: ({ chainId, marketId, userAddress, userCollateral }: CollateralParams) =>
     [
@@ -10,7 +16,12 @@ export const { useQuery: useAddCollateralFutureLeverage } = queryFactory({
       'addCollateralFutureLeverage',
       { userCollateral },
     ] as const,
-  queryFn: async ({ marketId, userCollateral }: CollateralQuery) =>
-    await getLlamaMarket(marketId).addCollateralFutureLeverage(userCollateral),
+  queryFn: async ({ marketId, userCollateral }: CollateralQuery) => {
+    const market = getLlamaMarket(marketId)
+    // TODO: get expected leverage value for new mint markets as well
+    return market instanceof LendMarketTemplate
+      ? (decimal(await market.addCollateralFutureLeverage(userCollateral)) ?? null)
+      : null
+  },
   validationSuite: collateralValidationSuite,
 })
--- apps/main/src/llamalend/queries/user-current-leverage.query.ts
+++ apps/main/src/llamalend/queries/user-current-leverage.query.ts
@@ -1,14 +1,21 @@
+import { LendMarketTemplate } from '@curvefi/llamalend-api/lib/lendMarkets'
 import { queryFactory } from '@ui-kit/lib/model/query/factory'
 import { rootKeys, UserMarketParams, UserMarketQuery } from '@ui-kit/lib/model/query/root-keys'
 import { userMarketValidationSuite } from '@ui-kit/lib/model/query/user-market-validation'
+import { decimal } from '@ui-kit/utils/decimal'
 import { getLlamaMarket } from '../llama.utils'
 
+/**
+ * Query to get the user's current leverage value in a market.
+ * Note this is ONLY valid for lend market.
+ */
 export const { useQuery: useUserCurrentLeverage } = queryFactory({
   queryKey: ({ chainId, userAddress, marketId }: UserMarketParams) =>
     [...rootKeys.userMarket({ chainId, userAddress, marketId }), 'user-current-leverage'] as const,
   queryFn: async ({ marketId, userAddress }: UserMarketQuery) => {
     const market = getLlamaMarket(marketId)
-    return await market.currentLeverage(userAddress)
+    // TODO: get leverage value for new mint markets as well
+    return market instanceof LendMarketTemplate ? (decimal(await market.currentLeverage(userAddress)) ?? null) : null
   },
   refetchInterval: '1m',
   validationSuite: userMarketValidationSuite,
