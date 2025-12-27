0951532204deab80451e30b8eab5ecd6959dd355
Alunara <alunara@curve.finance>
2025-12-27 18:38:22 +0100

    refactor: replace integrations index with the root component itself

--- packages/curve-ui-kit/src/features/integrations/index.ts
+++
@@ -1,4 +0,0 @@
-export type { Tag, IntegrationApp, IntegrationsTags } from './types'
-export { Integrations } from './components/Integrations'
-export { useIntegrations } from './queries/integrations'
-export { useIntegrationsTags } from './queries/integrations-tags'
