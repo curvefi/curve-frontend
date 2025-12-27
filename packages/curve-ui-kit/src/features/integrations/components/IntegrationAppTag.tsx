dcad9bf062ee94974d3f14ed4c74d41e85651a07
Alunara <alunara@curve.finance>
2025-12-27 18:50:15 +0100

    styling: increase distance between app tag color and label

--- packages/curve-ui-kit/src/features/integrations/components/IntegrationAppTag.tsx
+++ packages/curve-ui-kit/src/features/integrations/components/IntegrationAppTag.tsx
@@ -6,7 +6,7 @@ import type { IntegrationTag } from '../types'
 const { Spacing, Sizing } = SizesAndSpaces
 
 export const IntegrationAppTag = ({ tag }: { tag: IntegrationTag }) => (
-  <Stack direction="row" alignItems="center" gap={Spacing.xs}>
+  <Stack direction="row" alignItems="center" gap={Spacing.sm}>
     {tag.color && <Box sx={{ width: Sizing.xs, height: Sizing.xs, backgroundColor: tag.color }} />}
     {tag.displayName}
   </Stack>
