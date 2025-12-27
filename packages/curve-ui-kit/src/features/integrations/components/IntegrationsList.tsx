f2b5d66ac32c2a46e7c8ca6c7a47072ff6aa42cd
Alunara <alunara@curve.finance>
2025-12-27 17:53:03 +0100

    styling: use mui skeleton rather than spinner

--- packages/curve-ui-kit/src/features/integrations/components/IntegrationsList.tsx
+++ packages/curve-ui-kit/src/features/integrations/components/IntegrationsList.tsx
@@ -6,7 +6,6 @@ import MenuItem from '@mui/material/MenuItem'
 import Select from '@mui/material/Select'
 import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
-import Spinner, { SpinnerWrapper } from '@ui/Spinner'
 import { BLOCKCHAIN_LEGACY_NAMES, CURVE_ASSETS_URL, type BaseConfig } from '@ui/utils'
 import { useIntegrations, useIntegrationsTags, type IntegrationApp, type Tag } from '@ui-kit/features/integrations'
 import { useNavigate, useSearchParams } from '@ui-kit/hooks/router'
@@ -17,8 +16,9 @@ import { SelectableChip } from '@ui-kit/shared/ui/SelectableChip'
 import { SizesAndSpaces } from '@ui-kit/themes/design/1_sizes_spaces'
 import IntegrationAppComp from './IntegrationApp'
 import { IntegrationAppTag } from './IntegrationAppTag'
+import { WithSkeleton } from '@ui-kit/shared/ui/WithSkeleton'
 
-const { Spacing, ButtonSize } = SizesAndSpaces
+const { Spacing, Sizing, ButtonSize } = SizesAndSpaces
 
 const NetworkItem = ({ network }: { network?: BaseConfig }) => (
   <Stack direction="row" gap={Spacing.sm} alignItems="center">
@@ -82,84 +82,82 @@ export const IntegrationsList = ({ chainId, networks }: IntegrationsListProps) =
       : list
   }, [filterNetwork, filterTag, integrations, searchText])
 
-  return isLoading ? (
-    <SpinnerWrapper>
-      <Spinner />
-    </SpinnerWrapper>
-  ) : (
-    <Stack direction="column" gap={Spacing.md}>
-      <Stack direction={{ mobile: 'column', tablet: 'row' }} spacing={Spacing.xs} alignItems="center">
-        <Select
-          aria-label={t`Select network`}
-          value={filterNetwork?.chainId}
-          onChange={(e) => {
-            updateFilters({ chainId: Number(e.target.value) })
-          }}
-          displayEmpty
-          renderValue={() => <NetworkItem network={filterNetwork} />}
-          sx={{ minWidth: '12rem', height: ButtonSize.md }}
+  return (
+    <WithSkeleton loading={isLoading} sx={{ height: Sizing.xxl }}>
+      <Stack direction="column" gap={Spacing.md}>
+        <Stack direction={{ mobile: 'column', tablet: 'row' }} spacing={Spacing.xs} alignItems="center">
+          <Select
+            aria-label={t`Select network`}
+            value={filterNetwork?.chainId}
+            onChange={(e) => {
+              updateFilters({ chainId: Number(e.target.value) })
+            }}
+            displayEmpty
+            renderValue={() => <NetworkItem network={filterNetwork} />}
+            sx={{ minWidth: '12rem', height: ButtonSize.md }}
+          >
+            {networks.map((network) => (
+              <MenuItem key={network.chainId} value={network.chainId}>
+                <NetworkItem network={network} />
+              </MenuItem>
+            ))}
+          </Select>
+
+          <SearchField onSearch={setSearchText} placeholder={t`Search for an integration`} />
+        </Stack>
+
+        <Grid
+          container
+          columnSpacing={Spacing.xs}
+          rowSpacing={Spacing.md}
+          direction="row"
+          size={{ mobile: 12, desktop: 'auto' }}
         >
-          {networks.map((network) => (
-            <MenuItem key={network.chainId} value={network.chainId}>
-              <NetworkItem network={network} />
-            </MenuItem>
-          ))}
-        </Select>
-
-        <SearchField onSearch={setSearchText} placeholder={t`Search for an integration`} />
-      </Stack>
-
-      <Grid
-        container
-        columnSpacing={Spacing.xs}
-        rowSpacing={Spacing.md}
-        direction="row"
-        size={{ mobile: 12, desktop: 'auto' }}
-      >
-        {Object.values(tags).map((tag) => (
-          <Grid container key={tag.id} size={{ mobile: 12, tablet: 'auto' }} spacing={Spacing.xxs}>
-            <SelectableChip
-              size="small"
-              label={<IntegrationAppTag tag={tag} />}
-              selected={filterTag == tag.id}
-              toggle={() => updateFilters({ tag: tag.id })}
-              sx={{ width: { mobile: '100%', tablet: 'auto' } }}
-            />
-          </Grid>
-        ))}
-      </Grid>
-
-      {!integrationsFiltered.length ? (
-        <Box sx={{ display: 'flex', justifyContent: 'center', padding: Spacing.xxl }}>
-          <Trans>
-            No integration apps found with for {searchText ? <>&ldquo;{searchText}&rdquo;</> : ''}{' '}
-            {!!searchText && !!filterTag ? <>and </> : ''}
-            {filterTag ? <>&ldquo;{filterTag}&rdquo;</> : ''}
-          </Trans>
-        </Box>
-      ) : (
-        <Grid container spacing={Spacing.md}>
-          {(integrationsFiltered ?? []).map((app, idx) => (
-            <Grid key={`${app.name}_${idx}`} size={{ mobile: 12, tablet: 6, desktop: 4 }}>
-              <IntegrationAppComp
-                {...app}
-                filterKey={filterTag}
-                integrationsTags={tags}
-                integrationsAppNetworks={
-                  chainId && (
-                    <Stack direction="row" gap={Spacing.xs}>
-                      {Object.keys(app.networks).map((networkId) => (
-                        <ChainIcon key={networkId} blockchainId={networkId} size="sm" />
-                      ))}
-                    </Stack>
-                  )
-                }
-                imageUrl={app.imageId ? `${CURVE_ASSETS_URL}/platforms/${app.imageId}` : ''}
+          {Object.values(tags).map((tag) => (
+            <Grid container key={tag.id} size={{ mobile: 12, tablet: 'auto' }} spacing={Spacing.xxs}>
+              <SelectableChip
+                size="small"
+                label={<IntegrationAppTag tag={tag} />}
+                selected={filterTag == tag.id}
+                toggle={() => updateFilters({ tag: tag.id })}
+                sx={{ width: { mobile: '100%', tablet: 'auto' } }}
               />
             </Grid>
           ))}
         </Grid>
-      )}
-    </Stack>
+
+        {!integrationsFiltered.length ? (
+          <Box sx={{ display: 'flex', justifyContent: 'center', padding: Spacing.xxl }}>
+            <Trans>
+              No integration apps found with for {searchText ? <>&ldquo;{searchText}&rdquo;</> : ''}{' '}
+              {!!searchText && !!filterTag ? <>and </> : ''}
+              {filterTag ? <>&ldquo;{filterTag}&rdquo;</> : ''}
+            </Trans>
+          </Box>
+        ) : (
+          <Grid container spacing={Spacing.md}>
+            {(integrationsFiltered ?? []).map((app, idx) => (
+              <Grid key={`${app.name}_${idx}`} size={{ mobile: 12, tablet: 6, desktop: 4 }}>
+                <IntegrationAppComp
+                  {...app}
+                  filterKey={filterTag}
+                  integrationsTags={tags}
+                  integrationsAppNetworks={
+                    chainId && (
+                      <Stack direction="row" gap={Spacing.xs}>
+                        {Object.keys(app.networks).map((networkId) => (
+                          <ChainIcon key={networkId} blockchainId={networkId} size="sm" />
+                        ))}
+                      </Stack>
+                    )
+                  }
+                  imageUrl={app.imageId ? `${CURVE_ASSETS_URL}/platforms/${app.imageId}` : ''}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Stack>
+    </WithSkeleton>
   )
 }
