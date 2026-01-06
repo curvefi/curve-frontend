9b5be32bc00753e682bfbabc214252971069a782
Alunara <alunara@curve.finance>
2026-01-06 19:56:19 +0100

    chore: test description

--- tests/cypress/e2e/bridge/basic.cy.ts
+++ tests/cypress/e2e/bridge/basic.cy.ts
@@ -1,7 +1,7 @@
 import { LOAD_TIMEOUT } from '@cy/support/ui'
 
 describe('Basic Access Test', () => {
-  it('should open the Bridge DApp successfully', () => {
+  it('should open the Bridge page successfully', () => {
     cy.visit('/bridge/')
     cy.url(LOAD_TIMEOUT).should('match', /http:\/\/localhost:\d+\/bridge\/ethereum\/bridges\/?$/)
     cy.get('[data-testid^="bridge-page"]', LOAD_TIMEOUT).should('be.visible')
