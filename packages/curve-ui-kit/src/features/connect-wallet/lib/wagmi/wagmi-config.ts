dad394993ed80f0fb98e098186507271a454e47c
Alunara <alunara@curve.finance>
2026-01-06 20:24:48 +0100

    feat: re-enable multiInjectedProviderDiscovery and remove memoizee usage

--- packages/curve-ui-kit/src/features/connect-wallet/lib/wagmi/wagmi-config.ts
+++ packages/curve-ui-kit/src/features/connect-wallet/lib/wagmi/wagmi-config.ts
@@ -1,4 +1,3 @@
-import memoize from 'memoizee'
 import type { Chain } from 'viem'
 import { createConfig, type Transport, type CreateConnectorFn } from '@wagmi/core'
 import { connectors as defaultConnectors } from './connectors'
@@ -21,35 +20,12 @@ type CreateWagmiConfigOptions<TChains extends readonly [Chain, ...Chain[]]> = {
 
 /**
  * Creates a Wagmi configuration based on chains, transports and connectors.
- * We use memoize here to ensure only one config instance exists, preventing issues with wagmi auto-reconnect.
- * Auto-reconnect for injected wallets is disabled to prevent unwanted reconnections on disconnect.
  * @return A Wagmi configuration object that includes chains, connectors, and transports.
  */
-export const createWagmiConfig = memoize(
-  <TChains extends readonly [Chain, ...Chain[]]>({
-    chains,
-    transports,
-    connectors = Object.values(defaultConnectors),
-  }: CreateWagmiConfigOptions<TChains>) =>
-    createConfig({
-      chains,
-      connectors,
-      transports,
-      /**
-       * Disabled to prevent auto-reconnect on disconnecting an injected wallet.
-       * Solves the same issue as discussed on https://github.com/wevm/wagmi/discussions/3537.
-       * We don't use wallet specific injected connectors anyway and prefer to use the single global one.
-       */
-      multiInjectedProviderDiscovery: false,
-    }),
-  {
-    max: 1, // only memoize the last call
-    normalizer: ([{ chains, transports }]) =>
-      JSON.stringify({
-        chainIds: chains.map((c) => c.id),
-        transportIds: Object.keys(transports),
-      }),
-  },
-)
+export const createWagmiConfig = <TChains extends readonly [Chain, ...Chain[]]>({
+  chains,
+  transports,
+  connectors = Object.values(defaultConnectors),
+}: CreateWagmiConfigOptions<TChains>) => createConfig({ chains, connectors, transports })
 
 export type WagmiChainId = ReturnType<typeof createWagmiConfig>['chains'][number]['id']
