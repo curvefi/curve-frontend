6181dd125d36756bf90ce1656070b712168d5639
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-01-12 15:21:28 +0100

    e2e: create loan form

---
+++ tests/cypress/e2e/loan/create-loan.cy.ts
@@ -0,0 +1,25 @@
+import { oneBool } from '@cy/support/generators'
+import {
+  checkLoanDetailsLoaded,
+  checkLoanRangeSlider,
+  oneLoanTestMarket,
+  submitCreateLoanForm,
+  writeCreateLoanForm,
+} from '@cy/support/helpers/create-loan.helpers'
+import { LlamaMarketType } from '@ui-kit/types/market'
+
+describe(`Create Mint market loan`, () => {
+  const { collateral, borrow, path } = oneLoanTestMarket(LlamaMarketType.Mint)
+  const leverageEnabled = oneBool()
+
+  beforeEach(() => {
+    cy.visit(path)
+  })
+
+  it(`should open the borrow details`, () => {
+    writeCreateLoanForm({ collateral, borrow, leverageEnabled })
+    checkLoanDetailsLoaded({ leverageEnabled })
+    checkLoanRangeSlider(leverageEnabled)
+    submitCreateLoanForm().then(() => expect(cy.get('[data-testid="loan-form-error"]')).includes('unknown account'))
+  })
+})
