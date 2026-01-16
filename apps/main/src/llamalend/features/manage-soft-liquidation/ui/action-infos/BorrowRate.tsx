207924b241105b0baaa67d922e6b72c6d5e05beb
Pearce <pierre.vinet17@gmail.com>
2026-01-16 17:31:20 +0100

    refactor: llamalend table "Borrow Rate" column to "Net Borrow APR"

--- apps/main/src/llamalend/features/manage-soft-liquidation/ui/action-infos/BorrowRate.tsx
+++ apps/main/src/llamalend/features/manage-soft-liquidation/ui/action-infos/BorrowRate.tsx
@@ -2,7 +2,6 @@ import { t } from '@ui-kit/lib/i18n'
 import { type ActionInfoProps, ActionInfo } from '@ui-kit/shared/ui/ActionInfo'
 import type { Delta } from './types'
 import { formatValue } from './util'
-import { t } from '@ui-kit/lib/i18n'
 
 export type Props = Delta & Partial<ActionInfoProps>
 
