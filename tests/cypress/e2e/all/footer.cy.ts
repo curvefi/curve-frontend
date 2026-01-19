fbf238a9622268eb9d741589c0f92453ad60880c
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-01-19 17:15:43 +0100

    fix: delay canary

--- tests/cypress/e2e/all/footer.cy.ts
+++ tests/cypress/e2e/all/footer.cy.ts
@@ -1,12 +1,12 @@
 import { LOAD_TIMEOUT, oneAppPath, oneDesktopViewport, oneMobileViewport, oneTabletViewport } from '@cy/support/ui'
 
-const viewports = [oneDesktopViewport(), oneTabletViewport(), oneMobileViewport()]
+const viewports = [oneDesktopViewport(), oneTabletViewport(), oneMobileViewport()].slice(0, 1)
 
 describe('Footer', () => {
   viewports.forEach((viewport) => {
     it(`should contain multiple links on ${viewport[0]}x${viewport[1]} viewport`, () => {
       cy.viewport(...viewport)
-      cy.visit(`/${oneAppPath()}/`)
+      cy.visit(`/${oneAppPath()}`)
       cy.get(`[data-testid='footer']`, LOAD_TIMEOUT).should('be.visible')
       cy.get("[data-testid='footer'] a")
         .should('have.length.at.least', 1)
