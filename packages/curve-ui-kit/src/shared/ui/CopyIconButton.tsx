e0aa82c964bd3da974ef1c5faf1f1c77611540bd
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-01-21 10:29:46 +0100

    e2e: pass testId to CopyIconButton error
    During cypress tests, the click throws a permission error because the clipboard is not available programmatically (only on user events)

--- packages/curve-ui-kit/src/shared/ui/CopyIconButton.tsx
+++ packages/curve-ui-kit/src/shared/ui/CopyIconButton.tsx
@@ -5,43 +5,29 @@ import { InvertTheme } from '@ui-kit/shared/ui/ThemeProvider'
 import { Tooltip } from '@ui-kit/shared/ui/Tooltip'
 import { showToast } from '@ui-kit/widgets/Toast/toast.util'
 
-export function CopyIconButton({
-  copyText,
-  label,
-  confirmationText,
-  ...iconProps
-}: {
-  copyText: string
-  label: string
-  confirmationText: string
-} & IconButtonProps) {
-  return (
-    // Extra theme inverter so the tooltip doesn't change colors when inside an inverted block
-    <InvertTheme inverted={false}>
-      <Tooltip title={label} placement="top">
-        <IconButton
-          size="extraSmall"
-          {...iconProps}
-          onClick={() => {
-            console.info(`Copying to clipboard: ${copyText}`)
-            return navigator.clipboard
-              ? navigator.clipboard
-                  .writeText(copyText)
-                  .then(() =>
-                    showToast({
-                      message: copyText,
-                      severity: 'info',
-                      title: confirmationText,
-                      testId: 'copy-confirmation',
-                    }),
-                  )
-                  .catch((e) => showToast({ title: e.message, severity: 'error' }))
-              : showToast({ title: t`Clipboard not available due to unsecure origin`, severity: 'warning' })
-          }}
-        >
-          <CopyIcon />
-        </IconButton>
-      </Tooltip>
-    </InvertTheme>
-  )
-}
+const testId = 'copy-confirmation' as const
+
+type CopyIconBUttonProps = { copyText: string; label: string; confirmationText: string } & IconButtonProps
+
+export const CopyIconButton = ({ copyText, label, confirmationText, ...iconProps }: CopyIconBUttonProps) => (
+  // Extra theme inverter so the tooltip doesn't change colors when inside an inverted block
+  <InvertTheme inverted={false}>
+    <Tooltip title={label} placement="top">
+      <IconButton
+        size="extraSmall"
+        {...iconProps}
+        onClick={() => {
+          console.info(`Copying to clipboard: ${copyText}`)
+          return (
+            navigator.clipboard?.writeText(copyText).then(
+              () => showToast({ message: copyText, severity: 'info', title: confirmationText, testId }),
+              (e) => showToast({ title: e.message, severity: 'error', testId }),
+            ) ?? showToast({ title: t`Clipboard not available`, severity: 'warning', testId })
+          )
+        }}
+      >
+        <CopyIcon />
+      </IconButton>
+    </Tooltip>
+  </InvertTheme>
+)
