08d62ee471bab4e27cdc3da33d4d6047f97a0209
Pearce <pierre.vinet17@gmail.com>
2026-01-22 21:11:21 +0100

    fix: llamalend columns header tootltips not showing

--- packages/curve-ui-kit/src/shared/ui/DataTable/Sortable.tsx
+++ packages/curve-ui-kit/src/shared/ui/DataTable/Sortable.tsx
@@ -1,38 +1,48 @@
-import type { ReactNode, MouseEvent } from 'react'
+import { forwardRef, type ReactNode, type MouseEvent, type RefAttributes } from 'react'
 import Stack from '@mui/material/Stack'
 import type { Column } from '@tanstack/react-table'
 import { ArrowDownIcon } from '@ui-kit/shared/icons/ArrowDownIcon'
 import { getFlexAlignment, type TableItem } from './data-table.utils'
 import { RotatableIcon } from './RotatableIcon'
 
-export const Sortable = <T extends TableItem>({
-  children,
-  column,
-  isEnabled = true,
-}: {
-  column: Column<T> | undefined
+type SortableProps<T extends TableItem> = {
+  column: Column<T, unknown> | undefined
   children: ReactNode
   isEnabled?: boolean
-}) => (
-  <Stack
-    direction="row"
-    alignItems="end"
-    {...(column && {
-      justifyContent: getFlexAlignment(column),
-      onClick: (e: MouseEvent) => {
-        column.getToggleSortingHandler()?.(e)
-        e.stopPropagation()
-      },
-    })}
-    {...(isEnabled && { sx: { cursor: 'pointer', '&:hover': { color: `text.highlight` } } })}
-  >
-    {children}
-    <RotatableIcon
-      icon={ArrowDownIcon}
-      rotated={column?.getIsSorted() === 'asc'}
-      fontSize={column?.getIsSorted() ? 20 : 0}
-      isEnabled={isEnabled}
-      {...(isEnabled && { testId: `icon-sort-${column?.id}-${column?.getIsSorted()}` })}
-    />
-  </Stack>
-)
+}
+
+const _Sortable = forwardRef<HTMLDivElement, SortableProps<TableItem>>(function Sortable(
+  { children, column, isEnabled = true, ...props },
+  ref,
+) {
+  return (
+    <Stack
+      ref={ref}
+      {...props}
+      direction="row"
+      alignItems="end"
+      {...(column && {
+        justifyContent: getFlexAlignment(column),
+        onClick: (e: MouseEvent) => {
+          column.getToggleSortingHandler()?.(e)
+          e.stopPropagation()
+        },
+      })}
+      {...(isEnabled && { sx: { cursor: 'pointer', '&:hover': { color: `text.highlight` } } })}
+    >
+      {children}
+      <RotatableIcon
+        icon={ArrowDownIcon}
+        rotated={column?.getIsSorted() === 'asc'}
+        fontSize={column?.getIsSorted() ? 20 : 0}
+        isEnabled={isEnabled}
+        {...(isEnabled && { testId: `icon-sort-${column?.id}-${column?.getIsSorted()}` })}
+      />
+    </Stack>
+  )
+})
+
+/** Type assertion to support generics with forwardRef (forwardRef doesn't natively support generic components) */
+export const Sortable = _Sortable as <T extends TableItem>(
+  props: SortableProps<T> & RefAttributes<HTMLDivElement>,
+) => ReactNode
