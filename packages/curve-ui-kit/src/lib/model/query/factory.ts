88de8e70566697a157d3cb01434107ecf6d0680c
Alunara <alunara@curve.finance>
2026-01-23 01:06:42 +0100

    refactor: move factory types to factory implementation file

--- packages/curve-ui-kit/src/lib/model/query/factory.ts
+++ packages/curve-ui-kit/src/lib/model/query/factory.ts
@@ -1,10 +1,82 @@
+import type { IsStringLiteral, SingleKeyObject } from 'type-fest'
+import type { Suite } from 'vest'
 import { CB } from 'vest-utils'
-import { QueryFunctionContext, queryOptions, useQuery } from '@tanstack/react-query'
+import {
+  QueryFunctionContext,
+  queryOptions,
+  useQuery,
+  type DefaultError,
+  type FetchQueryOptions,
+  type QueryKey,
+  type UseQueryOptions,
+  type UseQueryResult,
+} from '@tanstack/react-query'
 import { queryClient } from '@ui-kit/lib/api/query-client'
 import { logQuery } from '@ui-kit/lib/logging'
 import { REFRESH_INTERVAL } from '@ui-kit/lib/model/time'
-import { QueryFactoryInput, QueryFactoryOutput } from '@ui-kit/lib/types'
 import { checkValidity, FieldName, FieldsOf } from '@ui-kit/lib/validation'
+
+// Checks if T is a string literal or an object with one property
+type IsLiteralOrSingleKeyObject<T> =
+  IsStringLiteral<T> extends true ? true : T extends SingleKeyObject<T> ? true : false
+
+// Recursively checks each element in the tuple
+type AreAllElementsLiteralOrSinglePropertyObject<T extends readonly unknown[]> = T extends readonly [
+  infer First,
+  ...infer Rest,
+]
+  ? IsLiteralOrSingleKeyObject<First> extends true
+    ? AreAllElementsLiteralOrSinglePropertyObject<Rest>
+    : false
+  : true
+
+// Combined type that ensures T is a tuple and all elements pass the checks
+type QueryKeyTuple<T extends readonly unknown[]> = T extends readonly [...infer Elements]
+  ? number extends Elements['length']
+    ? never // Not a tuple
+    : AreAllElementsLiteralOrSinglePropertyObject<T> extends true
+      ? T // Valid tuple
+      : never // Elements fail the check
+  : never // Not an array
+
+type QueryFactoryInput<
+  TValidParams extends object,
+  TKey extends readonly unknown[],
+  TData,
+  TParams extends FieldsOf<TValidParams> = FieldsOf<TValidParams>,
+  TField extends string = FieldName<TValidParams>,
+  TGroup extends string = string,
+  TCallback extends CB = CB<TValidParams, TField[]>,
+> = {
+  queryKey: (params: TParams) => QueryKeyTuple<TKey>
+  validationSuite: Suite<TField, TGroup, TCallback>
+  queryFn: (params: TValidParams) => Promise<TData>
+  gcTime?: keyof typeof REFRESH_INTERVAL
+  staleTime?: keyof typeof REFRESH_INTERVAL
+  refetchInterval?: keyof typeof REFRESH_INTERVAL
+  dependencies?: (params: TParams) => QueryKey[]
+  refetchOnWindowFocus?: 'always'
+  refetchOnMount?: 'always'
+  disableLog?: true
+}
+
+type QueryFactoryOutput<
+  TValidParams extends object,
+  TKey extends readonly unknown[],
+  TData,
+  TParams extends FieldsOf<TValidParams> = FieldsOf<TValidParams>,
+  TError = DefaultError,
+> = {
+  getQueryOptions: (params: TParams, enabled?: boolean) => UseQueryOptions<TData, TError, TData, TKey>
+  queryKey: (params: TParams) => QueryKeyTuple<TKey>
+  useQuery: (params: TParams, enabled?: boolean) => UseQueryResult<TData, TError>
+  getQueryData: (params: TParams) => TData | undefined
+  setQueryData: (params: TParams, data: TData) => void
+  prefetchQuery: (params: TParams, staleTime?: number) => Promise<void>
+  fetchQuery: (params: TParams, options?: Partial<FetchQueryOptions<TData, TError, TData, TKey>>) => Promise<TData>
+  refetchQuery: (params: TParams) => Promise<TData>
+  invalidate: (params: TParams) => void // todo: should be Promise<void>
+}
 
 const getParamsFromQueryKey = <TKey extends readonly unknown[], TParams>(queryKey: TKey) =>
   Object.fromEntries(queryKey.flatMap((i) => (i && typeof i === 'object' ? Object.entries(i) : []))) as TParams
