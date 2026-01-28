321d95e11186e8fe62ac0a574606c6ce6726a204
Alunara <alunara@curve.finance>
2026-01-28 19:25:01 +0100

    fix: errors when user pool list is being fetched with no RPC

--- apps/main/src/dex/queries/user-pools.query.ts
+++ apps/main/src/dex/queries/user-pools.query.ts
@@ -2,6 +2,7 @@ import { requireLib } from '@ui-kit/features/connect-wallet'
 import { createValidationSuite, type FieldsOf } from '@ui-kit/lib'
 import { queryFactory, rootKeys, type ChainQuery, type UserQuery } from '@ui-kit/lib/model'
 import { chainValidationGroup } from '@ui-kit/lib/model/query/chain-validation'
+import { curveApiValidationGroup } from '@ui-kit/lib/model/query/curve-api-validation'
 import { userAddressValidationGroup } from '@ui-kit/lib/model/query/user-address-validation'
 
 type UserPoolsQuery = ChainQuery & UserQuery
@@ -10,10 +11,15 @@ type UserPoolsParams = FieldsOf<UserPoolsQuery>
 export const { useQuery: useUserPools, fetchQuery: fetchUserPools } = queryFactory({
   queryKey: ({ chainId, userAddress }: UserPoolsParams) =>
     [...rootKeys.chain({ chainId }), ...rootKeys.user({ userAddress }), 'pools'] as const,
-  queryFn: async ({ userAddress }: UserPoolsQuery) => await requireLib('curveApi').getUserPoolList(userAddress),
+  queryFn: async ({ userAddress }: UserPoolsQuery) => {
+    const curve = requireLib('curveApi')
+    // User pool list only be fetched with an RPC as it uses on chain calls
+    return curve.isNoRPC ? [] : await curve.getUserPoolList(userAddress)
+  },
   staleTime: '1m',
-  validationSuite: createValidationSuite(({ chainId, userAddress }: UserPoolsParams) => {
-    chainValidationGroup({ chainId })
-    userAddressValidationGroup({ userAddress })
+  validationSuite: createValidationSuite((params: UserPoolsParams) => {
+    curveApiValidationGroup(params)
+    chainValidationGroup(params)
+    userAddressValidationGroup(params)
   }),
 })
