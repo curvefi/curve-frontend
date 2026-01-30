fc31ddbac30d7a128be34f9247042ace81be10d6
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-01-30 11:13:16 +0100

    fix: storybook build

--- packages/curve-ui-kit/vite.config.ts
+++ packages/curve-ui-kit/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(__dirname, '../../apps/main/src'),
+      '@cy': resolve(__dirname, '../../tests/cypress'),
       '@ui': resolve(__dirname, '../ui/src'),
       '@ui-kit': resolve(__dirname, './src'),
       '@external-rewards': resolve(__dirname, '../external-rewards/src/index.ts'),
