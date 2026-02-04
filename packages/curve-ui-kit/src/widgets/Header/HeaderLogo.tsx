f5f27504cf75d78351dabc6b685f01eaf4fe3a56
0xtutti <0xtutti@protonmail.com>
2026-02-04 17:52:30 +0100

    Reduce header logo size

--- packages/curve-ui-kit/src/widgets/Header/HeaderLogo.tsx
+++ packages/curve-ui-kit/src/widgets/Header/HeaderLogo.tsx
@@ -4,8 +4,8 @@ import { RouterLink } from '@ui-kit/shared/ui/RouterLink'
 import type { SxProps } from '@ui-kit/utils'
 
 const Image = styled('img')({
-  width: 28,
-  height: 28,
+  width: 26,
+  height: 26,
 })
 
 export type HeaderLogoProps = {
@@ -14,6 +14,6 @@ export type HeaderLogoProps = {
 
 export const HeaderLogo = ({ sx }: HeaderLogoProps) => (
   <RouterLink href="/" sx={{ display: 'flex', alignItems: 'center', textDecoration: 'none', ...sx }}>
-    <Image src={CURVE_LOGO_URL} alt="Curve" width={28} height={28} />
+    <Image src={CURVE_LOGO_URL} alt="Curve" width={26} height={26} />
   </RouterLink>
 )
