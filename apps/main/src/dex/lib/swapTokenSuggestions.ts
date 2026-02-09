65e9432c15a5abfa6961f3d1ef4f5a2d30eed433
0xtutti <0xtutti@protonmail.com>
2026-02-09 13:55:40 +0100

    fix(swap): correct predefined WETH suggestion address

--- apps/main/src/dex/lib/swapTokenSuggestions.ts
+++ apps/main/src/dex/lib/swapTokenSuggestions.ts
@@ -11,7 +11,7 @@ type NativeToken = {
 const PREDEFINED_SWAP_SUGGESTIONS: Record<number, string[]> = {
   1: [
     ethAddress,
-    '0xc02aa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
+    '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
     '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
     '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
     '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
