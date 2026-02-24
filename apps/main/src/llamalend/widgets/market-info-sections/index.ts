00c800d4157b7ec9672fa03b7016a9ef96e162f1
JustJousting <justjousting2moon@gmail.com>
2026-02-24 14:39:43 +0200

    chore: clean up

--- apps/main/src/llamalend/widgets/market-info-sections/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './ContractsSection'
-export * from './MarketInfoSections'
-export * from './MarketParametersSection'
-export { MarketParametersSection as MarketParameters } from './MarketParametersSection'
