2bb86b72833279b74bda45d5169e36b549842100
Alunara <alunara@curve.finance>
2026-03-05 00:37:45 +0100

    styling: aligns datatable title to the bottom

--- packages/curve-ui-kit/src/shared/ui/DataTable/TableFiltersTitles.tsx
+++ packages/curve-ui-kit/src/shared/ui/DataTable/TableFiltersTitles.tsx
@@ -1,12 +1,13 @@
+import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 
 export const TableFiltersTitles = ({ title, subtitle }: { title: string; subtitle?: string }) => (
-  <>
+  <Stack height="100%" justifyContent="end">
     <Typography variant="headingSBold">{title}</Typography>
     {subtitle && (
       <Typography variant="bodySRegular" color="textSecondary">
         {subtitle}
       </Typography>
     )}
-  </>
+  </Stack>
 )
