b6e3773e6ad3683c0412a3e3e78b7fb38db80d8a
JustJousting <justjousting2moon@gmail.com>
2026-03-04 20:22:27 +0200

    refactor: add Etherlink to Chain enum

--- apps/main/src/dex/lib/networks.ts
+++ apps/main/src/dex/lib/networks.ts
@@ -322,8 +322,8 @@ export async function getNetworks() {
   const liteNetworks = Object.values(resp).reduce((prev, { chainId, ...config }) => {
     const baseConfig = NETWORK_BASE_CONFIG[chainId as keyof typeof NETWORK_BASE_CONFIG]
     const isUpgraded = !!baseConfig // networks upgraded from lite to full
-    const isOnlyPoolRewardsUpgraded = chainId === Chain.Taiko || chainId === 42793 // networks that has only been upgraded to show pool rewards APY, 42793 = Etherlink
-    const isLiteFxswapEnabled = chainId === 42793 // enabled on Etherlink despite being a Lite network
+    const isOnlyPoolRewardsUpgraded = chainId === Chain.Taiko || chainId === Chain.Etherlink // networks that has only been upgraded to show pool rewards APY, 42793 = Etherlink
+    const isLiteFxswapEnabled = chainId === Chain.Etherlink // enabled on Etherlink despite being a Lite network
     prev[chainId] = {
       ...DEFAULT_NETWORK_CONFIG,
       ...getBaseNetworksConfig<NetworkEnum, ChainId>(Number(chainId), { ...config, ...baseConfig }),
