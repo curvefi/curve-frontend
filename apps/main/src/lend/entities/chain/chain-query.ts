0ca444845437fdfe59106b715ed55c095745ffb7
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-03-05 09:54:52 +0100

    feat: show llamalend v2 markets

--- apps/main/src/lend/entities/chain/chain-query.ts
+++ apps/main/src/lend/entities/chain/chain-query.ts
@@ -1,14 +1,14 @@
-import { ChainId } from '@/lend/types/lend.types'
+import { USE_API } from '@/llamalend/queries/market/market.constants'
 import { requireLib } from '@ui-kit/features/connect-wallet'
-import { ChainParams, ChainQuery, queryFactory } from '@ui-kit/lib/model/query'
+import { ChainParams, queryFactory, rootKeys } from '@ui-kit/lib/model/query'
 import { llamaApiValidationSuite } from '@ui-kit/lib/model/query/curve-api-validation'
 
 export const { useQuery: useOneWayMarketNames, prefetchQuery: prefetchMarkets } = queryFactory({
-  queryKey: ({ chainId }: ChainParams) => ['chain', { chainId }, 'markets'] as const,
-  queryFn: async ({ chainId }: ChainQuery<ChainId>): Promise<string[]> => {
-    const useAPI = chainId !== 146 // disable API for sonic
+  queryKey: ({ chainId }: ChainParams) => [...rootKeys.chain({ chainId }), 'lendMarkets.getMarketList'] as const,
+  queryFn: async (): Promise<string[]> => {
     const api = requireLib('llamaApi')
-    await api.lendMarkets.fetchMarkets({ useApi: useAPI })
+    const versions = ['v1', 'v2'] as const
+    await Promise.all([versions.map((version) => api.lendMarkets.fetchMarkets({ useApi: USE_API, version }))])
     return api.lendMarkets.getMarketList()
   },
   validationSuite: llamaApiValidationSuite,
