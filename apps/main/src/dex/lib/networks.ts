2c0184f7f982d540eeca6a468b31b74d986c1359
JustJousting <justjousting2moon@gmail.com>
2026-03-05 13:22:45 +0200

    refactor: review comment

--- apps/main/src/dex/lib/networks.ts
+++ apps/main/src/dex/lib/networks.ts
@@ -316,13 +316,18 @@ export const defaultNetworks = Object.entries({
   {} as Record<ChainId, NetworkConfig>,
 )
 
+/** Networks that has only been upgraded to show pool rewards APY */
+const poolRewardsUpgradedChains = [Chain.Taiko, Chain.Etherlink]
+/** Networks that has FXSwap enabled in pool creation */
+const fxSwapUpgradedChains = [Chain.Etherlink]
+
 export async function getNetworks() {
   const resp = await curve.getCurveLiteNetworks() // returns [] in case of error
   const liteNetworks = Object.values(resp).reduce((prev, { chainId, ...config }) => {
     const baseConfig = NETWORK_BASE_CONFIG[chainId as keyof typeof NETWORK_BASE_CONFIG]
     const isUpgraded = !!baseConfig // networks upgraded from lite to full
-    const isOnlyPoolRewardsUpgraded = chainId === Chain.Taiko || chainId === Chain.Etherlink // networks that has only been upgraded to show pool rewards APY, 42793 = Etherlink
-    const isLiteFxswapEnabled = chainId === Chain.Etherlink // enabled on Etherlink despite being a Lite network
+    const isOnlyPoolRewardsUpgraded = poolRewardsUpgradedChains.includes(chainId)
+    const isLiteFxswapEnabled = fxSwapUpgradedChains.includes(chainId)
     prev[chainId] = {
       ...DEFAULT_NETWORK_CONFIG,
       ...getBaseNetworksConfig<NetworkEnum, ChainId>(Number(chainId), { ...config, ...baseConfig }),
