220cb28bedafa434b053a91b9c993b6dd933912b
Alunara <alunara@curve.finance>
2026-03-06 20:20:27 +0100

    feat: analytics app

--- apps/main/src/routes/analytics.routes.tsx
+++ apps/main/src/routes/analytics.routes.tsx
@@ -2,6 +2,7 @@ import '@/analytics/llamadash'
 import { createRoute, lazyRouteComponent } from '@tanstack/react-router'
 import { rootRoute } from './root.routes'
 import { createSharedRoutes } from './shared.routes'
+import { redirectTo } from './util'
 
 const PageHome = lazyRouteComponent(() => import('@/analytics/components/PageHome'), 'PageHome')
 
@@ -16,6 +17,11 @@ export const analyticsRoutes = analyticsLayoutRoute.addChildren([
   ...createSharedRoutes('analytics', layoutProps),
   createRoute({
     path: '$network',
+    loader: ({ params: { network } }) => redirectTo(`/analytics/${network}/home`),
+    ...layoutProps,
+  }),
+  createRoute({
+    path: '$network/home',
     component: PageHome,
     ...layoutProps,
   }),
