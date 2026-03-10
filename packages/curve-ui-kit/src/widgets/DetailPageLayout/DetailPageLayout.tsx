d954a143e6adca89d9ad5a12cfe938252253a813
Pearce <pierre.vinet17@gmail.com>
2026-03-10 16:10:01 +0100

    fix: bridge cards with new layout

--- packages/curve-ui-kit/src/widgets/DetailPageLayout/DetailPageLayout.tsx
+++ packages/curve-ui-kit/src/widgets/DetailPageLayout/DetailPageLayout.tsx
@@ -38,25 +38,23 @@ export const DetailPageLayout = ({
       {/* In Figma, columns are 12/4/3, but too small around breakpoints. I've added one extra column.
           Ultrawide isn't a breakpoint yet, use maxWidth so it's not too large. */}
       {formTabs !== null && (
-        <>
-          <Grid
-            size={{ mobile: 12, tablet: 5, desktop: 4 }}
-            maxWidth={{ desktop: MaxWidth.actionCard }}
-            sx={{
-              // action forms on the right for Beta
-              order: { mobile: 1, tablet: isNewLayout ? 2 : 1 },
-              ...(isNewLayout && stickySx(navHeight)),
-            }}
-          >
-            {formTabs || <FormSkeleton />}
-          </Grid>
-          <Grid size="grow" sx={{ order: { mobile: 2, tablet: isNewLayout ? 1 : 2 } }}>
-            <Stack flexDirection="column" flexGrow={1} sx={{ gap: Spacing.md }}>
-              {children}
-            </Stack>
-          </Grid>
-        </>
+        <Grid
+          size={{ mobile: 12, tablet: 5, desktop: 4 }}
+          maxWidth={{ desktop: MaxWidth.actionCard }}
+          sx={{
+            // action forms on the right for Beta
+            order: { mobile: 1, tablet: isNewLayout ? 2 : 1 },
+            ...(isNewLayout && stickySx(navHeight)),
+          }}
+        >
+          {formTabs || <FormSkeleton />}
+        </Grid>
       )}
+      <Grid size="grow" sx={{ order: { mobile: 2, tablet: isNewLayout ? 1 : 2 } }}>
+        <Stack flexDirection="column" flexGrow={1} sx={{ gap: Spacing.md }}>
+          {children}
+        </Stack>
+      </Grid>
 
       {footer && <Grid size={12}>{footer}</Grid>}
     </Grid>
