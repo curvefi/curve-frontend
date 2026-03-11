2ea78d209c3c6ec881e6b73cc0ed54a2cbe8f78c
Alunara <alunara@curve.finance>
2026-03-11 01:49:24 +0100

    refactor: extract SelectTimeOption for re-use in analytics app

---
+++ packages/curve-ui-kit/src/shared/ui/Chart/SelectTimeOption.tsx
@@ -0,0 +1,40 @@
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
+import Typography from '@mui/material/Typography'
+
+export type TimeOption<TTimeOption extends string = string> = {
+  options: readonly TTimeOption[]
+  activeOption: TTimeOption
+  setActiveOption: (newTimeOption: TTimeOption) => void
+}
+
+export const SelectTimeOption = <TTimeOption extends string = string>({
+  options,
+  activeOption,
+  setActiveOption,
+  isLoading = false,
+}: TimeOption<TTimeOption> & {
+  isLoading: boolean
+}) => (
+  <Select
+    value={activeOption}
+    onChange={(event) => {
+      if (event.target.value != null) setActiveOption(event.target.value as TTimeOption)
+    }}
+    size="small"
+    sx={{ alignSelf: 'center' }}
+    disabled={isLoading}
+    MenuProps={{
+      anchorOrigin: { vertical: 'bottom', horizontal: 'right' },
+      transformOrigin: { vertical: 'top', horizontal: 'right' },
+    }}
+  >
+    {options.map((option) => (
+      <MenuItem value={option} key={option}>
+        <Typography variant="bodySBold" sx={{ textTransform: 'uppercase' }}>
+          {option}
+        </Typography>
+      </MenuItem>
+    ))}
+  </Select>
+)
