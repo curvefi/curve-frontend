0707b2cb7251833ef57b36315ad3aea961826ba8
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-03-12 09:35:51 +0100

    fix: call text()

--- tests/cypress/e2e/llamalend/create-loan.cy.ts
+++ tests/cypress/e2e/llamalend/create-loan.cy.ts
@@ -26,7 +26,7 @@ describe('Create loan', () => {
       checkLoanRangeSlider({ leverageEnabled })
       // e2e tests run with a 'fake' account so the transaction fails
       submitCreateLoanForm('error', 'Transaction failed').then(() =>
-        cy.get('[data-testid="loan-form-error"]', LOAD_TIMEOUT).its('text').should('match', expectedErrorRegex),
+        cy.get('[data-testid="loan-form-error"]', LOAD_TIMEOUT).invoke('text').should('match', expectedErrorRegex),
       )
     })
   })
