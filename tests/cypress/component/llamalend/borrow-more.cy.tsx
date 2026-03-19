7b6e0b89b79fd5eab971cb0be9dfd34cc6069edd
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-03-19 15:44:01 +0100

    fix: typecheck

--- tests/cypress/component/llamalend/borrow-more.cy.tsx
+++ tests/cypress/component/llamalend/borrow-more.cy.tsx
@@ -43,6 +43,7 @@ describe('BorrowMoreForm (mocked)', () => {
             chainId={chainId}
             onSuccess={onSuccess}
             onPricesUpdated={onPricesUpdated}
+            enabled
           />
         </MockLoanTestWrapper>,
       )
