81a8950b6142fca8c97343b26a9afc80fae4b3ef
JustJousting <justjousting2moon@gmail.com>
2026-03-22 21:24:38 +0200

    refactor: add movingAverage to array.utils.ts and simplify addMovingAverages

--- packages/curve-ui-kit/src/shared/ui/Chart/chart.utils.ts
+++ packages/curve-ui-kit/src/shared/ui/Chart/chart.utils.ts
@@ -1,44 +1,21 @@
+import { meanBy } from 'lodash'
+import { movingAverage } from '@primitives/array.utils'
 import { TIME_FRAMES } from '@ui-kit/lib/model/time'
 
-type MovingAverageConfig<T> = {
-  /** Extract the numeric value to average */
-  getValue: (item: T) => number
-  /** Extract the timestamp in milliseconds */
-  getTimestamp: (item: T) => number
-  /** Window size in milliseconds (defaults to 7 days) */
-  windowMs?: number
-}
-
-type WithAverages = { movingAverage: number; totalAverage: number }
-
-/**
- * Enriches a sorted time-series array with a moving average and total average.
- * Data must be sorted by timestamp ascending before calling this function.
- */
-export function addMovingAverages<T>(data: T[], config: MovingAverageConfig<T>): (T & WithAverages)[] {
-  const { getValue, getTimestamp, windowMs = 7 * TIME_FRAMES.DAY_MS } = config
-
+export function addMovingAverages<T>(
+  data: T[],
+  getValue: (item: T) => number,
+  getTimestamp: (item: T) => number,
+  windowMs = 7 * TIME_FRAMES.DAY_MS,
+) {
   if (data.length === 0) return []
 
-  const totalAverage = data.reduce((sum, item) => sum + getValue(item), 0) / data.length
+  const totalAverage = meanBy(data, getValue)
+  const averages = movingAverage(data.map(getValue), data.map(getTimestamp), windowMs)
 
-  const values = data.map(getValue)
-  const timestamps = data.map(getTimestamp)
-
-  let windowStart = 0
-  let windowSum = 0
-
-  return data.map((item, i) => {
-    windowSum += values[i]
-    while (timestamps[windowStart] < timestamps[i] - windowMs) {
-      windowSum -= values[windowStart]
-      windowStart++
-    }
-
-    return {
-      ...item,
-      movingAverage: windowSum / (i - windowStart + 1),
-      totalAverage,
-    }
-  })
+  return data.map((item, i) => ({
+    ...item,
+    movingAverage: averages[i],
+    totalAverage,
+  }))
 }
