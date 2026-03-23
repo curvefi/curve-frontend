2e3f5fe14ea82e575229916da1349153e57f7ec3
Pearce <pierre.vinet17@gmail.com>
2026-03-23 10:54:29 +0100

    refactor: simplified and grouped lending vault shares query and calculations

--- apps/main/src/llamalend/features/supply/components/WithdrawSupplyInfoList.tsx
+++ apps/main/src/llamalend/features/supply/components/WithdrawSupplyInfoList.tsx
@@ -2,16 +2,17 @@ import BigNumber from 'bignumber.js'
 import type { UseFormReturn } from 'react-hook-form'
 import type { NetworkDict } from '@/llamalend/llamalend.types'
 import { useMarketSupplyFutureRates, useMarketRates } from '@/llamalend/queries/market'
-import { useWithdrawExpectedVaultShares } from '@/llamalend/queries/supply/supply-expected-vault-shares.query'
+import { useWithdrawRemovableVaultShares } from '@/llamalend/queries/supply/supply-expected-vault-shares.query'
 import { useWithdrawEstimateGas } from '@/llamalend/queries/supply/supply-withdraw-estimate-gas.query'
-import { useUserBalances } from '@/llamalend/queries/user'
 import type { WithdrawForm, WithdrawParams } from '@/llamalend/queries/validation/supply.validation'
 import { SupplyActionInfoList } from '@/llamalend/widgets/action-card/SupplyActionInfoList'
 import type { IChainId } from '@curvefi/llamalend-api/lib/interfaces'
 import { type Token } from '@primitives/address.utils'
+import { combineQueryState } from '@ui-kit/lib/queries/combine'
 import { mapQuery, q } from '@ui-kit/types/util'
 import { decimal } from '@ui-kit/utils'
 import { isFormTouched } from '@ui-kit/utils/react-form.utils'
+import { useVaultUserBalances } from '../hooks/useVaultUserBalances'
 
 export type WithdrawSupplyInfoListProps<ChainId extends IChainId> = {
   params: WithdrawParams<ChainId>
@@ -29,25 +30,34 @@ export function WithdrawSupplyInfoList<ChainId extends IChainId>({
   const { chainId, marketId, userAddress, withdrawAmount, isFull } = params
   const isOpen = isFormTouched(form, 'withdrawAmount')
 
-  const userBalances = useUserBalances({ chainId, marketId, userAddress }, isOpen)
-  const prevAmountSupplied = mapQuery(userBalances, (d) => d.vaultSharesConverted)
-
-  const prevVaultShares = mapQuery(userBalances, (d) => d.vaultShares)
-  const vaultShares = useWithdrawExpectedVaultShares(params, isOpen)
-
   const marketRates = useMarketRates(params, isOpen)
   const futureRates = useMarketSupplyFutureRates({ chainId, marketId, reserves: withdrawAmount }, isOpen)
+
+  const userBalances = useVaultUserBalances({ chainId, marketId, userAddress }, isOpen)
+  const removableVaultShares = useWithdrawRemovableVaultShares(params, isOpen)
 
   return (
     <SupplyActionInfoList
       isOpen={isOpen}
       suppliedSymbol={tokens.borrowToken?.symbol}
-      prevVaultShares={prevVaultShares}
-      vaultShares={isFull ? mapQuery(prevVaultShares, () => decimal(0)) : q(vaultShares)}
-      prevAmountSupplied={q(prevAmountSupplied)}
+      prevVaultShares={mapQuery(userBalances, (d) => d.totalShares)}
+      vaultShares={{
+        data:
+          userBalances.data.totalShares &&
+          removableVaultShares.data &&
+          userBalances.data.depositedShares &&
+          decimal(
+            new BigNumber(userBalances.data.totalShares).minus(
+              isFull ? userBalances.data.depositedShares : removableVaultShares.data,
+            ),
+          ),
+        ...combineQueryState(userBalances, removableVaultShares),
+      }}
+      prevAmountSupplied={mapQuery(userBalances, (d) => d.totalSharesAmount)}
       amountSupplied={mapQuery(
-        prevAmountSupplied,
-        (prevAmount) => withdrawAmount && decimal(new BigNumber(prevAmount).minus(withdrawAmount)),
+        userBalances,
+        (d) =>
+          d.totalSharesAmount && withdrawAmount && decimal(new BigNumber(d.totalSharesAmount).minus(withdrawAmount)),
       )}
       prevSupplyApy={mapQuery(marketRates, (d) => d.lendApy)}
       supplyApy={mapQuery(futureRates, (d) => d.lendApy)}
