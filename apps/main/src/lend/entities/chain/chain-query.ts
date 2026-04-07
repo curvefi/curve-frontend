2f24df19b4dbc9c47f4e97b2664e4517249d071d
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-04-07 09:37:28 +0200

    feat: enable llamalend v2

--- apps/main/src/lend/entities/chain/chain-query.ts
+++ apps/main/src/lend/entities/chain/chain-query.ts
@@ -1,13 +1,20 @@
 import { USE_API } from '@/llamalend/queries/market/market.constants'
+import { notFalsy } from '@primitives/objects.utils'
 import { requireLib } from '@ui-kit/features/connect-wallet'
+import { isLLv2Enabled } from '@ui-kit/hooks/useFeatureFlags'
 import { ChainParams, queryFactory, rootKeys } from '@ui-kit/lib/model/query'
 import { llamaApiValidationSuite } from '@ui-kit/lib/model/query/curve-api-validation'
+
+const V1 = 'v1' as const
+const V2 = 'v2' as const
 
 export const { useQuery: useOneWayMarketNames, prefetchQuery: prefetchMarkets } = queryFactory({
   queryKey: ({ chainId }: ChainParams) => [...rootKeys.chain({ chainId }), 'lendMarkets.getMarketList'] as const,
   queryFn: async (): Promise<string[]> => {
     const api = requireLib('llamaApi')
-    await api.lendMarkets.fetchMarkets({ useApi: USE_API, version: 'v1' })
+    await Promise.all(
+      notFalsy(V1, isLLv2Enabled() && V2).map((version) => api.lendMarkets.fetchMarkets({ useApi: USE_API, version })),
+    )
     return api.lendMarkets.getMarketList()
   },
   validationSuite: llamaApiValidationSuite,
