763cb314043d397a217de9674c248244f1e03a6d
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-04-13 12:19:53 +0200

    fix: release channel hydration for LLV2 support

--- packages/curve-ui-kit/src/hooks/useLocalStorage.ts
+++ packages/curve-ui-kit/src/hooks/useLocalStorage.ts
@@ -39,7 +39,8 @@ const useLocalStorage = <T>(key: string, initialValue: T, migration?: MigrationO
 /* -- Export specific hooks so that we can keep an overview of all the local storage keys used in the app -- */
 export const useShowTestNets = () => useLocalStorage<boolean>('showTestnets', false)
 
-export const getReleaseChannel = () => getFromLocalStorage<ReleaseChannel>('release-channel') ?? defaultReleaseChannel
+export const getReleaseChannel = () =>
+  getFromLocalStorage<ReleaseChannel>('release-channel-v1') ?? defaultReleaseChannel
 export const useReleaseChannel = () =>
   useLocalStorage<ReleaseChannel>('release-channel', defaultReleaseChannel, {
     version: 1,
