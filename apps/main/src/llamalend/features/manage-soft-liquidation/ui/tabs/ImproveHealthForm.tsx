b9aa0181b69dd9f451600c4900a5b504cbd88a4e
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-04-15 11:35:44 +0200

    chore: review comment

--- apps/main/src/llamalend/features/manage-soft-liquidation/ui/tabs/ImproveHealthForm.tsx
+++ apps/main/src/llamalend/features/manage-soft-liquidation/ui/tabs/ImproveHealthForm.tsx
@@ -2,16 +2,8 @@ import { noop } from 'lodash'
 import type { ComponentProps } from 'react'
 import { RepayForm } from '@/llamalend/features/manage-loan/components/RepayForm'
 
-/**
- * Form to repay debt to increase health in soft liquidation. Similar to RepayForm, but with some differences:
- * - No token selection, since only the borrow token can be repaid to increase health
- * - No price impact or swap info, since no swap is involved in improving health
- * - Different info list component that focuses on health improvement details rather than general repay details
- */
+const onPricesUpdated = noop // disable prices chart for soft liquidation
+
 export const ImproveHealthForm = ({ ...props }: Omit<ComponentProps<typeof RepayForm>, 'onPricesUpdated'>) => (
-  <RepayForm
-    {...props}
-    isInSoftLiquidation
-    onPricesUpdated={noop} // disable prices chart for soft liquidation
-  />
+  <RepayForm {...props} isInSoftLiquidation onPricesUpdated={onPricesUpdated} />
 )
