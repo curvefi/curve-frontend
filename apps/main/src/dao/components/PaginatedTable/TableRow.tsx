313e0514cb3d376a73c3474374c3cfa08c9343a5
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-04-20 13:04:52 +0200

    fix: merge issue

--- apps/main/src/dao/components/PaginatedTable/TableRow.tsx
+++ apps/main/src/dao/components/PaginatedTable/TableRow.tsx
@@ -4,7 +4,7 @@ import { InternalLink } from '@ui/Link'
 import type { InternalLinkProps } from '@ui/Link/InternalLink'
 
 type TableRowWrapperProps = { columns: number; gridTemplateColumns?: string }
-// eslint-disable-next-line react-refresh/only-export-components
+
 export const TableRowWrapper: IStyledComponent<'web', TableRowWrapperProps & ComponentPropsWithRef<'div'>> =
   styled.div<TableRowWrapperProps>`
     display: grid;
@@ -17,7 +17,6 @@ export const TableRowWrapper: IStyledComponent<'web', TableRowWrapperProps & Com
     }
   `
 
-// eslint-disable-next-line react-refresh/only-export-components
 export const TableData: IStyledComponent<'web', ComponentPropsWithRef<'p'>> = styled.p`
   font-variant-numeric: tabular-nums;
   font-size: var(--font-size-2);
@@ -42,7 +41,6 @@ export const TableData: IStyledComponent<'web', ComponentPropsWithRef<'p'>> = st
   }
 `
 
-// eslint-disable-next-line react-refresh/only-export-components
 export const TableDataLink: IStyledComponent<'web', InternalLinkProps> = styled(InternalLink)<InternalLinkProps>`
   font-variant-numeric: tabular-nums;
   font-size: var(--font-size-2);
