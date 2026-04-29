d5f5f6349ffc1b7f57f1d71f538873a801017984
Alunara <alunara@curve.finance>
2026-04-29 15:18:41 +0200

    refactor: get rid of BaseCell

--- apps/main/src/llamalend/features/manage-soft-liquidation/ui/cells/BaseCell.tsx
+++
@@ -1,7 +0,0 @@
-import type { ReactNode } from 'react'
-import Stack from '@mui/material/Stack'
-import { SizesAndSpaces } from '@ui-kit/themes/design/1_sizes_spaces'
-
-const { Spacing } = SizesAndSpaces
-
-export const BaseCell = ({ children }: { children: ReactNode }) => <Stack paddingBlock={Spacing.md}>{children}</Stack>
