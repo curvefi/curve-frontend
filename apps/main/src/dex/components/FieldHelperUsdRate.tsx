5224f13ed6e06603559b177355839ecba02ceb07
Alunara <alunara@curve.finance>
2026-05-09 18:44:20 +0200

    refactor: remove non-default BN usage and delete utilsFormat.ts

--- apps/main/src/dex/components/FieldHelperUsdRate.tsx
+++ apps/main/src/dex/components/FieldHelperUsdRate.tsx
@@ -1,7 +1,7 @@
+import BigNumber from 'bignumber.js'
 import { useMemo } from 'react'
 import { styled } from 'styled-components'
 import { Chip } from '@ui/Typography/Chip'
-import { BN } from '@ui/utils'
 import { amount as toAmount, formatNumber } from '@ui-kit/utils'
 
 export const FieldHelperUsdRate = ({ amount, usdRate }: { amount: string; usdRate: number | undefined }) => {
@@ -9,7 +9,7 @@ export const FieldHelperUsdRate = ({ amount, usdRate }: { amount: string; usdRat
     let total = ''
 
     if (usdRate != null && +usdRate > 0 && +amount > 0) {
-      total = BN(usdRate).multipliedBy(amount).toString()
+      total = BigNumber(usdRate).multipliedBy(amount).toFixed()
     }
     return total
   }, [usdRate, amount])
--- packages/ui/src/utils/utilsFormat.ts
+++
@@ -1,5 +0,0 @@
-import BigNumber from 'bignumber.js'
-
-// At the moment of writing there's a Notion ticket to remove BigNumber.js
-BigNumber.config({ EXPONENTIAL_AT: 20, ROUNDING_MODE: BigNumber.ROUND_HALF_UP })
-export const BN = BigNumber
