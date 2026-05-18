0f00b778afddde3e06c7a851fc76ae731b13c857
JustJousting <justjousting2moon@gmail.com>
2026-05-18 23:32:18 +0300

    refactor: add loading skeleton

--- apps/main/src/llamalend/widgets/small-liquidation-range-chart/SmallLiquidationRangeChart.tsx
+++ apps/main/src/llamalend/widgets/small-liquidation-range-chart/SmallLiquidationRangeChart.tsx
@@ -1,13 +1,48 @@
 import ReactECharts from 'echarts-for-react'
+import { useMemo } from 'react'
 import Box from '@mui/material/Box'
+import Skeleton from '@mui/material/Skeleton'
 import Stack from '@mui/material/Stack'
 import { SMALL_LIQUIDATION_RANGE_CHART_HEIGHT_PX } from './small-liquidation-range-chart.constants'
-import type { SmallLiquidationRangeChartProps } from './small-liquidation-range-chart.types'
+import type { SmallLiquidationRangeChartData, SmallLiquidationRangeChartProps } from './small-liquidation-range-chart.types'
 import { useSmallLiquidationRangeChartOption } from './useSmallLiquidationRangeChartOption'
 
 export type { SmallLiquidationRangeChartProps } from './small-liquidation-range-chart.types'
 
-export const SmallLiquidationRangeChart = ({ liquidationRanges, oraclePrice }: SmallLiquidationRangeChartProps) => {
+const useSmallLiquidationRangeChartData = ({
+  prices,
+  prevPrices,
+  oraclePrice,
+}: SmallLiquidationRangeChartProps): SmallLiquidationRangeChartData =>
+  useMemo(() => {
+    const currentRange = prevPrices.data
+    const newRange = prices.data ?? undefined
+    const chartOraclePrice = oraclePrice.data ?? undefined
+    const hasError = !!prices.error || !!prevPrices.error || !!oraclePrice.error
+    const loading = !hasError && (prices.isLoading || prevPrices.isLoading || oraclePrice.isLoading)
+
+    return {
+      liquidationRanges: {
+        ...(currentRange && { currentRange }),
+        ...(newRange && { newRange }),
+      },
+      oraclePrice: chartOraclePrice,
+      loading,
+    }
+  }, [
+    oraclePrice.data,
+    oraclePrice.error,
+    oraclePrice.isLoading,
+    prevPrices.data,
+    prevPrices.error,
+    prevPrices.isLoading,
+    prices.data,
+    prices.error,
+    prices.isLoading,
+  ])
+
+export const SmallLiquidationRangeChart = (props: SmallLiquidationRangeChartProps) => {
+  const { liquidationRanges, oraclePrice, loading } = useSmallLiquidationRangeChartData(props)
   const option = useSmallLiquidationRangeChartOption({ liquidationRanges, oraclePrice })
 
   return (
@@ -19,11 +54,15 @@ export const SmallLiquidationRangeChart = ({ liquidationRanges, oraclePrice }: S
           minHeight: `${SMALL_LIQUIDATION_RANGE_CHART_HEIGHT_PX}px`,
         }}
       >
-        <ReactECharts
-          option={option}
-          notMerge
-          style={{ width: '100%', height: SMALL_LIQUIDATION_RANGE_CHART_HEIGHT_PX }}
-        />
+        {loading ? (
+          <Skeleton variant="rectangular" width="100%" height={SMALL_LIQUIDATION_RANGE_CHART_HEIGHT_PX} />
+        ) : (
+          <ReactECharts
+            option={option}
+            notMerge
+            style={{ width: '100%', height: SMALL_LIQUIDATION_RANGE_CHART_HEIGHT_PX }}
+          />
+        )}
       </Box>
     </Stack>
   )
