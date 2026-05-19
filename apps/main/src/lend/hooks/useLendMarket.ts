2a7a4ac0be57dec67f393b9dc8c59883171697ac
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-05-19 14:47:24 +0200

    Revert "Merge pull request #2539 from curvefi/fix/rpc"
    This reverts commit ffd1d68b7c51577b6fa6627ee62e838001b582c0, reversing
    changes made to 1860c121cabb229eb5f816e5996d51fb5cd27ad3.

--- apps/main/src/lend/hooks/useLendMarket.ts
+++ apps/main/src/lend/hooks/useLendMarket.ts
@@ -4,15 +4,15 @@ import { useCurve } from '@ui-kit/features/connect-wallet'
 import { useLendMarkets } from '../queries/lend-markets.query'
 import { ChainId } from '../types/lend.types'
 
-export function useLendMarketData(chainId: ChainId, rMarket: string, enabled?: boolean) {
-  const { data, error, isSuccess } = useLendMarkets({ chainId, enableLLv2: useLLv2() }, enabled)
+export function useLendMarketData(chainId: ChainId, rMarket: string) {
+  const { data, error, isSuccess } = useLendMarkets({ chainId, enableLLv2: useLLv2() })
   const marketData = useMemo(() => data?.[rMarket], [data, rMarket])
   return { error, isSuccess, data: marketData }
 }
 
-export const useLendMarket = (chainId: ChainId, rMarket: string, enabled?: boolean) => {
+export const useLendMarket = (chainId: ChainId, rMarket: string) => {
   const { llamaApi: api } = useCurve()
-  const { data, error, isSuccess } = useLendMarketData(chainId, rMarket, enabled)
+  const { data, error, isSuccess } = useLendMarketData(chainId, rMarket)
   const market = useMemo(() => api && data && api.getLendMarketByData(data.id, data), [api, data])
   return { data: market, error, isSuccess: isSuccess && !!api }
 }
--- apps/main/src/loan/hooks/useMintMarket.ts
+++ apps/main/src/loan/hooks/useMintMarket.ts
@@ -3,15 +3,15 @@ import { useCurve } from '@ui-kit/features/connect-wallet'
 import { useMintMarkets } from '../entities/mint-markets.query'
 import { ChainId } from '../types/loan.types'
 
-export function useMintMarketData(chainId: ChainId, rMarket: string, enabled?: boolean) {
-  const { data, error, isSuccess } = useMintMarkets({ chainId }, enabled)
+export function useMintMarketData(chainId: ChainId, rMarket: string) {
+  const { data, error, isSuccess } = useMintMarkets({ chainId })
   const marketData = useMemo(() => data?.[rMarket], [data, rMarket])
   return { error, isSuccess, data: marketData }
 }
 
-export const useMintMarket = (chainId: ChainId, rMarket: string, enabled?: boolean) => {
+export const useMintMarket = (chainId: ChainId, rMarket: string) => {
   const { llamaApi: api } = useCurve()
-  const { data, error, isSuccess } = useMintMarketData(chainId, rMarket, enabled)
+  const { data, error, isSuccess } = useMintMarketData(chainId, rMarket)
   const market = useMemo(() => api && data && api.getMintMarketByData(data.id, data), [api, data])
   return { data: market, error, isSuccess: isSuccess && !!api }
 }
