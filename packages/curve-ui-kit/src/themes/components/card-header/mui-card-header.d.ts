de295330e108d97eb9e53a4c9bbf64d4639b8ea7
Alunara <alunara@curve.finance>
2026-05-23 15:07:42 +0200

    refactor: add inline size to cards

--- packages/curve-ui-kit/src/themes/components/card-header/mui-card-header.d.ts
+++ packages/curve-ui-kit/src/themes/components/card-header/mui-card-header.d.ts
@@ -2,7 +2,6 @@ import '@mui/material/CardHeader'
 
 declare module '@mui/material/CardHeader' {
   export interface CardHeaderOwnProps {
-    size?: 'small'
-    'data-inline'?: boolean
+    size?: 'small' | 'inline'
   }
 }
