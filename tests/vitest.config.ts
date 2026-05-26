03c24779174c3026f7761fd6330b442bcafdcee7
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-05-26 17:23:20 +0200

    fix: vitest alias

--- tests/vitest.config.ts
+++ tests/vitest.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config'
+import { resolve } from 'path'
 
 export default defineConfig({
   test: {
     environment: 'node',
     globals: true,
     include: ['../packages/*/src/**/*.{test,spec}.{js,ts}'],
+  },
+  resolve: {
+    alias: [{ find: '@primitives', replacement: resolve(__dirname, '../packages/primitives/src') }],
   },
 })
