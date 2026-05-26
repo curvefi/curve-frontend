a6bed14783340bbb89f3f37bfb2c1d2859770c59
Alunara <alunara@curve.finance>
2026-05-26 17:27:47 +0200

    refactor: rename getTestId to getTestById

--- tests/cypress/e2e/dex/refuel.cy.ts
+++ tests/cypress/e2e/dex/refuel.cy.ts
@@ -5,14 +5,14 @@ const REFUEL_POOL_ADDRESS = '0x6e5492f8ea2370844ee098a56dd88e1717e4a9c2'
 const REFUEL_ROUTE = `dex/ethereum/pools/${REFUEL_POOL_ADDRESS}/refuel` satisfies AppRoute
 
 /** Smol helper to reduce amount of repetition */
-const getTestId = (testId: string, options?: Partial<Cypress.Timeoutable>) =>
+const getTestById = (testId: string, options?: Partial<Cypress.Timeoutable>) =>
   cy.get(`[data-testid="refuel-${testId}"]`, options)
 
 describe('Refuel page', () => {
   const visitRefuelPage = () => {
     cy.visitWithoutTestConnector(REFUEL_ROUTE)
     cy.location('pathname', LOAD_TIMEOUT).should('equal', `/${REFUEL_ROUTE}`)
-    getTestId('page', LOAD_TIMEOUT).should('be.visible')
+    getTestById('page', LOAD_TIMEOUT).should('be.visible')
   }
 
   it('renders the crvUSD/WETH refuel dashboard and disconnected form state', () => {
@@ -26,50 +26,50 @@ describe('Refuel page', () => {
     cy.contains('crvUSD', API_LOAD_TIMEOUT).should('be.visible')
     cy.contains('WETH', API_LOAD_TIMEOUT).should('be.visible')
 
-    getTestId('connect-wallet-button').should('be.visible').and('contain.text', 'Connect Wallet')
-    getTestId('submit-button').should('not.exist')
+    getTestById('connect-wallet-button').should('be.visible').and('contain.text', 'Connect Wallet')
+    getTestById('submit-button').should('not.exist')
 
-    getTestId('size-action-info').should('contain.text', 'Percentage of pool TVL')
-    getTestId('weekly-action-info').should('contain.text', 'Weekly')
-    getTestId('bi-weekly-action-info').should('contain.text', 'Bi-weekly')
-    getTestId('monthly-action-info').should('contain.text', 'Monthly')
+    getTestById('size-action-info').should('contain.text', 'Percentage of pool TVL')
+    getTestById('weekly-action-info').should('contain.text', 'Weekly')
+    getTestById('bi-weekly-action-info').should('contain.text', 'Bi-weekly')
+    getTestById('monthly-action-info').should('contain.text', 'Monthly')
 
-    getTestId('size-action-info-value').should('have.attr', 'data-value', '-')
-    getTestId('weekly-action-info-value').should('have.attr', 'data-value', '-')
-    getTestId('bi-weekly-action-info-value').should('have.attr', 'data-value', '-')
-    getTestId('monthly-action-info-value').should('have.attr', 'data-value', '-')
+    getTestById('size-action-info-value').should('have.attr', 'data-value', '-')
+    getTestById('weekly-action-info-value').should('have.attr', 'data-value', '-')
+    getTestById('bi-weekly-action-info-value').should('have.attr', 'data-value', '-')
+    getTestById('monthly-action-info-value').should('have.attr', 'data-value', '-')
 
-    getTestId('pool-information', API_LOAD_TIMEOUT).should('be.visible')
-    getTestId('pool-tvl-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
-    getTestId('pool-volume-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
-    getTestId('pool-fees-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
-    getTestId('pool-apr-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
+    getTestById('pool-information', API_LOAD_TIMEOUT).should('be.visible')
+    getTestById('pool-tvl-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
+    getTestById('pool-volume-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
+    getTestById('pool-fees-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
+    getTestById('pool-apr-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
 
-    getTestId('prices-chart', API_LOAD_TIMEOUT).should('be.visible')
-    getTestId('lp-token-value-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
-    getTestId('virtual-price-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
-    getTestId('prices-chart').contains('Last price').should('be.visible')
-    getTestId('prices-chart').contains('Oracle price').should('be.visible')
-    getTestId('prices-chart').contains('Price scale').should('be.visible')
+    getTestById('prices-chart', API_LOAD_TIMEOUT).should('be.visible')
+    getTestById('lp-token-value-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
+    getTestById('virtual-price-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
+    getTestById('prices-chart').contains('Last price').should('be.visible')
+    getTestById('prices-chart').contains('Oracle price').should('be.visible')
+    getTestById('prices-chart').contains('Price scale').should('be.visible')
 
-    getTestId('budget-chart', API_LOAD_TIMEOUT).should('be.visible')
-    getTestId('shares-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
-    getTestId('duration-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
-    getTestId('max-ratio-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
-    getTestId('budget-chart').contains('Refuel shares').should('be.visible')
-    getTestId('budget-chart').contains('Unlocked refuel shares').should('be.visible')
+    getTestById('budget-chart', API_LOAD_TIMEOUT).should('be.visible')
+    getTestById('shares-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
+    getTestById('duration-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
+    getTestById('max-ratio-value', API_LOAD_TIMEOUT).invoke('attr', 'data-value').should('match', /\d/)
+    getTestById('budget-chart').contains('Refuel shares').should('be.visible')
+    getTestById('budget-chart').contains('Unlocked refuel shares').should('be.visible')
 
-    getTestId('reserves-composition-chart', API_LOAD_TIMEOUT).should('be.visible')
-    getTestId('reserves-composition-chart').contains('crvUSD').should('be.visible')
-    getTestId('reserves-composition-chart').contains('WETH').should('be.visible')
+    getTestById('reserves-composition-chart', API_LOAD_TIMEOUT).should('be.visible')
+    getTestById('reserves-composition-chart').contains('crvUSD').should('be.visible')
+    getTestById('reserves-composition-chart').contains('WETH').should('be.visible')
 
-    getTestId('daily-refuels-chart', API_LOAD_TIMEOUT).should('be.visible')
-    getTestId('daily-refuels-chart').contains('Daily refuels').should('be.visible')
-    getTestId('daily-refuels-chart').contains('Refuel count').should('be.visible')
+    getTestById('daily-refuels-chart', API_LOAD_TIMEOUT).should('be.visible')
+    getTestById('daily-refuels-chart').contains('Daily refuels').should('be.visible')
+    getTestById('daily-refuels-chart').contains('Refuel count').should('be.visible')
 
-    getTestId('recent-refuels', API_LOAD_TIMEOUT).should('be.visible')
-    getTestId('recent-refuels').find('[data-testid="data-table"]', API_LOAD_TIMEOUT).should('be.visible')
-    getTestId('recent-refuels')
+    getTestById('recent-refuels', API_LOAD_TIMEOUT).should('be.visible')
+    getTestById('recent-refuels').find('[data-testid="data-table"]', API_LOAD_TIMEOUT).should('be.visible')
+    getTestById('recent-refuels')
       .find('[data-testid^="data-table-row-"]', API_LOAD_TIMEOUT)
       .its('length')
       .should('be.greaterThan', 0)
