719f3a3ac30756e986c81a6c2104ae7c7a3be2c7
0xtutti <0xtutti@protonmail.com>
2026-06-01 17:05:30 +0200

    fix(ui-kit): refresh mona sans font

--- packages/curve-ui-kit/.storybook/preview.tsx
+++ packages/curve-ui-kit/.storybook/preview.tsx
@@ -36,7 +36,10 @@ const decorators: Decorator[] = [
         <style>
           {`
         @font-face {
-          font-family: MonaSans;
+          font-family: 'Mona Sans';
+          font-weight: 200 900;
+          font-style: normal;
+          font-display: swap;
           src: url('fonts/Mona-Sans.woff2') format('woff2');
           unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
         }
