b56e7492c3bc4b2f951808b7208358a1cfd7ee14
0xtutti <0xtutti@protonmail.com>
2026-06-01 16:32:27 +0200

    fix(ui-kit): remove minecraft font fallback

--- packages/curve-ui-kit/.storybook/preview.tsx
+++ packages/curve-ui-kit/.storybook/preview.tsx
@@ -41,16 +41,6 @@ const decorators: Decorator[] = [
           unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
         }
         @font-face {
-          font-family: Minecraft;
-          font-weight: normal;
-          src: url('fonts/Minecraft-Regular.otf') format('opentype');
-        }
-        @font-face {
-          font-family: Minecraft;
-          font-weight: bold;
-          src: url('fonts/Minecraft-Bold.otf') format('opentype');
-        }
-        @font-face {
           font-family: 'Ioskeley Mono';
           font-weight: 300;
           font-style: normal;
