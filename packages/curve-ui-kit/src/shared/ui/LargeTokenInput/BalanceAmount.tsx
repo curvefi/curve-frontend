ffc91e8e855860604fbf53dd2ffe387e9e60f48d
Alunara <alunara@curve.finance>
2026-06-04 00:41:56 +0200

    fix: BalanceAmount should format balances compact as they're clickable

--- packages/curve-ui-kit/src/shared/ui/LargeTokenInput/BalanceAmount.tsx
+++ packages/curve-ui-kit/src/shared/ui/LargeTokenInput/BalanceAmount.tsx
@@ -33,7 +33,7 @@ export const BalanceAmount = <T extends Amount>({
         ...sx,
       }}
     >
-      {loading ? '?????' : children == null ? '-' : formatNumber(children, 'token.balance')}
+      {loading ? '?????' : children == null ? '-' : formatNumber(children, 'token.compact')}
     </Typography>
   </WithSkeleton>
 )
