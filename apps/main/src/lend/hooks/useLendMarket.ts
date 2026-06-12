d3d9cdbd44596de81fa894232c81baae19c5e55c
Alunara <alunara@curve.finance>
2026-06-12 19:03:18 +0200

    refactor: use useMappedQuery

--- apps/main/src/lend/hooks/useLendMarket.ts
+++ apps/main/src/lend/hooks/useLendMarket.ts
@@ -1,19 +1,19 @@
 import { useLLv2 } from 'curve-ui-kit/src/hooks/useFeatureFlags'
 import { useCurve } from '@ui-kit/features/connect-wallet'
-import { mapQuery } from '@ui-kit/types/util'
+import { useMappedQuery } from '@ui-kit/types/util'
 import { useLendMarkets } from '../queries/lend-markets.query'
 import { ChainId } from '../types/lend.types'
 
 export function useLendMarketData(chainId: ChainId, rMarket: string, enabled?: boolean) {
   const lendMarkets = useLendMarkets({ chainId, enableLLv2: useLLv2() }, enabled)
-  return { ...mapQuery(lendMarkets, data => data?.[rMarket]), isSuccess: lendMarkets.isSuccess }
+  return { ...useMappedQuery(lendMarkets, data => data?.[rMarket]), isSuccess: lendMarkets.isSuccess }
 }
 
 export const useLendMarket = (chainId: ChainId, rMarket: string, enabled?: boolean) => {
   const { llamaApi: api } = useCurve()
   const lendMarketData = useLendMarketData(chainId, rMarket, enabled)
   return {
-    ...mapQuery(lendMarketData, data => api && data && api.getLendMarketByData(data.id, data)),
+    ...useMappedQuery(lendMarketData, data => api && data && api.getLendMarketByData(data.id, data)),
     isSuccess: lendMarketData.isSuccess && !!api,
   }
 }
--- apps/main/src/loan/hooks/useMintMarket.ts
+++ apps/main/src/loan/hooks/useMintMarket.ts
@@ -1,18 +1,18 @@
 import { useCurve } from '@ui-kit/features/connect-wallet'
-import { mapQuery } from '@ui-kit/types/util'
+import { useMappedQuery } from '@ui-kit/types/util'
 import { useMintMarkets } from '../entities/mint-markets.query'
 import { ChainId } from '../types/loan.types'
 
 export function useMintMarketData(chainId: ChainId, rMarket: string, enabled?: boolean) {
   const mintMarkets = useMintMarkets({ chainId }, enabled)
-  return { ...mapQuery(mintMarkets, data => data?.[rMarket]), isSuccess: mintMarkets.isSuccess }
+  return { ...useMappedQuery(mintMarkets, data => data?.[rMarket]), isSuccess: mintMarkets.isSuccess }
 }
 
 export const useMintMarket = (chainId: ChainId, rMarket: string, enabled?: boolean) => {
   const { llamaApi: api } = useCurve()
   const mintMarketData = useMintMarketData(chainId, rMarket, enabled)
   return {
-    ...mapQuery(mintMarketData, data => api && data && api.getMintMarketByData(data.id, data)),
+    ...useMappedQuery(mintMarketData, data => api && data && api.getMintMarketByData(data.id, data)),
     isSuccess: mintMarketData.isSuccess && !!api,
   }
 }
