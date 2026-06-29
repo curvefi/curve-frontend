a918e6821e867ed06fc190d2a05b8d6d517c14f6
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-06-29 17:29:44 +0200

    feat: add fee receiver for enso and 0x

---
+++ apps/router-api/src/router-fees.ts
@@ -0,0 +1,10 @@
+import type { Address } from '@primitives/address.utils'
+import type { Decimal } from '@primitives/decimal.utils'
+import { Chain } from '@primitives/network.utils'
+
+export const ROUTER_FEE_BPS: Decimal = '7'
+
+export const ROUTER_FEE_RECEIVER_BY_CHAIN_ID: Record<number, Address> = {
+  [Chain.Ethereum]: '0xB4c2C0B045fA0517cACEebC917443Fa041A9c18B',
+  [Chain.Optimism]: '0x3Aa9742e8BA5eA0F573FcE69e1c8b49aFd0Af610',
+}
