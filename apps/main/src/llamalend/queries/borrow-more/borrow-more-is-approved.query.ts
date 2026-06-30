59f6c5047e540913e29bc10d960070ef8b913235
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-06-30 09:53:17 +0200

    fix: typecheck

--- apps/main/src/llamalend/queries/borrow-more/borrow-more-is-approved.query.ts
+++ apps/main/src/llamalend/queries/borrow-more/borrow-more-is-approved.query.ts
@@ -1,6 +1,7 @@
 import { getBorrowMoreImplementation } from '@/llamalend/queries/borrow-more/borrow-more-query.helpers'
 import type { BorrowMoreParams, BorrowMoreQuery } from '@/llamalend/queries/validation/borrow-more.validation'
 import { borrowMoreValidationSuite } from '@/llamalend/queries/validation/borrow-more.validation'
+import { assert } from '@primitives/objects.utils'
 import { queryFactory, rootKeys } from '@ui-kit/lib/model'
 
 export const {
@@ -31,7 +32,8 @@ export const {
     const [type, impl] = getBorrowMoreImplementation(marketId, leverageEnabled)
     switch (type) {
       case 'zapV2':
-        return await impl.borrowMoreIsApproved({ userCollateral, userBorrowed })
+        assert(!+userBorrowed, `Unsupported userBorrowed for zapv2: ${userBorrowed}`)
+        return await impl.borrowMoreIsApproved({ userCollateral })
       case 'V1':
       case 'V2':
         return await impl.borrowMoreIsApproved(userCollateral, userBorrowed)
