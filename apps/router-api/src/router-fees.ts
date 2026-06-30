a2e6f599718d870e4ff51449045985e03d057852
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-06-30 12:24:39 +0200

    chore: increase fee bps

--- apps/router-api/src/router-fees.ts
+++ apps/router-api/src/router-fees.ts
@@ -2,7 +2,7 @@ import type { Address } from '@primitives/address.utils'
 import type { Decimal } from '@primitives/decimal.utils'
 import { Chain } from '@primitives/network.utils'
 
-export const ROUTER_FEE_BPS: Decimal = '7'
+export const ROUTER_FEE_BPS: Decimal = '8'  // note: no fractions allowed by enso
 
 export const ROUTER_FEE_RECEIVER_BY_CHAIN_ID: Record<number, Address> = {
   [Chain.Ethereum]: '0xB4c2C0B045fA0517cACEebC917443Fa041A9c18B',
