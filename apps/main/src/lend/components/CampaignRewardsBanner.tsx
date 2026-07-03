ef41b58fd030c84903325ddc8dbcfa1eaa253e2d
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-07-03 14:16:35 +0200

    refactor: campaign rewards

--- apps/main/src/lend/components/CampaignRewardsBanner.tsx
+++ apps/main/src/lend/components/CampaignRewardsBanner.tsx
@@ -1,10 +1,10 @@
 import { networks } from '@/lend/networks'
 import { ChainId } from '@/lend/types/lend.types'
-import { getControllerAddress } from '@/llamalend/llama.utils'
+import { getControllerAddress, getVaultAddress } from '@/llamalend/llama.utils'
 import { LendMarketTemplate } from '@curvefi/llamalend-api/lib/lendMarkets'
 import type { Chain } from '@curvefi/prices-api'
-import type { Address } from '@primitives/address.utils'
 import { notFalsy } from '@primitives/objects.utils'
+import { capitalize } from '@primitives/string.utils'
 import { CampaignBannerComp } from '@ui/CampaignRewards/CampaignBannerComp'
 import { useCampaignsByAddress } from '@ui-kit/entities/campaigns'
 import { t } from '@ui-kit/lib/i18n'
@@ -16,20 +16,10 @@ type CampaignRewardsBannerProps = {
 
 export const CampaignRewardsBanner = ({ chainId, market }: CampaignRewardsBannerProps) => {
   const blockchainId = networks[chainId].id as Chain
-  const { data: supplyCampaigns } = useCampaignsByAddress({
-    blockchainId,
-    address: market?.addresses.vault as Address | undefined,
-  })
+  const { data: supplyCampaigns } = useCampaignsByAddress({ blockchainId, address: getVaultAddress(market) })
   const { data: borrowCampaigns } = useCampaignsByAddress({ blockchainId, address: getControllerAddress(market) })
 
-  const action =
-    supplyCampaigns.length && borrowCampaigns.length
-      ? t`Suppling and borrowing`
-      : supplyCampaigns.length
-        ? t`Supplying`
-        : borrowCampaigns.length
-          ? t`Borrowing`
-          : ''
+  const action = notFalsy(supplyCampaigns.length && t`supplying`, borrowCampaigns.length && t`borrowing`).join(' and ')
 
   const rewardTypes = notFalsy(...supplyCampaigns.concat(borrowCampaigns).map(campaign => campaign.reward?.type))
   const hasApr = rewardTypes.includes('apr')
@@ -40,7 +30,7 @@ export const CampaignRewardsBanner = ({ chainId, market }: CampaignRewardsBanner
     supplyCampaigns.length + borrowCampaigns.length > 0 && (
       <CampaignBannerComp
         campaignRewards={[...supplyCampaigns, ...borrowCampaigns]}
-        message={t`${action} in this market earns ${rewardType}`}
+        message={t`${capitalize(action)} in this market earns ${rewardType}`}
       />
     )
   )
