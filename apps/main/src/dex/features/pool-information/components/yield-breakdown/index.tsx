96cafac7e641d27c4ea1b55afe385658322cabe9
Alunara <alunara@curve.finance>
2026-07-03 15:26:17 +0200

    fix: negative base APY wasn't shown in yield breakdown

--- apps/main/src/dex/features/pool-information/components/yield-breakdown/index.tsx
+++ apps/main/src/dex/features/pool-information/components/yield-breakdown/index.tsx
@@ -38,7 +38,7 @@ export const YieldBreakdown = ({
 
   return (
     rows.length > 0 &&
-    total > 0 && (
+    total != 0 && (
       <Stack>
         <CardHeader title={t`Yield Breakdown`} size="small" />
         <DataTable<YieldBreakdownRow>
