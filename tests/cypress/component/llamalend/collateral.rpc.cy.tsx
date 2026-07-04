7892413ba9f7723b3dcc43cfd44bfa3247f2b06a
Alunara <alunara@curve.finance>
2026-07-04 12:27:17 +0200

    test: manually create a soft liq position rather than fork

--- tests/cypress/component/llamalend/collateral.rpc.cy.tsx
+++ tests/cypress/component/llamalend/collateral.rpc.cy.tsx
@@ -31,8 +31,8 @@ describe('Collateral forms', () => {
       chainId,
       collateral,
       collateralAddress,
-      controllerAddress,
       collateralDecimals,
+      controllerAddress,
       id,
       label,
       marketType,
@@ -88,6 +88,9 @@ describe('Collateral forms', () => {
             collateral,
             collateralDecimals,
             borrow,
+            borrowedDecimals,
+            range: 10n,
+            collateralFundingMultiplier: 2n,
           })
         })
 
