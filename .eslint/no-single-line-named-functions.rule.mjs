b3a7e2578317276dabb60a086584edf241b5e9a4
Daniel Schiavini <daniel.schiavini@gmail.com>
2026-07-13 15:05:41 +0200

    refactor: don't check for `this`

--- .eslint/no-single-line-named-functions.rule.mjs
+++ .eslint/no-single-line-named-functions.rule.mjs
@@ -3,11 +3,8 @@ const isNamedFunction = node => node.type === 'FunctionDeclaration' || (node.typ
 const hasSingleLineBody = node =>
   node.body?.body.length === 1 && node.body.body[0].loc?.start.line === node.body.body[0].loc?.end.line
 
-const containsThisExpression = (sourceCode, node) =>
-  sourceCode.getTokens(node.body).some(token => token.type === 'Keyword' && token.value === 'this')
-
 /**
- * Forbids one-line named functions. Prefer one-line arrow functions unless a function `this` binding is needed.
+ * Forbids one-line named functions. Prefer one-line arrow functions.
  *
  * @type {eslint.Rule.Module}
  */
@@ -16,15 +13,11 @@ export const noSingleLineNamedFunctionsRule = {
     type: 'suggestion',
     docs: { description: 'Disallow single-line named functions when an arrow function is enough' },
     messages: {
-      preferArrow:
-        'Prefer a one-line arrow function instead of a named function. Use a named function only when it needs its own `this` binding.',
+      preferArrow: 'Prefer a one-line arrow function instead of a named function.',
     },
   },
   create: context => ({
     ':function': node =>
-      isNamedFunction(node) &&
-      hasSingleLineBody(node) &&
-      !containsThisExpression(context.sourceCode, node) &&
-      context.report({ node, messageId: 'preferArrow' }),
+      isNamedFunction(node) && hasSingleLineBody(node) && context.report({ node, messageId: 'preferArrow' }),
   }),
 }
