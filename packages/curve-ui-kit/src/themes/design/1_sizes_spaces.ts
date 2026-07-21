435a23e229624dd6c24d0be8fd877ba4fe8d9b76
0xtutti <0xtutti@protonmail.com>
2026-07-21 18:08:33 +0200

    feat(ui-kit): add extra-small tabs

--- packages/curve-ui-kit/src/themes/design/1_sizes_spaces.ts
+++ packages/curve-ui-kit/src/themes/design/1_sizes_spaces.ts
@@ -57,6 +57,10 @@ const MappedButtonSize = {
   lg: Sizing[650], // 56px
 }
 
+const MappedTabPadding = {
+  extraSmall: { x: Spacing[300] },
+} as const
+
 const MappedHorizontalMetricSize = {
   xs: Sizing[250], // 20px
   sm: Sizing[300], // 24px
@@ -241,6 +245,7 @@ export const SizesAndSpaces = {
   Sizing: MappedSizing,
   IconSize: MappedIconSize,
   ButtonSize: MappedButtonSize,
+  Tab: { Padding: MappedTabPadding },
   InputSize: MappedInputSize,
   InputSpacing: MappedInputSpacing,
   SelectSize: MappedSelectSize,
