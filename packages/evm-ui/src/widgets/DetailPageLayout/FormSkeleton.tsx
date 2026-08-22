2d5d1f48dfce88e063c93a9b8b2d52fa5d31f0ca
0xLokiB <305783192+0xLokiB@users.noreply.github.com>
2026-08-22 13:52:16 +0200

    chore: form skeleton height improvements

--- packages/evm-ui/src/widgets/DetailPageLayout/FormSkeleton.tsx
+++ packages/evm-ui/src/widgets/DetailPageLayout/FormSkeleton.tsx
@@ -4,7 +4,7 @@ import Typography from '@mui/material/Typography'
 import { noop } from '@tanstack/react-query'
 import { BUTTON_FORM_SIZE } from '@ui-kit/features/forms/constants'
 import { t } from '@ui-kit/lib/i18n'
-import { LargeTokenInput } from '@ui-kit/shared/ui/LargeTokenInput'
+import { HelperMessage, LargeTokenInput } from '@ui-kit/shared/ui/LargeTokenInput'
 import { FormContent } from './FormContent'
 import { FormTabs } from './FormTabs'
 
@@ -14,14 +14,24 @@ export const FormSkeleton = () => (
     menu={[
       {
         value: 'tab',
-        label: <Skeleton width={100} />,
+        label: (
+          <Skeleton variant="rectangular" width={100}>
+            <Typography variant="buttonTabsM">{t`Loading`}</Typography>
+          </Skeleton>
+        ),
         component: () => (
           <FormContent>
-            <Skeleton width="100%">
-              <LargeTokenInput name="loading" onBalance={noop} message={t`Loading`} />
-            </Skeleton>
-            <Skeleton width="100%">
-              <Typography variant="buttonM">{t`Loading form...`}</Typography>
+            <Skeleton variant="rectangular" width="100%">
+              <LargeTokenInput
+                name="loading"
+                onBalance={noop}
+                label={t`Loading`}
+                tokenSelector={<Typography variant="bodyMBold">{t`Token`}</Typography>}
+                walletBalance={{ balance: '0', symbol: t`Token` }}
+                inputBalanceUsd="0"
+              >
+                <HelperMessage message={t`Loading`} />
+              </LargeTokenInput>
             </Skeleton>
             <Button loading disabled fullWidth size={BUTTON_FORM_SIZE} />
           </FormContent>
