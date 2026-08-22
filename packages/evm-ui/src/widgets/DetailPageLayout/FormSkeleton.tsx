93a7150b71ef9fe81721b0b0a7184ce62efb9f91
0xLokiB <305783192+0xLokiB@users.noreply.github.com>
2026-08-22 14:08:08 +0200

    test: form skeleton height

--- packages/evm-ui/src/widgets/DetailPageLayout/FormSkeleton.tsx
+++ packages/evm-ui/src/widgets/DetailPageLayout/FormSkeleton.tsx
@@ -4,7 +4,7 @@ import Typography from '@mui/material/Typography'
 import { noop } from '@tanstack/react-query'
 import { BUTTON_FORM_SIZE } from '@ui-kit/features/forms/constants'
 import { t } from '@ui-kit/lib/i18n'
-import { HelperMessage, LargeTokenInput } from '@ui-kit/shared/ui/LargeTokenInput'
+import { LargeTokenInput } from '@ui-kit/shared/ui/LargeTokenInput'
 import { FormContent } from './FormContent'
 import { FormTabs } from './FormTabs'
 
@@ -29,9 +29,7 @@ export const FormSkeleton = () => (
                 tokenSelector={<Typography variant="bodyMBold">{t`Token`}</Typography>}
                 walletBalance={{ balance: '0', symbol: t`Token` }}
                 inputBalanceUsd="0"
-              >
-                <HelperMessage message={t`Loading`} />
-              </LargeTokenInput>
+              />
             </Skeleton>
             <Button loading disabled fullWidth size={BUTTON_FORM_SIZE} />
           </FormContent>
