bef86f996def48f1ac2aa7a9dd2ae5d01ce3b44f
Alunara <alunara@curve.finance>
2026-08-26 00:35:32 +0200

    feat: add new pool title tooltip

--- apps/main/src/dex/features/pool-list/cells/PoolTitleCell/index.tsx
+++ apps/main/src/dex/features/pool-list/cells/PoolTitleCell/index.tsx
@@ -2,23 +2,27 @@ import { t } from '@evm-ui/lib/i18n'
 import { TableRowTitle } from '@evm-ui/shared/ui/DataTable/TableRowTitle'
 import { UserPositionIndicator } from '@evm-ui/shared/ui/DataTable/UserPositionIndicator'
 import { TokenIcons } from '@evm-ui/shared/ui/TokenIcons'
+import { Tooltip } from '@evm-ui/shared/ui/Tooltip'
 import { SizesAndSpaces } from '@evm-ui/themes/design/1_sizes_spaces'
 import Stack from '@mui/material/Stack'
 import type { CellContext } from '@tanstack/react-table'
 import type { PoolRow } from '../../types'
 import { PoolBadges } from './PoolBadges'
+import { PoolTooltipContent } from './PoolTooltipContent'
 
 const { Spacing, Height } = SizesAndSpaces
 
 export const PoolTitleCell = ({ row: { original: pool } }: CellContext<PoolRow, string>) => (
   <Stack direction="row" sx={{ height: Height.row }}>
     {pool.hasPosition && <UserPositionIndicator tooltipTitle={t`You have a balance in this pool`} />}
-    <Stack direction="row" sx={{ alignItems: 'center', gap: Spacing.sm }}>
-      <TokenIcons blockchainId={pool.network} tokens={pool.tradeableCoins} />
-      <Stack direction="column" sx={{ justifyContent: 'center', gap: Spacing.xxs }}>
-        <TableRowTitle id={pool.address} url={pool.url} title={pool.name} />
-        <PoolBadges pool={pool} />
+    <Tooltip clickable title={pool.name} body={<PoolTooltipContent pool={pool} />} placement="top">
+      <Stack direction="row" sx={{ alignItems: 'center', gap: Spacing.sm }}>
+        <TokenIcons blockchainId={pool.network} tokens={pool.tradeableCoins} showTooltips={false} />
+        <Stack direction="column" sx={{ justifyContent: 'center', gap: Spacing.xxs }}>
+          <TableRowTitle id={pool.address} url={pool.url} title={pool.name} />
+          <PoolBadges pool={pool} />
+        </Stack>
       </Stack>
-    </Stack>
+    </Tooltip>
   </Stack>
 )
