0e74475d220f3cc70b5f082c31e3583eca1350a9
0xLokiB <305783192+0xLokiB@users.noreply.github.com>
2026-08-28 15:48:24 +0200

    fix: typecheck

--- apps/main/src/dex/entities/gauge/lib/gauge-info.ts
+++ apps/main/src/dex/entities/gauge/lib/gauge-info.ts
@@ -3,21 +3,8 @@ import {
   useEstimateGasDepositReward,
   useEstimateGasDepositRewardApprove,
   useGaugeDepositRewardIsApproved,
-  useGaugeManager,
-  useGaugeRewardsDistributors,
-  useIsDepositRewardAvailable,
 } from '@/dex/entities/gauge/model'
 import { createApprovedEstimateGasHook, createEstimateGasHook } from '@evm-ui/lib/model/entities/gas-info'
-
-export {
-  useEstimateGasAddRewardToken,
-  useEstimateGasDepositReward,
-  useEstimateGasDepositRewardApprove,
-  useGaugeDepositRewardIsApproved,
-  useGaugeManager,
-  useGaugeRewardsDistributors,
-  useIsDepositRewardAvailable,
-}
 
 export const useDepositRewardEstimateGas = createApprovedEstimateGasHook({
   useIsApproved: useGaugeDepositRewardIsApproved,
