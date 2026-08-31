40230eba48f522334266c945865fb107f47fca0e
Alunara <alunara@curve.finance>
2026-08-31 15:34:41 +0200

    refactor: remove useless chain id prop from NETWORK_BASE_CONFIG

--- packages/legacy-ui/src/utils/utilsNetworks.ts
+++ packages/legacy-ui/src/utils/utilsNetworks.ts
@@ -13,7 +13,6 @@ const NETWORK_BASE_CONFIG_DEFAULT = {
 export const NETWORK_BASE_CONFIG = {
   [Chain.Ethereum]: {
     id: 'ethereum',
-    chainId: Chain.Ethereum,
     gasPricesUrl: 'https://api.curve.finance/api/getGas',
     gasPricesDefault: 1,
     nativeCurrencySymbol: 'ETH',
@@ -23,7 +22,6 @@ export const NETWORK_BASE_CONFIG = {
   },
   [Chain.Optimism]: {
     id: 'optimism',
-    chainId: Chain.Optimism,
     gasL2: true,
     rpcUrl: 'https://mainnet.optimism.io',
     nativeCurrencySymbol: 'ETH',
@@ -32,7 +30,6 @@ export const NETWORK_BASE_CONFIG = {
   [Chain.Gnosis]: {
     id: 'xdai',
     name: 'Gnosis',
-    chainId: Chain.Gnosis,
     rpcUrl: 'https://rpc.gnosischain.com',
     nativeCurrencySymbol: 'XDAI',
     explorerUrl: 'https://gnosisscan.io/',
@@ -40,7 +37,6 @@ export const NETWORK_BASE_CONFIG = {
   },
   [Chain.Moonbeam]: {
     id: 'moonbeam',
-    chainId: Chain.Moonbeam,
     rpcUrl: 'https://moonbeam.drpc.org',
     nativeCurrencySymbol: 'GLMR',
     explorerUrl: 'https://moonscan.io/',
@@ -48,7 +44,6 @@ export const NETWORK_BASE_CONFIG = {
   },
   [Chain.Polygon]: {
     id: 'polygon',
-    chainId: Chain.Polygon,
     gasPricesUrl: 'https://gasstation.polygon.technology/v2',
     gasPricesDefault: 0,
     rpcUrl: 'https://polygon.drpc.org',
@@ -58,7 +53,6 @@ export const NETWORK_BASE_CONFIG = {
   },
   [Chain.Kava]: {
     id: 'kava',
-    chainId: Chain.Kava,
     gasPricesUnit: 'UKAVA',
     rpcUrl: 'https://evm.kava.io',
     nativeCurrencySymbol: 'KAVA',
@@ -67,7 +61,6 @@ export const NETWORK_BASE_CONFIG = {
   },
   [Chain.Fantom]: {
     id: 'fantom',
-    chainId: Chain.Fantom,
     rpcUrl: 'https://rpc.ftm.tools/',
     nativeCurrencySymbol: 'FTM',
     explorerUrl: 'https://ftmscout.com/',
@@ -75,7 +68,6 @@ export const NETWORK_BASE_CONFIG = {
   },
   [Chain.Arbitrum]: {
     id: 'arbitrum',
-    chainId: Chain.Arbitrum,
     rpcUrl: 'https://arb1.arbitrum.io/rpc',
     nativeCurrencySymbol: 'ETH',
     explorerUrl: 'https://arbiscan.io/',
@@ -83,7 +75,6 @@ export const NETWORK_BASE_CONFIG = {
   },
   [Chain.Avalanche]: {
     id: 'avalanche',
-    chainId: Chain.Avalanche,
     gasPricesUnit: 'nAVAX',
     gasPricesUrl: 'https://api.avax.network/ext/bc/C/rpc',
     gasPricesDefault: 0,
@@ -94,7 +85,6 @@ export const NETWORK_BASE_CONFIG = {
   },
   [Chain.Celo]: {
     id: 'celo',
-    chainId: Chain.Celo,
     rpcUrl: 'https://forno.celo.org',
     nativeCurrencySymbol: 'CELO',
     explorerUrl: 'https://celoscan.io/',
@@ -102,7 +92,6 @@ export const NETWORK_BASE_CONFIG = {
   },
   [Chain.Aurora]: {
     id: 'aurora',
-    chainId: Chain.Aurora,
     rpcUrl: 'https://mainnet.aurora.dev',
     nativeCurrencySymbol: 'aETH',
     explorerUrl: 'https://aurorascan.dev/',
@@ -111,14 +100,12 @@ export const NETWORK_BASE_CONFIG = {
   [Chain.ZkSync]: {
     id: 'zksync',
     name: 'zkSync Era',
-    chainId: Chain.ZkSync,
     rpcUrl: 'https://mainnet.era.zksync.io',
     nativeCurrencySymbol: 'ETH',
     explorerUrl: 'https://era.zksync.network/',
   },
   [Chain.Base]: {
     id: 'base',
-    chainId: Chain.Base,
     gasL2: true,
     rpcUrl: 'https://base-rpc.publicnode.com',
     nativeCurrencySymbol: 'ETH',
@@ -126,42 +113,36 @@ export const NETWORK_BASE_CONFIG = {
   },
   [Chain.Bsc]: {
     id: 'bsc',
-    chainId: Chain.Bsc,
     rpcUrl: 'https://rpc.nodeflare.app/bnb/public',
     nativeCurrencySymbol: 'BNB',
     explorerUrl: 'https://bscscan.com/',
   },
   [Chain.Fraxtal]: {
     id: 'fraxtal',
-    chainId: Chain.Fraxtal,
     rpcUrl: `https://rpc.frax.com`,
     nativeCurrencySymbol: 'FRAX',
     explorerUrl: 'https://fraxscan.com/',
   },
   [Chain.XLayer]: {
     id: 'x-layer',
-    chainId: Chain.XLayer,
     rpcUrl: `https://rpc.xlayer.tech`,
     nativeCurrencySymbol: 'OKB',
     explorerUrl: 'https://www.okx.com/web3/explorer/xlayer/',
   },
   [Chain.Mantle]: {
     id: 'mantle',
-    chainId: Chain.Mantle,
     rpcUrl: `https://rpc.mantle.xyz`,
     nativeCurrencySymbol: 'MNT',
     explorerUrl: 'https://mantlescan.xyz/',
   },
   [Chain.Sonic]: {
     id: 'sonic',
-    chainId: Chain.Sonic,
     rpcUrl: `https://rpc.soniclabs.com`,
     nativeCurrencySymbol: 'S',
     explorerUrl: 'https://sonicscan.org/',
   },
   [Chain.Hyperliquid]: {
     id: 'hyperliquid',
-    chainId: Chain.Hyperliquid,
     rpcUrl: `https://rpc.hyperliquid.xyz/evm`,
     nativeCurrencySymbol: 'HYPE',
     explorerUrl: 'https://hyperevmscan.io/',
