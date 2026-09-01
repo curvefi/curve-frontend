89f6941d86a601074b2759aff500394d016d82fa
0xLokiB <305783192+0xLokiB@users.noreply.github.com>
2026-09-01 11:52:12 +0200

    chore: revert new component tests in veCRV branch

--- tests/cypress/support/helpers/dao/test-context.helpers.ts
+++
@@ -1,8 +0,0 @@
-import { globalLibs } from '@evm-ui/features/connect-wallet/lib/utils'
-import { queryClient } from '@evm-ui/lib/api'
-
-export const setupMockedDaoComponentTest = () => {
-  queryClient.clear()
-  globalLibs.current = {}
-  globalLibs.hydrated = {}
-}
