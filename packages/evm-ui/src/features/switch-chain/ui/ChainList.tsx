05b4b242821585c59a64ba5d98c16e08874c8977
Alunara <alunara@curve.finance>
2026-09-03 17:59:30 +0200

    chore: formatting

--- packages/evm-ui/src/features/switch-chain/ui/ChainList.tsx
+++ packages/evm-ui/src/features/switch-chain/ui/ChainList.tsx
@@ -1,6 +1,11 @@
 import lodash from 'lodash'
 import { Fragment, useMemo, useState } from 'react'
-import { getChainName, isChainConfigured, isChainLite, isChainTestnet } from '@evm-ui/features/connect-wallet/lib/wagmi/chains'
+import {
+  getChainName,
+  isChainConfigured,
+  isChainLite,
+  isChainTestnet,
+} from '@evm-ui/features/connect-wallet/lib/wagmi/chains'
 import { usePathname } from '@evm-ui/hooks/router'
 import { t } from '@evm-ui/lib/i18n'
 import { getCurrentApp, getInternalUrl } from '@evm-ui/shared/routes'
