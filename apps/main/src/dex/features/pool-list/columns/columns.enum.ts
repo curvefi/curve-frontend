ee6db1e3ac90243e1f14be6294a1b85912c6fdd6
Alunara <alunara@curve.finance>
2026-09-03 19:34:09 +0200

    feat: change apy to apr in pool list column titles

--- apps/main/src/dex/features/pool-list/columns/columns.enum.ts
+++ apps/main/src/dex/features/pool-list/columns/columns.enum.ts
@@ -1,6 +1,7 @@
 export enum PoolColumnId {
   PoolName = 'PoolName',
   Tokens = 'Tokens',
+  // The 'APY' columns aren't exactly always APY, but we keep the enum for the sake of not needing a column visibility migration
   NetApy = 'NetApy',
   BaseApy = 'BaseApy',
   WeeklyBaseApy = 'WeeklyBaseApy',
