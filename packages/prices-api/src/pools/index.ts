3f9f92a6f3e55e535a2abd1687727987c40aea2b
Alunara <alunara@curve.finance>
2026-09-06 16:54:56 +0200

    feat: reimplement user pool position detection using prices api

--- packages/prices-api/src/pools/index.ts
+++ packages/prices-api/src/pools/index.ts
@@ -1,5 +1,6 @@
-import type { Address } from '@primitives/address.utils'
+import type { Address, Hex } from '@primitives/address.utils'
 import { addQueryString, fetchJson as fetch } from '@primitives/fetch.utils'
+import { maybe } from '@primitives/objects.utils'
 import { getHost, type Chain, type Options } from '..'
 import { getTimeRange } from '../timestamp'
 import * as Schema from './schema'
@@ -110,6 +111,19 @@ export async function listPoolRegistries({ chainId }: { chainId: number }, optio
   const response = await fetch(`${host}/v2/pools/registries/${query}`)
 
   return Schema.listPoolRegistriesResponse.parse(response)
+}
+
+export async function getUserPoolPositions(
+  { chainId, userAddress, newTx }: { chainId: number; userAddress: Address; newTx?: Hex },
+  options?: Options,
+) {
+  const host = getHost(options)
+  const query = maybe(newTx, newTx => addQueryString({ new_tx: newTx })) ?? ''
+  const response = await fetch(`${host}/v2/pools/${chainId}/users/${userAddress}/positions${query}`, {
+    signal: options?.signal,
+  })
+
+  return Schema.getUserPoolPositionsResponse.parse(response)
 }
 
 export async function getVolume(chain: Chain, poolAddr: string, options?: Options) {
