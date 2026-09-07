7d09fdceef0e76d8699676fb4a13b086095f319e
0xLokiB <305783192+0xLokiB@users.noreply.github.com>
2026-09-07 16:10:39 +0200

    fix: avoid groupBy due to vercel issues

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -1,9 +1,16 @@
 module.exports = {
   /** Verifies that all duplicated dependencies are declaring the exact same version range. */
   constraints: async ({ Yarn: { dependencies } }) =>
-    [...Map.groupBy(dependencies(), dependency => dependency.ident)]
-      .filter(([, deps]) => new Set(deps.map(d => d.range)).size > 1)
-      .forEach(([name, deps]) =>
-        deps.forEach(d => d.error(`Found ${name} with different versions: ${deps.map(d => d.range).join(', ')}`)),
+    Object.entries(
+      dependencies().reduce(
+        (groups, { ident, range }) => ({ ...groups, [ident]: [...(groups[ident] ?? []), range] }),
+        {},
+      ),
+    )
+      .filter(([, ranges]) => new Set(ranges).size > 1)
+      .forEach(([name, ranges]) =>
+        dependencies({ ident: name }).forEach(dependency =>
+          dependency.error(`Found ${name} with different versions: ${[...new Set(ranges)].join(', ')}`),
+        ),
       ),
 }
