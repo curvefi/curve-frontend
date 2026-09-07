0cff3cd600975f2308d54ec2743cdb58ff865184
0xLokiB <305783192+0xLokiB@users.noreply.github.com>
2026-09-07 11:55:47 +0200

    refactor: remove unnecessary props

--- packages/evm-ui/src/shared/ui/EvmErrorMessage.tsx
+++ packages/evm-ui/src/shared/ui/EvmErrorMessage.tsx
@@ -1,8 +1,7 @@
 import type { ComponentProps } from 'react'
 import { useConnection } from 'wagmi'
 import { ErrorMessage } from '@evm-ui/shared/ui/ErrorMessage'
-import type { ConnectionProps } from '@ui/components/ConnectWalletButton'
 
-export const EvmErrorMessage = (
-  props: Omit<ComponentProps<typeof ErrorMessage>, 'userAddress' | keyof ConnectionProps>,
-) => <ErrorMessage {...props} userAddress={useConnection().address} />
+export const EvmErrorMessage = (props: Omit<ComponentProps<typeof ErrorMessage>, 'userAddress'>) => (
+  <ErrorMessage {...props} userAddress={useConnection().address} />
+)
--- packages/evm-ui/src/widgets/EvmErrorBoundary.tsx
+++ packages/evm-ui/src/widgets/EvmErrorBoundary.tsx
@@ -1,8 +1,7 @@
 import type { ComponentProps } from 'react'
 import { useConnection } from 'wagmi'
 import { ErrorBoundary } from '@evm-ui/widgets/ErrorBoundary'
-import type { ConnectionProps } from '@ui/components/ConnectWalletButton'
 
-export const EvmErrorBoundary = (
-  props: Omit<ComponentProps<typeof ErrorBoundary>, 'userAddress' | keyof ConnectionProps>,
-) => <ErrorBoundary {...props} userAddress={useConnection().address} />
+export const EvmErrorBoundary = (props: Omit<ComponentProps<typeof ErrorBoundary>, 'userAddress'>) => (
+  <ErrorBoundary {...props} userAddress={useConnection().address} />
+)
