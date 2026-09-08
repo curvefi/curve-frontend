42084e5dff929b6c5eca861cb194af350f00316b
0xLokiB <305783192+0xLokiB@users.noreply.github.com>
2026-09-08 22:14:05 +0200

    fix: stellar base styles

--- apps/stellar/src/routes/dex.routes.tsx
+++ apps/stellar/src/routes/dex.routes.tsx
@@ -1,25 +1,11 @@
-import Card from '@mui/material/Card'
-import Stack from '@mui/material/Stack'
+import { WorkInProgress } from '@/components/WorkInProgress'
 import { createRoute, Outlet } from '@tanstack/react-router'
-import { EmptyStateCard } from '@ui/components/EmptyStateCard'
-import { SizesAndSpaces } from '@ui/features/themes/design/1_sizes_spaces'
 import { redirectTo } from '@ui/hooks/router'
-import { t } from '@ui/lib/i18n'
 import { rootRoute } from './root.routes'
-
-const { MaxWidth } = SizesAndSpaces
 
 const dexLayoutRoute = createRoute({ getParentRoute: () => rootRoute, path: 'dex', component: Outlet })
 
 const layoutProps = { getParentRoute: () => dexLayoutRoute }
-
-const WipComponent = () => (
-  <Stack sx={{ minHeight: 300, justifyContent: 'center' }}>
-    <Card sx={{ maxWidth: MaxWidth.actionCard, margin: '0 auto' }}>
-      <EmptyStateCard title={t`Work in progress`} description={t`We are working on it`} />
-    </Card>
-  </Stack>
-)
 
 export const dexRoutes = dexLayoutRoute.addChildren([
   createRoute({ path: '/', loader: () => redirectTo('/dex/stellar/pools/'), ...layoutProps }),
@@ -30,13 +16,13 @@ export const dexRoutes = dexLayoutRoute.addChildren([
   }),
   createRoute({
     path: '$network/pools',
-    component: WipComponent,
+    component: WorkInProgress,
     head: () => ({ meta: [{ title: 'Pools - Curve Stellar' }] }),
     ...layoutProps,
   }),
   createRoute({
     path: '$network/pools/$poolIdOrAddress',
-    component: WipComponent,
+    component: WorkInProgress,
     head: () => ({ meta: [{ title: 'Pool - Curve Stellar' }] }),
     ...layoutProps,
   }),
