a6c14daf6bcc435e3451a8750f86f209f2b9cb02
0xPearce <291735384+0xPearce@users.noreply.github.com>
2026-09-09 19:35:36 +0200

    chore: lint

--- packages/legacy-ui/src/CampaignRewards/CampaignRewardsComp.tsx
+++ packages/legacy-ui/src/CampaignRewards/CampaignRewardsComp.tsx
@@ -3,8 +3,8 @@ import type { CampaignRewards } from '@evm-ui/entities/campaigns'
 import { TooltipMessage } from '@legacy-ui/CampaignRewards/TooltipMessage'
 import { Icon } from '@legacy-ui/Icon'
 import { TooltipButton as Tooltip } from '@legacy-ui/Tooltip/TooltipButton'
-import { aprToApy } from '@primitives/rates.utils'
 import { formatNumber } from '@primitives/number.utils'
+import { aprToApy } from '@primitives/rates.utils'
 
 type CampaignRewardsCompProps = {
   rewardsPool: CampaignRewards
