a6e370034f0f0308c4cd0f72a431845ebcdbbb2b
Alunara <alunara@curve.finance>
2026-09-17 17:13:58 +0200

    refactor: simplify zero balance check

--- packages/evm-ui/src/hooks/useTokenBalance.ts
+++ packages/evm-ui/src/hooks/useTokenBalance.ts
@@ -231,9 +231,6 @@ export const prefetchTokenBalances = async (
    */
   zip(tokenContracts, chunk(results, 2))
     .map(([contracts, tokenResults]) => [readContractsQueryOptions(config, { contracts }), tokenResults] as const)
-    .filter(
-      ([{ queryKey }, tokenResults]) =>
-        (tokenResults?.[0].result ?? 0n) > 0n || queryClient.getQueryData(queryKey) !== undefined,
-    )
+    .filter(([{ queryKey }, tokenResults]) => tokenResults?.[0].result || queryClient.getQueryData(queryKey))
     .forEach(([{ queryKey }, tokenResults]) => queryClient.setQueryData(queryKey, tokenResults, { updatedAt }))
 }
