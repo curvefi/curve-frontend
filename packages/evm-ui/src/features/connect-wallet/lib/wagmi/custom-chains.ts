c913d4d44f8866aa1a623c5bff433f7e6557e327
Alunara <alunara@curve.finance>
2026-09-17 17:08:20 +0200

    feat: use etherscan for robinhood explorer

--- packages/evm-ui/src/features/connect-wallet/lib/wagmi/custom-chains.ts
+++ packages/evm-ui/src/features/connect-wallet/lib/wagmi/custom-chains.ts
@@ -1,7 +1,7 @@
 import { chainConfig } from 'viem/op-stack'
 import { defineChain } from 'viem/utils'
 import { Chain as ChainId } from '@primitives/network.utils'
-import { mainnet } from '@wagmi/core/chains'
+import { mainnet, robinhood as robinhoodViem } from '@wagmi/core/chains'
 import { RPC } from './rpc'
 
 export const megaeth = defineChain({
@@ -41,4 +41,11 @@ export const arc = defineChain({
     default: { name: 'Arc Explorer', url: 'https://arc.etherscan.io', apiUrl: 'https://explorer.arc.io/api/v2' },
   },
   contracts: { multicall3: { address: '0xcA11bde05977b3631167028862bE2a173976CA11', blockCreated: 0 } },
+})
+
+export const robinhood = defineChain({
+  ...robinhoodViem,
+  blockExplorers: {
+    default: { name: 'Etherscan', url: 'https://robin.etherscan.io', apiUrl: 'https://robin.etherscan.io/api' },
+  },
 })
