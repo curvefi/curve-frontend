7db5542dce1ef4de2b3d3e764ab8e59c0e3e96a0
0xPearce <291735384+0xPearce@users.noreply.github.com>
2026-09-24 16:10:16 +0200

    test: simplify market assets slippage coverage

--- apps/main/src/llamalend/market-assets-type.utils.ts
+++ apps/main/src/llamalend/market-assets-type.utils.ts
@@ -1,8 +1,19 @@
 import { getAddress } from 'viem'
+import type { MarketAssetsType } from '@evm-ui/types/market'
 import type { Address } from '@primitives/address.utils'
-import { maybe } from '@primitives/objects.utils'
+import { maybe, recordEntries, recordValues } from '@primitives/objects.utils'
 // eslint-disable-next-line no-restricted-imports
 import { MARKET_ASSETS_TYPE_BY_CONTROLLER } from './markets.constants'
 
 export const getMarketAssetsType = (chainId: number, controllerAddress: Address | undefined) =>
   maybe(controllerAddress, address => MARKET_ASSETS_TYPE_BY_CONTROLLER[chainId]?.[getAddress(address)])
+
+export const getMarketAddressesByAssetsType = (assetsType: MarketAssetsType) => [
+  ...new Set(
+    recordValues(MARKET_ASSETS_TYPE_BY_CONTROLLER).flatMap(chainMarkets =>
+      recordEntries(chainMarkets)
+        .filter(([, type]) => type === assetsType)
+        .map(([address]) => address),
+    ),
+  ),
+]
