cb1c40304ad8f3beebe5672539789ebf4e3210ec
Alunara <alunara@curve.finance>
2026-09-24 19:48:44 +0200

    fix: still needs try catch for fetching pools in quickswap

--- apps/main/src/dex/components/PageRouterSwap/utils.ts
+++ apps/main/src/dex/components/PageRouterSwap/utils.ts
@@ -1,6 +1,7 @@
 import type { FormValues, FormStatus, Route } from '@/dex/components/PageRouterSwap/types'
 import type { IRouteStep } from '@curvefi/api/lib/interfaces'
 import type { PoolTemplate } from '@curvefi/api/lib/pools'
+import { log, LogStatus } from '@ui/lib/logging'
 
 export const DEFAULT_FORM_STATUS: FormStatus = {
   isApproved: false,
@@ -19,7 +20,16 @@ export function parseRouterRoutes(routes: IRouteStep[], getPool: (poolId: string
 
   if (Array.isArray(routes) && routes.length > 0) {
     parsedRoutes = routes.map(route => {
-      const pool = getPool(route.poolId)
+      let pool: PoolTemplate | undefined
+
+      // Try catch is needed for non-existent router pool IDs like "WETH wrapper".
+      // Possibly worth fixing, but this seems to be production-build behavior we shouldn't break.
+      try {
+        pool = getPool(route.poolId)
+      } catch (error) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- Existing violation before enabling this rule.
+        log('routerBestRouteAndOutput missing poolName', LogStatus.ERROR, route.poolId, error.message)
+      }
       const poolName = pool ? pool.name : route.poolId
 
       if (pool?.isCrypto) {
